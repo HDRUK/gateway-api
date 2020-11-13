a5d507f38b1dde7a557bb84097410ec54bed57fc
Richard <richard.hobbs@paconsulting.com>
2020-11-13 10:20:04 +0000

    Updated controller function

--- src/resources/eventlog/eventlog.controller.js
+++ src/resources/eventlog/eventlog.controller.js
@@ -2,24 +2,5 @@ import { EventLogModel } from './eventlog.model';
 import _ from 'lodash';
 
 module.exports = {
-    logEvent: async (req, res) => {
-
-        const { 
-            userId = '', 
-            email = '', 
-            event = '', 
-            provider = '', 
-            providerId = '', 
-            timestamp = Date.now(), 
-        } = req;
-
-        await EventLogModel.create({
-            userId,
-            email,
-            event,
-            provider,
-            providerId,
-            timestamp
-        });
-    }
+    logEvent: async (event) => await EventLogModel.create({...event})
 }
