1e01df8129772b2abea4967a1481cf2ccf5d1b35
Callum Reekie <cdjreekie@gmail.com>
2021-11-25 11:03:35 +0000

    CR - base NHSMail strategy

--- src/services/strategies/nhsmail.js
+++ src/services/strategies/nhsmail.js
@@ -1,14 +1,13 @@
 import Url from 'url';
 import axios from 'axios';
-import { to } from 'await-to-js';
-import queryString from 'query-string';
-import { v4 as uuidv4 } from 'uuid';
 import jwt from 'jsonwebtoken';
+import { to } from 'await-to-js';
+import { v4 as uuidv4 } from 'uuid';
 
-import { getUserByProviderId } from '../../resources/user/user.repository';
-import { createUser } from '../../resources/user/user.service';
 import { ROLES } from '../../resources/user/user.roles';
 import helper from '../../resources/utilities/helper.util';
+import { createUser } from '../../resources/user/user.service';
+import { getUserByProviderId } from '../../resources/user/user.repository';
 
 export default class NHSMailStrategy {
 	constructor() {
@@ -16,38 +15,36 @@ export default class NHSMailStrategy {
 			stateString: uuidv4(),
 			nonceString: uuidv4(),
 			baseAuthUrl: process.env.NHSMAIL_SSO_BASE_URL,
+			endpointAuth: '/authorize',
+			endpointToken: '/token',
 			clientID: process.env.NHSMAIL_SSO_CLIENT_ID,
 			clientSecret: process.env.NHSMAIL_SSO_CLIENT_SECRET,
+			scopes: 'openid',
+			callbackURL: '/auth/nhsmail/callback',
 		};
 	}
 
 	// Custom logic for verifying the user credentials
-	verifyCallback = async (accessToken, refreshToken, idToken) => {
+	verifyCallback = async idToken => {
 		if (!jwt.decode(idToken).nonce || jwt.decode(idToken).nonce !== nonceString) {
 			throw new Error('The nonce value is missing or the value in the returned ID token does not match that sent in the initial request');
 		}
 
-		const { data: profile } = await axios({
-			url: baseAuthUrl + '/userinfo',
-			method: 'get',
-			headers: {
-				Authorization: `Bearer ${accessToken}`,
-			},
-		});
+		const profile = jwt.decode(idToken);
 
-		if (!profile || !profile.nhs_number) {
+		if (!profile || !profile.some_unique_id) {
 			throw new Error('Profile information missing from user-info, re-directing to /loginerror');
 		}
 
-		let [err, user] = await to(getUserByProviderId(profile.nhs_number));
+		let [err, user] = await to(getUserByProviderId(profile.some_unique_id));
 		if (err || user) {
 			return [err, user];
 		}
 
 		const [createdError, createdUser] = await to(
 			createUser({
-				provider: 'nhslogin',
-				providerId: profile.nhs_number,
+				provider: 'nhsmail',
+				providerId: profile.some_unique_id,
 				firstname: profile.given_name,
 				lastname: helper.toTitleCase(profile.family_name),
 				password: null,
@@ -59,57 +56,43 @@ export default class NHSMailStrategy {
 		return [createdError, createdUser];
 	};
 
-	initialise = (req, res, next) => {
-		// Initialise the request by redirecting to the appropriate sign in page
+	initialise = (req, res) => {
 		res.redirect(
-			`${this.strategyOptions.baseAuthUrl}/authorize?` +
-				queryString.stringify({
-					client_id: this.strategyOptions.clientID,
-					redirect_uri: Url.format({
-						protocol: req.protocol,
-						host: req.get('host'),
-						pathname: '/auth/nhsmail/callback',
-					}),
-					response_type: 'code',
-					scope: 'openid',
-					nonce: this.strategyOptions.nonceString,
-					state: this.strategyOptions.stateString,
-				})
+			`${this.strategyOptions.baseAuthUrl + this.strategyOptions.endpointAuth}` +
+				`?client_id=${this.strategyOptions.clientID}` +
+				`&redirect_uri=${Url.format({
+					protocol: req.protocol,
+					host: req.get('host'),
+					pathname: this.strategyOptions.callbackURL,
+				})}` +
+				`&response_type=code` +
+				`&scope=${this.strategyOptions.scopes}` +
+				`&nonce=${this.strategyOptions.nonceString}` +
+				`&state=${this.strategyOptions.stateString}`
 		);
 	};
 
 	callback = async (req, res, next) => {
 		try {
-			// Confirm the state string
 			if (req.query.state !== stateString) {
 				throw new Error('The response state parameter does not match the state of the initial request');
 			}
 
-			// Make a request to the token endpoint using the authorisation code
 			const {
-				data: { access_token, refresh_token, id_token },
+				data: { id_token },
 			} = await axios.post(
-				`${this.strategyOptions.baseAuthUrl}/token`,
-				queryString.stringify({
-					code: req.query.code,
-					client_id: this.strategyOptions.clientID,
-					redirect_uri: Url.format({
+				`${this.strategyOptions.baseAuthUrl + this.strategyOptions.endpointToken}` +
+					`?code=${req.query.code}` +
+					`&client_id=${this.strategyOptions.clientID}` +
+					`&redirect_uri=${Url.format({
 						protocol: req.protocol,
 						host: req.get('host'),
 						pathname: new URL(req.originalUrl).pathname,
-					}),
-					grant_type: 'authorization_code',
-					client_assertion_type: 'urn:ietf:params:oauth:client-assertion-type:jwt-bearer',
-					client_assertion: createAndSignBearerToken({
-						clientId: this.strategyOptions.clientID,
-						audience: `${this.strategyOptions.baseAuthUrl}/token`,
-						privateKey: this.strategyOptions.clientSecret,
-					}),
-				})
+					})}` +
+					`&grant_type=authorization_code`
 			);
 
-			// Perform a check on the credentials
-			const [err, user] = await verifyCallback(access_token, refresh_token, id_token);
+			const [err, user] = await verifyCallback(id_token);
 			req.auth = {
 				err: err,
 				user: user,
