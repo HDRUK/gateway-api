640ba2ef27fd5f68db196c945a1adc44ada1a649
Dan Nita <dan.nita@hdruk.ac.uk>
2022-11-28 13:58:07 +0000

    cosmetic changes

--- src/resources/review/v3/review.route.js
+++ src/resources/review/v3/review.route.js
@@ -5,7 +5,7 @@ const ReviewController = require('./review.controller');
 
 const router = express.Router();
 
-// @router   GET /api/v1/reviews/:reviewId?
+// @router   GET /api/v3/reviews/:reviewId?
 // @desc     get all reviews or find reviews by reviewId
 // @access   Private
 router.get('/:reviewId?', passport.authenticate('jwt'), (req, res) => ReviewController.handleReviews(req, res));
