5d1cd3252c9009071ed8a6659dd9d1e088bba9a6
Dan Nita <dan.nita@hdruk.ac.uk>
2022-11-30 11:48:43 +0000

    review patch tech debt

--- src/resources/review/v3/review.controller.js
+++ src/resources/review/v3/review.controller.js
@@ -1,12 +1,14 @@
 import { Reviews } from '../review.model';
+import { storeNotificationMessages } from './notification';
+import { sendEmailNotifications } from './email';
 
 class ReviewController {
     constructor() {}
 
     async getReviews(req, res) {
-        const idString = parseInt(req.params.reviewId) || '';
+        const reviewId = parseInt(req.params.reviewId) || '';
 
-        const data = await this.statementExecutionDB(idString);
+        const data = await this.getDataReviews(reviewId);
 
         return res.status(200).json({
             'success': true,
@@ -14,27 +16,57 @@ class ReviewController {
         });
     }
 
-    async statementExecutionDB(idString) {
+    async updateStateReviews(req, res) {
+        const reviewId = parseInt(req.params.reviewId);
+        const { activeflag } = req.body;
+
+        if (!activeflag) {
+            process.stdout.write(`ReviewController.updateReviews : activeflag missing`);
+            throw new Error(`An error occurred : activeflag missing`);
+        }
+        const statusReview = activeflag === 'approve' ? 'active' : activeflag;
+
+        const review = await this.updateStateDataReviews({ reviewID: reviewId }, { activeflag: statusReview });
+
+        await storeNotificationMessages(review);
+        // Send email notififcation of approval to authors and admins who have opted in
+		await sendEmailNotifications(review, activeflag);
+
+        return res.status(200).json({
+            'success': true
+        });
+    }
+
+    async updateStateDataReviews(filter, update) {
         try {
-            const pipeline = this.reviewDynamicPipeline(idString);
-            const statement = Reviews.aggregate(pipeline);
-            return await statement.exec();
+            return await Reviews.findOneAndUpdate(filter, update, {
+                new: true
+            });
         } catch (err) {
-            process.stdout.write(`ReviewController.statementExecutionDB : ${err.message}`);
+            process.stdout.write(`ReviewController.updateDataReviews : ${err.message}`);
             throw new Error(`An error occurred : ${err.message}`);
         }
     }
 
-    reviewDynamicPipeline(idString) {
+    async getDataReviews(reviewId) {
+        try {
+            const pipeline = this.reviewAggregatePipeline(reviewId);
+            const statement = Reviews.aggregate(pipeline);
+            return await statement.exec();
+        } catch (err) {
+            process.stdout.write(`ReviewController.getDataReviews : ${err.message}`);
+            throw new Error(`An error occurred : ${err.message}`);
+        }
+    }
+
+    reviewAggregatePipeline(reviewId) {
         let query = [];
 
-        if (idString) {
-            query.push({ $match: { 'reviewID': idString } });
+        if (reviewId) {
+            query.push({ $match: { 'reviewID': reviewId } });
         }
         query.push({ "$lookup": { "from": "tools", "localField": "reviewerID", "foreignField": "id", "as": "person" } });
         query.push({ "$lookup": { "from": "tools", "localField": "toolID", "foreignField": "id", "as": "tool" } });
-
-        console.log(JSON.stringify(query));
 
         return query;
     }
--- src/resources/review/v3/review.route.js
+++ src/resources/review/v3/review.route.js
@@ -10,4 +10,11 @@ const router = express.Router();
 // @access   Private
 router.get('/:reviewId?', passport.authenticate('jwt'), (req, res) => ReviewController.getReviews(req, res));
 
+
+// @router      PATCH /api/v3/reviews/:reviewId
+// @bodyParam   {string} activeflag can be: active/approve (approve will be converted in active), reject, archive
+// @desc        get all reviews or find reviews by reviewId
+// @access      Private
+router.patch('/:reviewId', passport.authenticate('jwt'), (req, res) => ReviewController.updateReviews(req, res));
+
 module.exports = router;
