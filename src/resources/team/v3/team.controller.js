10e39afab18310a6b25f43c639c810e37ab8c01b
Dan Nita <dan.nita@hdruk.ac.uk>
2023-02-07 16:37:59 +0000

    http exception team

--- src/resources/team/v3/team.controller.js
+++ src/resources/team/v3/team.controller.js
@@ -17,178 +17,194 @@ class TeamController extends TeamService {
     }
 
     async getTeamMembers(req, res) {
-        const teamId = req.params.teamid;
-        const users = req.user;
-        const currentUserId = req.user._id;
-
-        const team = await this.getMembersByTeamId(teamId);
-
-		teamV3Util.checkUserAuthorization(currentUserId, '', team, users);
-
-        let members = teamV3Util.formatTeamMembers(team);
-
-        this.sendLogInGoogle({
-            action: 'getTeamMembers',
-            input: {
-                teamId,
-                currentUserId,
-            },
-            output: members,
-        });
-
-        res.status(200).json({
-            members,
-        });
+        try {
+            const teamId = req.params.teamid;
+            const users = req.user;
+            const currentUserId = req.user._id;
+    
+            const team = await this.getMembersByTeamId(teamId);
+    
+            teamV3Util.checkUserAuthorization(currentUserId, '', team, users);
+    
+            let members = teamV3Util.formatTeamMembers(team);
+    
+            this.sendLogInGoogle({
+                action: 'getTeamMembers',
+                input: {
+                    teamId,
+                    currentUserId,
+                },
+                output: members,
+            });
+    
+            res.status(200).json({
+                members,
+            });    
+        } catch (err) {
+            throw new HttpExceptions(err.message, err.status);
+        }
     }
 
     async deleteTeamMember(req, res) {
-        const teamId = req.params.teamid;
-        const deleteUserId = req.params.memberid;
-        const userObj = req.user;
-        const currentUserId = req.user._id;
-
-        const team = await this.getTeamByTeamId(teamId);
-
-        let { members = [], users = [] } = team;
-
-        teamV3Util.checkIfLastManager(members, deleteUserId);
-
-        let updatedMembers = [...members].filter(mem => mem.memberid.toString() !== deleteUserId.toString());
-        if (members.length === updatedMembers.length) {
-            throw new Error(`The user requested for deletion is not a member of this team.`);
-        }
-
-        team.members = updatedMembers;
         try {
-            team.save(function (err, result) {
-                if (err) {
-                    throw new HttpExceptions(err.message);
-                } else {
-                    let removedUser = users.find(user => user._id.toString() === deleteUserId.toString());
-                    teamV3Util.createTeamNotifications(constants.notificationTypes.MEMBERREMOVED, { removedUser }, team, userObj);
+            const teamId = req.params.teamid;
+            const deleteUserId = req.params.memberid;
+            const userObj = req.user;
+            const currentUserId = req.user._id;
 
-                    this.sendLogInGoogle({
-                        action: 'deleteTeamMember',
-                        input: {
-                            teamId,
-                            memberid: deleteUserId,
-                            currentUserId,
-                        },
-                        output: 'success'
-                    });
+            const team = await this.getTeamByTeamId(teamId);
 
-                    return res.status(204).json({
-                        success: true,
-                    });
-                }
-            });    
-        } catch (e) {
-            throw new Error(e.message);
+            let { members = [], users = [] } = team;
+
+            teamV3Util.checkIfLastManager(members, deleteUserId);
+
+            let updatedMembers = [...members].filter(mem => mem.memberid.toString() !== deleteUserId.toString());
+            if (members.length === updatedMembers.length) {
+                throw new Error(`The user requested for deletion is not a member of this team.`);
+            }
+
+            team.members = updatedMembers;
+            try {
+                team.save(function (err, result) {
+                    if (err) {
+                        throw new HttpExceptions(err.message);
+                    } else {
+                        let removedUser = users.find(user => user._id.toString() === deleteUserId.toString());
+                        teamV3Util.createTeamNotifications(constants.notificationTypes.MEMBERREMOVED, { removedUser }, team, userObj);
+
+                        this.sendLogInGoogle({
+                            action: 'deleteTeamMember',
+                            input: {
+                                teamId,
+                                memberid: deleteUserId,
+                                currentUserId,
+                            },
+                            output: 'success'
+                        });
+
+                        return res.status(204).json({
+                            success: true,
+                        });
+                    }
+                });    
+            } catch (e) {
+                throw new HttpExceptions(e.message);
+            }
+        } catch (err) {
+            throw new HttpExceptions(err.message, err.status);
         }
     }
 
     async addTeamMember(req, res) {
-        const teamId = req.params.teamid;
-        const currentUserId = req.user._id;
-        const { memberId, roles = [] } = req.body;
-
-        const team = await this.getTeamByTeamId(teamId);
-
-        let { members } = team;
-
-        let checkIfExistMember = members.find(item => item.memberid.toString() === memberId.toString());
-        if (checkIfExistMember) {
-            throw new HttpExceptions(`Member already exists`, 409);
-        }
-
-        let newMembers = {
-            roles: roles,
-            memberid: memberId,
-            notifications: []
-        };
-
-        team.members = team.members.concat(newMembers);
-		team.save(async err => {
-			if (err) {
-				throw new HttpExceptions(err.message);
-			} else {
-				let newUsers = await UserModel.find({ _id: memberId });
-				teamV3Util.createTeamNotifications(constants.notificationTypes.MEMBERADDED, { newUsers }, team, req.user);
-				const updatedTeam = await this.getMembersByTeamId(teamId);
-				let users = teamV3Util.formatTeamMembers(updatedTeam);
-
-                this.sendLogInGoogle({
-                    action: 'addTeamMember',
-                    input: {
-                        teamId,
-                        currentUserId,
-                        body: req.body,
-                    },
-                    output: users,
-                });
-
-				return res.status(201).json({
-					success: true,
-					members: users,
-				});
-			}
-		});
-    }
-
-    async updateTeamMember(req, res) {
-        const teamId = req.params.teamid;
-        const updateUserId = req.params.memberid;
-        const userObj = req.user;
-        const userTeams = userObj.teams || [];
-        const currentUserId = req.user._id;
-        const { roles = [] } = req.body;
-
-        const team = await this.getTeamByTeamId(teamId);
-
-        let { members } = team;
-
-        let checkIfExistMember = members.find(item => item.memberid.toString() === updateUserId.toString());
-        if (!checkIfExistMember) {
-            throw new HttpExceptions(`The member does not exist in the team`, 409);
-        }
-
-        const approverUserRoles = teamV3Util.getAllRolesForApproverUser(userTeams, teamId, currentUserId);
-        const approvedRoles = teamV3Util.listOfRolesAllowed(approverUserRoles, constants.rolesAcceptedByRoles);
-        teamV3Util.checkAllowNewRoles(roles, approvedRoles);
-
-        team.members.map(member => {
-            if (member.memberid.toString() === updateUserId.toString()) {
-                member.roles = roles;
-            }
-        });
-
         try {
+            const teamId = req.params.teamid;
+            const currentUserId = req.user._id;
+            const { memberId, roles = [] } = req.body;
+    
+            const team = await this.getTeamByTeamId(teamId);
+    
+            let { members } = team;
+    
+            let checkIfExistMember = members.find(item => item.memberid.toString() === memberId.toString());
+            if (checkIfExistMember) {
+                throw new HttpExceptions(`Member already exists`, 409);
+            }
+    
+            let newMembers = {
+                roles: roles,
+                memberid: memberId,
+                notifications: []
+            };
+    
+            team.members = team.members.concat(newMembers);
             team.save(async err => {
                 if (err) {
                     throw new HttpExceptions(err.message);
                 } else {
-                    let updatedTeam = await this.getMembersByTeamId(teamId);
+                    let newUsers = await UserModel.find({ _id: memberId });
+                    teamV3Util.createTeamNotifications(constants.notificationTypes.MEMBERADDED, { newUsers }, team, req.user);
+                    const updatedTeam = await this.getMembersByTeamId(teamId);
                     let users = teamV3Util.formatTeamMembers(updatedTeam);
-
+    
                     this.sendLogInGoogle({
-                        action: 'updateTeamMember',
+                        action: 'addTeamMember',
                         input: {
                             teamId,
-                            memberid: updateUserId,
                             currentUserId,
                             body: req.body,
                         },
                         output: users,
                     });
-
-                    return res.json({
+    
+                    return res.status(201).json({
                         success: true,
                         members: users,
                     });
                 }
-            });    
-        } catch (e) {
-            throw new HttpExceptions(e.message);
+            });
+        } catch (err) {
+            throw new HttpExceptions(err.message, err.status);
+        }
+    }
+
+    async updateTeamMember(req, res) {
+        try {
+            const teamId = req.params.teamid;
+            const updateUserId = req.params.memberid;
+            const userObj = req.user;
+            const userTeams = userObj.teams || [];
+            const currentUserId = req.user._id;
+            const { roles = [] } = req.body;
+    
+            const team = await this.getTeamByTeamId(teamId);
+    
+            let { members } = team;
+    
+            let checkIfExistMember = members.find(item => item.memberid.toString() === updateUserId.toString());
+            if (!checkIfExistMember) {
+                throw new HttpExceptions(`The member does not exist in the team`, 409);
+            }
+    
+            const approverUserRoles = teamV3Util.getAllRolesForApproverUser(userTeams, teamId, currentUserId);
+            const approvedRoles = teamV3Util.listOfRolesAllowed(approverUserRoles, constants.rolesAcceptedByRoles);
+            teamV3Util.checkAllowNewRoles(roles, approvedRoles);
+    
+            team.members.map(member => {
+                if (member.memberid.toString() === updateUserId.toString()) {
+                    member.roles = roles;
+                }
+            });
+    
+            try {
+                team.save(async err => {
+                    if (err) {
+                        throw new HttpExceptions(err.message);
+                    } else {
+                        let updatedTeam = await this.getMembersByTeamId(teamId);
+                        let users = teamV3Util.formatTeamMembers(updatedTeam);
+    
+                        this.sendLogInGoogle({
+                            action: 'updateTeamMember',
+                            input: {
+                                teamId,
+                                memberid: updateUserId,
+                                currentUserId,
+                                body: req.body,
+                            },
+                            output: users,
+                        });
+    
+                        return res.json({
+                            success: true,
+                            members: users,
+                        });
+                    }
+                });    
+            } catch (e) {
+                throw new HttpExceptions(e.message);
+            }    
+        } catch (err) {
+            throw new HttpExceptions(err.message, err.status);
         }
     }
 
