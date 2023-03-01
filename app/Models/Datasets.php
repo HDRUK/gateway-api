0a36dcf369eb4fa1a011bf00a98f9c724625d36a
Loki Sinclair <loki.sinclair@hdruk.ac.uk>
2023-03-01 15:36:05 +0000

    adds datasets model and migration for api 2.0

---
+++ app/Models/Datasets.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Datasets extends Model
+{
+    use HasFactory;
+
+    /**
+     * Table associated with this model
+     * 
+     * @var string
+     */
+    protected $table = 'datasets';
+
+    /**
+     * Indicates if this model is timestamped
+     * 
+     * @var bool
+     */
+    public $timestamps = true;
+
+    /**
+     * Dictates the mongo imported id associated with this model
+     * 
+     * @var string
+     */
+    private $mongoId = '';
+
+    /**
+     * Indicates if this model is active or draft
+     * 
+     * @var bool
+     */
+    private $active = false;
+
+    /**
+     * Author id associated with this model
+     * 
+     * @var int
+     */
+    private $applicationStatusAuthor = 0;
+
+    /**
+     * Description identifier associated with this model's application status
+     * 
+     * @var int
+     */
+    private $applicationStatusDesc = 0;
+
+    /**
+     * Indicates whether this model is used commercially
+     * 
+     * @var bool
+     */
+    private $commercialUse = false;
+
+    /**
+     * Legacy identifier to track dataset ids, but we'll organically
+     * have the models primary key supersed this
+     * 
+     * @var string
+     */
+    private $datasetId = '';
+
+    /**
+     * Indicates if this model uses 5 safes framework
+     * 
+     * @var bool
+     */
+    private $is5Safes = false;
+
+    /**
+     * Indicates if this model is for cohort discovery
+     * 
+     * @var bool
+     */
+    private $isCohortDiscovery = false;
+
+    /**
+     * License associated with this model
+     * 
+     * @var string
+     */
+    private $license = '';
+
+    /**
+     * Identifier associated with this model - legacy
+     * 
+     * @var string
+     */
+    private $pid = '';
+
+    /**
+     * Question Answers associated with this model
+     * 
+     * @var string
+     */
+    private $questionAnswers = '';
+
+    /**
+     * Source associated with this model
+     * 
+     * @var string
+     */
+    private $source = '';
+}
---
+++ database/migrations/2023_03_01_151815_create_datasets_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('datasets', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->char('mongo_id', 255)->nullable();
+            $table->boolean('active');
+            $table->integer('application_status_author')->nullable();
+            $table->integer('application_status_desc')->nullable();
+            $table->boolean('commercial_use')->nullable();
+            $table->char('dataset_id', 255)->nullable();
+            $table->boolean('is_5_safes')->nullable();
+            $table->boolean('is_cohort_discovery')->nullable();
+            $table->char('license', 255)->nullable();
+            $table->char('pid', 255)->nullable();
+            $table->json('question_answers')->nullable();
+            $table->char('source', 255)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('datasets');
+    }
+};
