6eb4c008fa6ed57f95f42632095b9c9b8fa517b6
Dan Nita <dan.nita@hdruk.ac.uk>
2023-03-16 12:18:06 +0000

    update add new user in team

--- src/resources/team/team.controller.js
+++ src/resources/team/team.controller.js
@@ -82,7 +82,7 @@ const formatTeamUsers = team => {
 				id,
 				_id,
 				email,
-				additionalInfo: { organisation, bio, showOrganisation = true, showBio = true },
+				additionalInfo: { organisation = '', bio = '', showOrganisation = true, showBio = true },
 			} = user;
 			let userMember = team.members.find(el => el.memberid.toString() === user._id.toString());
 			let { roles = [] } = userMember;
