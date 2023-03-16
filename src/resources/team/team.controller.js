91227c53fdd88d65c515e98aa55d7bc3757356f2
Dan Nita <dan.nita@hdruk.ac.uk>
2023-03-16 15:49:10 +0000

    update

--- src/resources/team/team.controller.js
+++ src/resources/team/team.controller.js
@@ -41,7 +41,6 @@ const getTeamById = async (req, res) => {
 // GET api/v1/teams/:id/members
 const getTeamMembers = async (req, res) => {
 	try {
-		// 1. Get the team from the database
 		const team = await TeamModel.findOne({ _id: req.params.id }).populate({
 			path: 'users',
 			populate: {
@@ -82,8 +81,9 @@ const formatTeamUsers = team => {
 				id,
 				_id,
 				email,
-				additionalInfo: { organisation = '', bio = '', showOrganisation = true, showBio = true },
+				additionalInfo,
 			} = user;
+			let { organisation = '', bio = '', showOrganisation = true, showBio = true } = { ...additionalInfo };
 			let userMember = team.members.find(el => el.memberid.toString() === user._id.toString());
 			let { roles = [] } = userMember;
 			return {
