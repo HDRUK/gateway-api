786835df62aa9db5d69987ff07e723572c93c88e
Sam Cox <sam.cox@nottingham.ac.uk>
2023-06-14 16:26:20 +0100

    Add Controller functions, but with reduced number of fields.

---
+++ app/Http/Requests/CreateDataUseRegisterRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Http\Requests\BaseFormRequest;
+
+class CreateDataUseRegisterRequest extends BaseFormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'counter' => [
+                'int',
+                'required',
+            ],
+            'user_id' => [
+                'int',
+                'required',
+                'exists:users,id',
+            ],
+            'team_id' => [
+                'int',
+                'required',
+                'exists:teams,id',
+            ],
+        ];
+    }
+}
--- app/Models/DataUseRegister.php
+++ app/Models/DataUseRegister.php
@@ -2,10 +2,72 @@
 
 namespace App\Models;
 
+use App\Models\Team;
+use App\Models\User;
+
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class DataUseRegister extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable
+     * 
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'counter',
+        // 'keywords',
+        // 'dataset_ids',
+        // 'gateway_dataset_ids',
+        // 'non_gateway_dataset_ids',
+        // 'gateway_applicants',
+        // 'non_gateway_applicants',
+        // 'funders_and_sponsors',
+        // 'other_approval_committees',
+        // 'gateway_output_tools',
+        // 'gateway_output_papers',
+        // 'non_gateway_outputs',
+        // 'project_title',
+        // 'project_id_text',
+        // 'organisation_name',
+        // 'organisation_sector',
+        // 'lay_summary',
+        // 'latest_approval_date',
+        // 'enabled',
+        'team_id',
+        'user_id',
+        // 'last_activity',
+        // 'manual_upload',
+        // 'rejection_reason',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        // 'latest_approval_date' => 'datetime',
+        // 'last_activity' => 'datetime',
+    ];
+
+    /**
+     * Get the user that owns the data use register
+     */
+    public function user(): HasOne
+    {
+        return $this->hasOne(User::class, 'id', 'user_id');
+    }
+
+    /**
+     * Get the team that owns the data use register
+     */
+    public function team(): HasOne
+    {
+        return $this->hasOne(Team::class, 'id', 'team_id');
+    }
 }
