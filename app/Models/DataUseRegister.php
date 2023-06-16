268c618eb300219a14a652c0f4140043bc4f2ea1
Sam Cox <sam.cox@nottingham.ac.uk>
2023-06-16 08:49:31 +0100

    Add soft deletes.

--- app/Models/DataUseRegister.php
+++ app/Models/DataUseRegister.php
@@ -8,10 +8,11 @@ use App\Models\User;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class DataUseRegister extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     /**
      * The attributes that are mass assignable
