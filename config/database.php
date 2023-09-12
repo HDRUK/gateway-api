a26f5206c0e841639d34792655eb08be3e6edacf
soringumeni1 <sorin.gumeni@hdruk.ac.uk>
2023-09-12 15:39:45 +0100

    GAT-2918: Add extra vars and remove the array filter

--- config/database.php
+++ config/database.php
@@ -58,11 +58,12 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
+            'options' => [
+                PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
                 PDO::MYSQL_ATTR_SSL_CERT => env('MYSQL_ATTR_SSL_CERT'),
                 PDO::MYSQL_ATTR_SSL_KEY => env('MYSQL_ATTR_SSL_KEY'),
-            ]) : [],
+            ],
         ],
 
         'pgsql' => [
