6523b49d8f080aba5e0203c6a73b4c56ef40df4b
Loki Sinclair <loki.sinclair@hdruk.ac.uk>
2023-09-19 09:14:40 +0100

    swagger annotations for new endpoint

--- app/Http/Controllers/ServiceLayerController.php
+++ app/Http/Controllers/ServiceLayerController.php
@@ -8,6 +8,29 @@ use Illuminate\Http\Request;
 
 class ServiceLayerController extends Controller
 {
+
+    /**
+     * @OA\Get(
+     *     path="/api/services/federations",
+     *     operationId="getActiveFederationApplications",
+     *     summary="ServiceLayerController@getActiveFederationApplications",
+     *     description="Get a list of all actve federations for use in service layer",
+     *     @OA\Response(
+     *         response="200",
+     *         description="Success response",
+     *         @OA\JsonContent(
+     *             @OA\Property(
+     *                 type="array",
+     *                 example="[]",
+     *                 @OA\Items(
+     *                     type="array",
+     *                     @OA\Items()
+     *                 )
+     *             )
+     *         )
+     *     )
+     * )
+     */
     public function getActiveFederationApplications(Request $request)
     {
         $federations = Federation::with('team')->where('enabled', true)->get();
