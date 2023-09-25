e26e94127e6c406c77bcb7bd7a74b48f701586ec
Dan Nita <dan.nita@hdruk.ac.uk>
2023-09-25 16:34:52 +0100

    update

--- app/Http/Controllers/Api/V1/LogoutController.php
+++ app/Http/Controllers/Api/V1/LogoutController.php
@@ -6,7 +6,6 @@ use Illuminate\Http\Request;
 use App\Models\AuthorisationCode;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Cookie;
-use Laravel\Socialite\Facades\Socialite;
 
 class LogoutController extends Controller
 {
