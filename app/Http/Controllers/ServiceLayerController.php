6bef5e9a79116a4ae27d2ec83d48ac495b77a98a
Loki Sinclair <loki.sinclair@hdruk.ac.uk>
2023-09-26 15:27:50 +0100

    updates for fma2 service layer

--- app/Http/Controllers/ServiceLayerController.php
+++ app/Http/Controllers/ServiceLayerController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use Config;
 use App\Models\Federation;
 
 use Illuminate\Http\Request;
+
 
 class ServiceLayerController extends Controller
 {
@@ -33,7 +35,77 @@ class ServiceLayerController extends Controller
      */
     public function getActiveFederationApplications(Request $request)
     {
-        $federations = Federation::with('team')->where('enabled', true)->get();
+        $federations = Federation::with('team')
+            ->where('enabled', 1)
+            ->where('tested', 1)
+            ->get();
         return response()->json($federations);
+    }
+
+    /**
+     * @OA\Post(
+     *     path="/api/services/federations/{id}",
+     *     operationId="setFederationInvalidRunState",
+     *     summary="ServiceLayerController@setFederationInvalidRunState",
+     *     description="Sets the specified federations run state to invalid, when errors are found",
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         description="federation id",
+     *         required=true,
+     *         example="1",
+     *         @OA\Schema(
+     *             type="integer",
+     *             description="federation id",
+     *         ),
+     *     ),
+     *     @OA\RequestBody(
+     *         required=true,
+     *         description="Enabled and tested flags to set",
+     *         @OA\MediaType(
+     *             mediaType="application/json",
+     *             @OA\Schema(
+     *                 @OA\Property(property="enabled", type="integer", example="0"),
+     *                 @OA\Property(property="tested", type="integer", example="0"),
+     *             ),
+     *         ),
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Updated",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="message", type="string", example="success"),
+     *         ),
+     *     ),
+     *     @OA\Response(
+     *         response=404,
+     *         description="Not found",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="message", type="string", example="not found"),
+     *         ),
+     *     ),
+     *     @OA\Response(
+     *         response=500,
+     *         description="Error",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="message", type="string", example="error"),
+     *         ),
+     *     ),
+     * )
+     */
+    public function setFederationInvalidRunState(Request $request, int $id)
+    {
+        try {
+            $federation = Federation::where('id', $id)->first();
+            $federation->enabled = 0;
+            $federation->tested = 0;
+            if ($federation->save()) {
+                return response()->json([
+                    'message' => Config::get('statuscodes.STATUS_OK.message'),
+                ]);
+            }
+        } catch (Exception $e) {
+            throw new Exception($e->getMessage());
+        }
     }
 }
