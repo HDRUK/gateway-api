b15ef86761d779aaea0cb16afe6a9c886d443bcf
Loki Sinclair <loki.sinclair@hdruk.ac.uk>
2023-10-11 14:39:22 +0100

    updated to allow arrays as per standard

--- config/cors.php
+++ config/cors.php
@@ -15,11 +15,11 @@ return [
     */
 
     'paths' => explode(',', env('CORS_PATH', 'api/*,sanctum/csrf-cookie')),
-    'allowed_methods' => explode(',', env('CORS_ALLOWED_METHODS', '*')),
-    'allowed_origins' => explode(',', env('CORS_ALLOWED_ORIGINS', '*')),
-    'allowed_origins_patterns' => explode(',', env('CORS_ALLOWED_ORIGINS_PATTERNS', '*')),
-    'allowed_headers' => explode(',', env('CORS_ALLOWED_HEADERS', '*')),
-    'exposed_headers' => explode(',', env('CORS_EXPOSED_HEADERS', '')),
+    'allowed_methods' => explode(',', env('CORS_ALLOWED_METHODS', ['*'])),
+    'allowed_origins' => explode(',', env('CORS_ALLOWED_ORIGINS', ['*'])),
+    'allowed_origins_patterns' => explode(',', env('CORS_ALLOWED_ORIGINS_PATTERNS', ['*'])),
+    'allowed_headers' => explode(',', env('CORS_ALLOWED_HEADERS', ['*'])),
+    'exposed_headers' => explode(',', env('CORS_EXPOSED_HEADERS', [])),
     'max_age' => env('CORS_MAX_AGE', 0),
     'supports_credentials' => env('CORS_SUPPORTS_CREDENTIALS', true),
 ];
