79907afd7981b94dd7465bb33f1ba7854e3730b4
Dan Nita <dan.nita@hdruk.ac.uk>
2024-01-22 09:35:08 +0000

    update db data_use_registers

--- database/migrations/2024_01_18_140535_add_fields_to_data_use_registers_table.php
+++ database/migrations/2024_01_18_140535_add_fields_to_data_use_registers_table.php
@@ -13,25 +13,25 @@ return new class extends Migration
     {
         Schema::table('data_use_registers', function (Blueprint $table) {
             $table->json('non_gateway_datasets')->nullable(); // nonGatewayDatasets
-            $table->json('non_gateway_applicants')->nullable(); // gatewayApplicants
-            $table->json('funders_and_sponsors')->nullable();
-            $table->json('other_approval_committees')->nullable();
-            $table->json('gateway_outputs_tools')->nullable();
-            $table->json('gateway_outputs_papers')->nullable();
-            $table->json('non_gateway_outputs')->nullable();
-            $table->string('project_title')->nullable();
-            $table->string('project_id_text')->nullable();
-            $table->string('organisation_name')->nullable();
-            $table->string('organisation_sector')->nullable();
-            $table->text('lay_summary')->nullable();
-            $table->text('technical_summary')->nullable();
-            $table->timestamp('latest_approval_date')->nullable(); // ???? vs updated_at ??? status
-            $table->boolean('manual_upload')->default(1);
+            $table->json('non_gateway_applicants')->nullable(); // nonGatewayApplicants
+            $table->json('funders_and_sponsors')->nullable(); // fundersAndSponsors
+            $table->json('other_approval_committees')->nullable(); // otherApprovalCommittees
+            $table->json('gateway_outputs_tools')->nullable(); // gatewayOutputsTools
+            $table->json('gateway_outputs_papers')->nullable(); // gatewayOutputsPapers
+            $table->json('non_gateway_outputs')->nullable(); // nonGatewayOutputs
+            $table->string('project_title')->nullable(); // projectTitle
+            $table->string('project_id_text')->nullable(); // projectIdText
+            $table->string('organisation_name')->nullable(); // organisationName
+            $table->string('organisation_sector')->nullable(); // organisationSector
+            $table->text('lay_summary')->nullable(); // laySummary
+            $table->text('technical_summary')->nullable(); // technicalSummary
+            $table->timestamp('latest_approval_date')->nullable(); // latestApprovalDate
+            $table->boolean('manual_upload')->default(1); // manualUpload
 
-            $table->string('rejection_reason')->nullable();
-            $table->string('sublicence_arrangements')->nullable();
-            $table->text('public_benefit_statement')->nullable();
-            $table->string('data_sensitivity_level')->nullable();
+            $table->string('rejection_reason')->nullable(); // rejectionReason
+            $table->string('sublicence_arrangements')->nullable(); // sublicenceArrangements
+            $table->text('public_benefit_statement')->nullable(); // publicBenefitStatement
+            $table->string('data_sensitivity_level')->nullable(); // dataSensitivityLevel
 
             $table->timestamp('project_start_date')->nullable(); // projectStartDate
             $table->timestamp('project_end_date')->nullable(); // projectEndDate
@@ -56,16 +56,18 @@ return new class extends Migration
 
             $table->text('technicalSummary')->nullable(); // technicalSummary
 
-            $table->bigInteger('user_id')->nullable()->default(null)->unsigned();
-            $table->bigInteger('team_id')->nullable()->default(null)->unsigned();
+            $table->bigInteger('user_id')->nullable()->default(null)->unsigned(); // user: from team
+            $table->bigInteger('team_id')->nullable()->default(null)->unsigned(); // publisher: from team
             $table->integer('counter')->default(0); // counter
 
             $table->char('mongo_object_id', 24)->nullable();
-            $table->char('mongo_id', 255)->nullable();
-            $table->boolean('enabled')->default(1);
+            $table->char('mongo_id', 255)->nullable(); // id
+            $table->boolean('enabled')->default(1); // activeflag
 
             $table->foreign('user_id')->references('id')->on('users');
             $table->foreign('team_id')->references('id')->on('teams');
+
+            $table->timestamp('last_activity')->nullable(); // lastActivity
         });
     }
 
