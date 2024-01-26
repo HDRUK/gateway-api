fc595a57bca8c055a76fc4527015fd746e154a4a
Calum Macdonald <calmacx@gmail.com>
2024-01-26 11:20:07 +0000

    bug fixes for datasets

--- app/Http/Controllers/Api/V1/DatasetController.php
+++ app/Http/Controllers/Api/V1/DatasetController.php
@@ -185,7 +185,7 @@ class DatasetController extends Controller
 
         // perform query for the matching datasets with ordering and pagination. 
         // Include soft-deleted versions.
-        $datasets = Dataset::with(['versions' => fn($version) => $version->withTrashed()->latest()->first()])
+        $datasets = Dataset::with(['versions' => fn($version) => $version->withTrashed()->latest()])
             ->whereIn('id', $matches)
             ->when($request->has('withTrashed') || $filterStatus === 'ARCHIVED', 
                 function ($query) {
