b7d458529a25038b504969395797d9fd904beb05
Dan Nita <dan.nita@hdruk.ac.uk>
2024-02-01 15:30:26 +0000

    create minim env for start migration

---
+++ database/seeders/StartMigrationSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class StartMigrationSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $this->call([
+            SectorSeeder::class,
+            PermissionSeeder::class,
+            RoleSeeder::class, 
+            StartMigrationUserSeeder::class,
+        ]);
+    }
+}
