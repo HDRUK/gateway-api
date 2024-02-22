2bf9e0389b870357b9679ecff3ee4c49553bd18d
Branwen Snelling <branwen.snelling@hdruk.ac.uk>
2024-02-22 14:08:46 +0000

    remove debug line

--- app/Http/Controllers/Api/V1/DatasetController.php
+++ app/Http/Controllers/Api/V1/DatasetController.php
@@ -573,8 +573,6 @@ class DatasetController extends Controller
                     base64_encode(gzcompress(gzencode(json_encode($input['metadata'])), 6))
                 );
 
-                MMC::reindexElastic($dataset->id);
-
                 return response()->json([
                     'message' => 'created',
                     'data' => $dataset->id,
