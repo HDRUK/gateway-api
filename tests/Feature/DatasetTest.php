e7947c4d6e77d4ac3a2d2588beaa507fa4c18df1
Dan Nita <dan.nita@hdruk.ac.uk>
2024-02-26 15:44:54 +0000

    update

--- tests/Feature/DatasetTest.php
+++ tests/Feature/DatasetTest.php
@@ -537,7 +537,8 @@ class DatasetTest extends TestCase
         $respArray = $responseGetOne->decodeResponseJson();
 
         $this->assertArrayHasKey('named_entities', $respArray['data']);
-
+        $this->assertNotEmpty($respArray['data']['named_entities']);
+        
         // delete dataset
         $responseDeleteDataset = $this->json(
             'DELETE',
