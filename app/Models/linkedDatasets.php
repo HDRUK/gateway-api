ad56b8de35a13dbf66b1e5854727bf3ddfcf9d85
tom.giles <tom.giles@nottingham.ac.uk>
2024-03-21 14:34:46 +0000

    Update to PascalCase

--- app/Models/linkedDatasets.php
+++ app/Models/linkedDatasets.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class linkedDatasets extends Model
+class LinkedDatasets extends Model
 {
     use HasFactory;
 
