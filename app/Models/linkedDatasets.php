aa411b6ba36035dca1f6458340ade6ac49e5e55d
tom.giles <tom.giles@nottingham.ac.uk>
2024-03-21 14:27:45 +0000

    Updated to use camelCase

--- app/Models/linkedDatasets.php
+++ app/Models/linkedDatasets.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class LinkedDatasets extends Model
+class linkedDatasets extends Model
 {
     use HasFactory;
 
