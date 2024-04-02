9cdd38ff96d2fec58b6800c192f5510b2c4e0b68
Dan Nita <dan.nita@hdruk.ac.uk>
2024-04-02 10:30:21 +0100

    tests

--- tests/Feature/EmailServiceTest.php
+++ tests/Feature/EmailServiceTest.php
@@ -4,20 +4,24 @@ namespace Tests\Feature;
 
 use Carbon\Carbon;
 
+use App\Mail\Email;
+use Tests\TestCase;
 use App\Jobs\SendEmailJob;
-use App\Models\EmailTemplate;
-use App\Exceptions\EmailTemplateException;
 
+use App\Models\EmailTemplate;
+use Illuminate\Support\Facades\Bus;
+use Illuminate\Support\Facades\Http;
+
+use Illuminate\Support\Facades\Mail;
+use App\Exceptions\EmailTemplateException;
+use Database\Seeders\EmailTemplatesSeeder;
+use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Support\Facades\Bus;
-
-use Tests\TestCase;
-use Database\Seeders\EmailTemplatesSeeder;
 
 class EmailServiceTest extends TestCase
 {
-    use RefreshDatabase;
+    use RefreshDatabase, WithFaker;
 
     public function setUp(): void
     {
@@ -34,8 +38,38 @@ class EmailServiceTest extends TestCase
         Bus::fake();
     }
 
-    public function test_dispatch_email_job()
+    // public function test_dispatch_email_job()
+    // {
+    //     $to = [
+    //         'to' => [
+    //             'email' => 'loki.sinclair@hdruk.ac.uk',
+    //             'name' => 'Loki Sinclair',
+    //         ],
+    //     ];
+
+    //     $template = EmailTemplate::where('identifier', '=', 'example_template')->first();
+
+    //     $replacements = [
+    //         '[[header_text]]' => 'Health Data Research UK',
+    //         '[[button_text]]' => 'Click me!',
+    //         '[[subheading_text]]' => 'Sub Heading Something or other',
+    //     ];
+
+    //     Bus::assertNothingDispatched();
+
+    //     SendEmailJob::dispatch($to, $template, $replacements);
+
+    //     Bus::assertDispatched(SendEmailJob::class);
+    // }
+
+    public function testSendEmailJobDispatchesEmailCorrectly()
     {
+        Mail::fake();
+
+        Http::fake([
+            '*' => Http::response(['html' => '<p>Mocked MJML APi responses HTML Content</p>'], 200),
+        ]);
+
         $to = [
             'to' => [
                 'email' => 'loki.sinclair@hdruk.ac.uk',
@@ -51,10 +85,19 @@ class EmailServiceTest extends TestCase
             '[[subheading_text]]' => 'Sub Heading Something or other',
         ];
 
+        // Dispatch the job
         Bus::assertNothingDispatched();
-
-        SendEmailJob::dispatch($to, $template, $replacements);
-
+        dispatch(new SendEmailJob($to, $template, $replacements));
         Bus::assertDispatched(SendEmailJob::class);
+
+        // Assert an email was sent to the correct recipient with the correct Mailable class
+        // Mail::assertSent(Email::class, function ($mail) use ($to) {
+        //     return $mail->hasTo($to['to']['email']);
+        // });
+
+        // // Assert that the HTTP client was called as expected (optional, depending on your test's focus)
+        // Http::assertSent(function ($request) {
+        //     return str_contains($request['mjml'], 'Hello, John Doe'); // Assert the request contains the replaced body text
+        // });
     }
 }
