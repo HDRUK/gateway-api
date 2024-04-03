9f4ac3758a95c346fc5f1cbb66020f15132358bb
Dan Nita <dan.nita@hdruk.ac.uk>
2024-04-03 11:14:52 +0100

    update

--- tests/Unit/CohortUserExpiryTest.php
+++ tests/Unit/CohortUserExpiryTest.php
@@ -43,6 +43,7 @@ class CohortUserExpiryTest extends TestCase
 
     public function test_it_can_expire_requests(): void
     {
+        Mail::fake();
         app()->make('config')->set('mail.driver', 'log');
 
         $req = CohortRequest::create([
@@ -89,8 +90,9 @@ class CohortUserExpiryTest extends TestCase
 
     public function test_it_doesnt_expire_valid_requests(): void
     {
+        Mail::fake();
         app()->make('config')->set('mail.driver', 'log');
-        
+
         $req = CohortRequest::create([
             'user_id' => 1,
             'request_status' => 'APPROVED',
