ceab772184f2e495968553776527f6daa8629051
Dan Nita <dan.nita@hdruk.ac.uk>
2024-04-04 11:24:08 +0100

    update

--- tests/Traits/MockExternalApis.php
+++ tests/Traits/MockExternalApis.php
@@ -670,7 +670,7 @@ trait MockExternalApis
         // makePartial so other MMC methods are not mocked
         MMC::shouldReceive('getElasticClient')->andReturn($this->testElasticClient);
         MMC::shouldReceive("translateDataModelType")
-            ->with($metadata, Config::get('metadata.GWDM.name'), Config::get('metadata.GWDM.version'))
+            ->with('', Config::get('metadata.GWDM.name'), Config::get('metadata.GWDM.version'))
             ->andReturnUsing(function(string $metadata){
             return [
                 "traser_message" => "",
