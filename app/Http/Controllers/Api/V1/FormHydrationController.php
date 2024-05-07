b52b43f8353774d151d40df0adc2b64dcabb61d7
Loki Sinclair <loki.sinclair@hdruk.ac.uk>
2024-05-07 15:57:59 +0100

    missed swagger annotation for new route

--- app/Http/Controllers/Api/V1/FormHydrationController.php
+++ app/Http/Controllers/Api/V1/FormHydrationController.php
@@ -75,6 +75,48 @@ class FormHydrationController extends Controller
 
     }
 
+    /**
+     * @OA\Get(
+     *      path="/api/v1/form_hydration",
+     *      operationId="onboardingFormHydration",
+     *      tags={"Form Hydration"},
+     *      summary="Retrieve form schema data",
+     *      description="Retrieves form schema data based on the provided model and version.",
+     *      @OA\Parameter(
+     *          name="name",
+     *          in="query",
+     *          required=false,
+     *          description="The model name for which form schema is requested.",
+     *          @OA\Schema(
+     *              type="string"
+     *          )
+     *      ),
+     *      @OA\Parameter(
+     *          name="version",
+     *          in="query",
+     *          required=false,
+     *          description="The version of the model for which form schema is requested.",
+     *          @OA\Schema(
+     *              type="string"
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="Successful operation",
+     *          @OA\JsonContent(
+     *              type="object"
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response=400,
+     *          description="Bad request. Missing required parameters or invalid parameters."
+     *      ),
+     *      @OA\Response(
+     *          response=500,
+     *          description="Internal server error. Failed to retrieve form schema data."
+     *      )
+     * )
+     */    
     public function onboardingFormHydration(Request $request): JsonResponse
     {
         $model = $request->input('model',Config::get('form_hydration.schema.model'));
