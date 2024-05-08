2fcdfc119ea14861d7f05c9ee343060a424345c9
Dan Nita <dan.nita@hdruk.ac.uk>
2024-05-08 10:18:58 +0100

    update

--- app/Http/Enums/SortOrderSavedSearch.php
+++ app/Http/Enums/SortOrderSavedSearch.php
@@ -4,9 +4,9 @@ namespace App\Http\Enums;
 
 enum SortOrderSavedSearch: string
 {
-    case MOST_RELEVANT = 'most_relevant';
-    case SORT_TITLE_ASC = 'title:asc';
-    case SORT_TITLE_DESC = 'title:desc';
-    case MOST_RECENTLY_UPDATED = 'updated_at:desc';
-    case LEAST_RECENTLY_UPDATED = 'updated_at:asc';
+    case MOST_RELEVANT = 'score';
+    case SORT_TITLE_ASC = 'title_asc';
+    case SORT_TITLE_DESC = 'title_desc';
+    case MOST_RECENTLY_UPDATED = 'updated_at_desc';
+    case LEAST_RECENTLY_UPDATED = 'updated_at_asc';
 }
