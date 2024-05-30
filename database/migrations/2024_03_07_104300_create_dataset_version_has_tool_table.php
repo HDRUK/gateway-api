961d87b3015a914e51fed907b779b0a76c32624e
tom.giles <tom.giles@nottingham.ac.uk>
2024-05-30 14:56:01 +0100

    Migrated LinkedDataset to DatasetVersionHasDatasetVersion

--- database/migrations/2024_03_07_104300_create_dataset_version_has_tool_table.php
+++ database/migrations/2024_03_07_104300_create_dataset_version_has_tool_table.php
@@ -11,12 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('dataset_has_tools', function (Blueprint $table) {
-            $table->bigInteger('dataset_id')->unsigned();
+        Schema::create('dataset_version_has_tools', function (Blueprint $table) {
+            $table->bigInteger('dataset_version_id')->unsigned();
             $table->bigInteger('tool_id')->unsigned();
 
-            $table->primary(['dataset_id', 'tool_id']);
-            $table->foreign('dataset_id')->references('id')->on('datasets')->onDelete('cascade');
+            $table->primary(['dataset_version_id', 'tool_id']);
+            $table->foreign('dataset_version_id')->references('id')->on('dataset_versions')->onDelete('cascade');
             $table->foreign('tool_id')->references('id')->on('tools')->onDelete('cascade');
 
             $table->timestamps();
@@ -28,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('dataset_has_tools');
+        Schema::dropIfExists('dataset_version_has_tools');
     }
 };
