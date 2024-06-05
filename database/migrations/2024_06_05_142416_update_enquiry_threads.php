ab235847d478d035184231ecca3951b30ee0e9bf
Calum Macdonald <calmacx@gmail.com>
2024-06-05 15:25:54 +0100

    adding new fields to enquiry threads

---
+++ database/migrations/2024_06_05_142416_update_enquiry_threads.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('enquiry_threads', function (Blueprint $table) {
+            $table->boolean('is_dar_dialogue')->default(false)->after('unique_key'); 
+            $table->boolean('is_dar_status')->default(false)->after('is_dar_dialogue');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('enquiry_threads', function (Blueprint $table) {
+            $table->dropColumn('is_dar_dialogue');
+            $table->dropColumn('is_dar_status');
+        });
+    }
+};
