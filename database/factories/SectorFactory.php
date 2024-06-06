e6b0e6f9a49f85357d391ce2fa7c49386fc12565
tom.giles <tom.giles@nottingham.ac.uk>
2024-06-06 14:53:59 +0100

    Added functions, Corrected LicenceFactory

--- database/factories/SectorFactory.php
+++ database/factories/SectorFactory.php
@@ -5,9 +5,9 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Sectors>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Sector>
  */
-class SectorsFactory extends Factory
+class SectorFactory extends Factory
 {
     /**
      * Define the model's default state.
