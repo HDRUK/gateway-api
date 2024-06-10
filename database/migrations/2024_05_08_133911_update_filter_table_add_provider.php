5f016903b493bf5f404d0e524905e3b85e7af574
Dan Nita <dan.nita@hdruk.ac.uk>
2024-06-10 20:03:39 +0100

    update

--- database/migrations/2024_05_08_133911_update_filter_table_add_provider.php
+++ database/migrations/2024_05_08_133911_update_filter_table_add_provider.php
@@ -34,6 +34,15 @@ return new class extends Migration
      */
     public function down(): void
     {
+        Schema::disableForeignKeyConstraints();
+        
+        $foreignKeys = $this->listTableForeignKeys('filters');
+        
+        if(in_array('filters_type_keys_unique', $foreignKeys)) {
+            DB::statement('ALTER TABLE `filters` DROP FOREIGN KEY `filters_type_keys_unique`');
+            DB::statement('ALTER TABLE `filters` DROP KEY `filters_type_keys_unique`');
+        }
+
         Schema::table('filters', function (Blueprint $table) {
             $table->dropUnique(['type', 'keys']);
             $table->enum('type_old', [
@@ -55,7 +64,17 @@ return new class extends Migration
 
         Schema::table('filters', function (Blueprint $table) {
             $table->renameColumn('type_old', 'type');
-            $table->unique(['type', 'keys']);
         }); 
+
+        Schema::enableForeignKeyConstraints();
+    }
+
+    public function listTableForeignKeys($table)
+    {
+        $conn = Schema::getConnection()->getDoctrineSchemaManager();
+
+        return array_map(function($key) {
+            return $key->getName();
+        }, $conn->listTableForeignKeys($table));
     }
 };
