fe3cee94551bc6adb3a8b91a4d92fe0cd0a88f4d
Dan Nita <dan.nita@hdruk.ac.uk>
2024-06-10 20:20:35 +0100

    update

--- database/migrations/2024_02_26_093354_add_unique_on_fields_to_filters_table.php
+++ database/migrations/2024_02_26_093354_add_unique_on_fields_to_filters_table.php
@@ -21,8 +21,24 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('filters', function (Blueprint $table) {
-            $table->dropUnique(['type', 'keys']);
-        });
+        Schema::disableForeignKeyConstraints();
+
+        $foreignKeys = $this->listTableForeignKeys('filters');
+
+        if(in_array('filters_type_keys_unique', $foreignKeys)) {
+            DB::statement('ALTER TABLE `filters` DROP FOREIGN KEY `filters_type_keys_unique`');
+            DB::statement('ALTER TABLE `filters` DROP KEY `filters_type_keys_unique`');
+        }
+
+        Schema::enableForeignKeyConstraints();
+    }
+
+    public function listTableForeignKeys($table)
+    {
+        $conn = Schema::getConnection()->getDoctrineSchemaManager();
+
+        return array_map(function($key) {
+            return $key->getName();
+        }, $conn->listTableForeignKeys($table));
     }
 };
