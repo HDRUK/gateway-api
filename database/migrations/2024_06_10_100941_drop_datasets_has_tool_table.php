29962e121c30a96eaf658c923aac9a482a7c5ea9
tom.giles <tom.giles@nottingham.ac.uk>
2024-06-12 14:27:01 +0100

    Hotfix

--- database/migrations/2024_06_10_100941_drop_datasets_has_tool_table.php
+++ database/migrations/2024_06_10_100941_drop_datasets_has_tool_table.php
@@ -25,14 +25,12 @@ class DropDatasetsHasToolTable extends Migration
      */
     public function down()
     {
-        Schema::create('dataset_has_tools', function (Blueprint $table) {
+        Schema::create('datasets_has_tool', function (Blueprint $table) {
             $table->bigInteger('dataset_id')->unsigned();
             $table->bigInteger('tool_id')->unsigned();
-
             $table->primary(['dataset_id', 'tool_id']);
             $table->foreign('dataset_id')->references('id')->on('datasets')->onDelete('cascade');
             $table->foreign('tool_id')->references('id')->on('tools')->onDelete('cascade');
-
             $table->timestamps();
         });
     }
