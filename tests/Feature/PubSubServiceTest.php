8586fcc9f724f6b1ea6aa3c2579c20c7fce4415f
Dan Nita <dan.nita@hdruk.ac.uk>
2024-06-18 13:33:21 +0100

    update

--- tests/Feature/PubSubServiceTest.php
+++ tests/Feature/PubSubServiceTest.php
@@ -4,15 +4,13 @@ namespace Tests\Feature;
 
 use Mockery;
 use App\Services\PubSubService;
-// use Config;
 use Google\Cloud\PubSub\PubSubClient;
 use Google\Cloud\PubSub\Topic;
 use Google\Cloud\PubSub\Message;
 use Tests\TestCase;
 use Illuminate\Support\Facades\Config;
 use Tests\CreatesApplication;
-// use Config;
-// use Google\Cloud\PubSub\MessageBuilder;
+
 
 class PubSubServiceTest extends TestCase
 {
@@ -22,7 +20,6 @@ class PubSubServiceTest extends TestCase
     {
         parent::setUp();
 
-        // Mock the Config facade
         Config::shouldReceive('get')
             ->with('services.googlepubsub.project_id')
             ->andReturn('test-project-id');
@@ -38,7 +35,6 @@ class PubSubServiceTest extends TestCase
     {
         $data = ['message' => 'test message'];
 
-        // Mock the PubSubClient and Topic classes
         $pubSubClientMock = $this->createMock(PubSubClient::class);
         $topicMock = $this->createMock(Topic::class);
 
@@ -54,10 +50,8 @@ class PubSubServiceTest extends TestCase
                 return $messageData === $data;
             }));
 
-        // Set the mocked PubSubClient in the service
         $this->pubSubService->setPubSubClient($pubSubClientMock);
 
-        // Call the publishMessage method with the test data
         $this->pubSubService->publishMessage($data);
     }
 
