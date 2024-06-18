8d60cf58815a40f9b3a4e3e3f61c2ec6239251f2
Dan Nita <dan.nita@hdruk.ac.uk>
2024-06-18 10:01:46 +0100

    update

--- app/Services/PubSubService.php
+++ app/Services/PubSubService.php
@@ -3,6 +3,7 @@ namespace App\Services;
 
 use Config;
 use Google\Cloud\PubSub\PubSubClient;
+use Google\Cloud\PubSub\MessageBuilder;
 
 class PubSubService
 {
@@ -17,11 +18,13 @@ class PubSubService
 
     public function publishMessage(array $data)
     {
-        $out = new \Symfony\Component\Console\Output\ConsoleOutput();
-        $out->writeln(json_encode($data));
+        // $out = new \Symfony\Component\Console\Output\ConsoleOutput();
+        // $out->writeln(json_encode($data));
+        // $topic = $this->pubSubClient->topic(Config::get('services.googlepubsub.pubsub_topic'));
+        // $result = $topic->publish(json_encode($data));
+        // $out->writeln(json_encode($result->result));
         $topic = $this->pubSubClient->topic(Config::get('services.googlepubsub.pubsub_topic'));
-        $result = $topic->publish(json_encode($data));
-        $out->writeln(json_encode($result->result));
-
+        // $topic->publish(['data' => json_encode($data)]);
+        $topic->publish((new MessageBuilder)->setData(json_encode($data))->build());
     }
 }
