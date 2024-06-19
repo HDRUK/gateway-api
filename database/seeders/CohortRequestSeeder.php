73af01e3c0ec11e7b9ee2dd51281d069352695fb
tom.giles <tom.giles@nottingham.ac.uk>
2024-06-19 16:19:25 +0100

    Update CRS

--- database/seeders/CohortRequestSeeder.php
+++ database/seeders/CohortRequestSeeder.php
@@ -1,14 +1,14 @@
 <?php
-
 namespace Database\Seeders;
-
 use App\Models\User;
 use App\Models\Permission;
 use App\Models\CohortRequest;
 use App\Models\CohortRequestHasLog;
 use App\Models\CohortRequestHasPermission;
 use App\Models\CohortRequestLog;
+use Illuminate\Support\Carbon;
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class CohortRequestSeeder extends Seeder
 {
@@ -19,12 +19,11 @@ class CohortRequestSeeder extends Seeder
     {
         for ($count = 1; $count <= 50; $count++) {
             $userId = User::all()->random()->id;
-
             $checkRequestByUserId = CohortRequest::where([
                 'user_id' => $userId,
             ])->first();
 
-            $status = fake()->randomElement(['PENDING', 'APPROVED', 'REJECTED', 'BANNED','SUSPENDED','EXPIRED']);
+            $status = fake()->randomElement(['PENDING', 'APPROVED']);
 
             if (!$checkRequestByUserId) {
                 $cohortRequest = CohortRequest::create([
@@ -33,7 +32,7 @@ class CohortRequestSeeder extends Seeder
                     'request_status' => $status,
                     'cohort_status' => ($status === 'PENDING' ? false : true),
                     'request_expire_at' => null,
-                    'accept_declaration' => ($status === 'APPROVED' ? true : false), // Simulating the response from the front
+                    'accept_declaration' => ($status === 'APPROVED' ? true : false),
                 ]);
 
                 if ($status === 'APPROVED') {
@@ -46,38 +45,15 @@ class CohortRequestSeeder extends Seeder
                         'permission_id' => $permissions->id,
                     ]);
                 }
-
                 $cohortRequestLog = CohortRequestLog::create([
                     'user_id' => $userId,
                     'details' => htmlentities(implode(" ", fake()->paragraphs(5, false)), ENT_QUOTES | ENT_IGNORE, "UTF-8"),
                     'request_status' => $status,
                 ]);
-
                 CohortRequestHasLog::create([
                     'cohort_request_id' => $cohortRequest->id,
                     'cohort_request_log_id' => $cohortRequestLog->id,
                 ]);
-            } else {
-                // Handle status change logic here
-                $newStatus = fake()->randomElement(['REJECTED', 'BANNED', 'SUSPENDED', 'EXPIRED']);
-                if (in_array($checkRequestByUserId->request_status, ['APPROVED']) && in_array($newStatus, ['REJECTED', 'BANNED', 'SUSPENDED', 'EXPIRED'])) {
-                    $checkRequestByUserId->update([
-                        'request_status' => $newStatus,
-                        'accept_declaration' => false,
-                    ]);
-
-                    // Log the status change
-                    $cohortRequestLog = CohortRequestLog::create([
-                        'user_id' => $userId,
-                        'details' => htmlentities(implode(" ", fake()->paragraphs(5, false)), ENT_QUOTES | ENT_IGNORE, "UTF-8"),
-                        'request_status' => $newStatus,
-                    ]);
-
-                    CohortRequestHasLog::create([
-                        'cohort_request_id' => $checkRequestByUserId->id,
-                        'cohort_request_log_id' => $cohortRequestLog->id,
-                    ]);
-                }
             }
         }
     }
