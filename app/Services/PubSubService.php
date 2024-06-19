6f462fc0acf5b8464a9952398b0e87774f4e8ab4
Dan Nita <dan.nita@hdruk.ac.uk>
2024-06-19 09:44:48 +0100

    update

--- app/Services/PubSubService.php
+++ app/Services/PubSubService.php
@@ -19,7 +19,7 @@ class PubSubService
     public function publishMessage(array $data)
     {
         \Log::info(json_encode(['$this->pubSubClient', $this->pubSubClient]));
-        $topic = $this->pubSubClient->topic(Config::get('services.googlepubsub.pubsub_topic', 'test-topic'));
+        $topic = $this->pubSubClient->topic(Config::get('services.googlepubsub.pubsub_topic'));
         \Log::info(json_encode(['$topic', $topic]));
         $publish = $topic->publish((new MessageBuilder)->setData(json_encode($data))->build());
         \Log::info(json_encode(['$publish', $publish]));
