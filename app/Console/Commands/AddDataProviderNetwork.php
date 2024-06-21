db910237079cc405c74887380a7077016b81bcb4
Dan Nita <dan.nita@hdruk.ac.uk>
2024-06-21 11:45:42 +0100

    update

--- app/Console/Commands/AddDataProviderNetwork.php
+++ app/Console/Commands/AddDataProviderNetwork.php
@@ -4,6 +4,7 @@ namespace App\Console\Commands;
 
 use App\Models\DataProviderColl;
 use App\Models\DataProviderCollHasTeam;
+use App\Models\Team;
 use Illuminate\Console\Command;
 
 class AddDataProviderNetwork extends Command
@@ -45,36 +46,77 @@ class AddDataProviderNetwork extends Command
 
         $csvData = $this->csvData;
 
-        dd($csvData);
+        if ($askDataProviderNetwork === 'all') {
+            foreach ($csvData as $item) {
+                $teamName = strtoupper(trim($item['Data provider/Team']));
+                $dataProviderName = strtoupper(trim($item['Data provider network']));
 
-        if ($csv === 'all') {
-            foreach ($csvData as $key => $value) {
-                // check in teams
-
-                // check if exists DataProviderNetwork
-
-                // check if DataProviderNetwork & Team exists
-
-                // create new DataProviderNetwork
-
-                // add DataProviderNetwork with Team
+                $return = $this->dataProviderNetworkTeam($dataProviderName, $teamName);
+                if (!$return) {
+                    continue;
+                }
             }
         }
 
-        if ($csv !== 'all') {
-            // check in teams
+        if ($askDataProviderNetwork !== 'all') {
+            $inputDataProviderName = strtoupper(trim($askDataProviderNetwork));
 
-            // check if exists DataProviderNetwork
+            foreach ($csvData as $item) {
+                $teamName = strtoupper(trim($item['Data provider/Team']));
+                $dataProviderName = strtoupper(trim($item['Data provider network']));
 
-            // check if DataProviderNetwork & Team exists
+                if ($inputDataProviderName !== $dataProviderName) {
+                    echo 'Found Data Provider Network name ' . $dataProviderName . '. skipping ...' . PHP_EOL;
+                    continue;
+                }
 
-            // create new DataProviderNetwork
+                $return = $this->dataProviderNetworkTeam($dataProviderName, $teamName);
+                if (!$return) {
+                    continue;
+                }
+            }
+        }
+    }
 
-            // add DataProviderNetwork with Team
+    private function dataProviderNetworkTeam(string $dataProviderNetworkName, string $teamName): bool
+    {
+        // check in teams
+        $team = Team::where('name', $teamName)->first();
+        if (!$team) {
+            echo 'Team ' . $teamName . ' not found. skipping ...' . PHP_EOL;
+            return false;
         }
 
+        // check if exists DataProviderNetwork and/or create one
+        $dataProviderNetwork = DataProviderColl::where('name', $dataProviderNetworkName)->withTrashed()->first();
+        if (!$dataProviderNetwork) {
+            $dataProviderNetwork = DataProviderColl::create([
+                'name' => $dataProviderNetworkName,
+                'enabled' => 1,
+                'img_url' => NULL,
+            ]);
 
-        dd($askDataProviderNetwork);
+            echo 'Data Provider network with name ' . $dataProviderNetworkName . ' was created.' . PHP_EOL;
+        }
+
+        // check if DataProviderNetwork & Team exists
+        $dataProviderNetworkTeam = DataProviderCollHasTeam::where([
+            'data_provider_coll_id' => (int) $dataProviderNetwork->id,
+            'team_id' => (int) $team->id,
+        ])->first();
+        if ($dataProviderNetworkTeam) {
+            echo 'The team ' . $teamName . ' found in relation with ' . $dataProviderNetworkName . '. skipping ...' . PHP_EOL;
+            return false;
+        }
+
+        // add DataProviderNetwork with Team
+        $createDataProviderNetworkTeam = DataProviderCollHasTeam::create([
+            'data_provider_coll_id' => (int) $dataProviderNetwork->id,
+            'team_id' => (int) $team->id,
+        ]);
+        echo 'Was created relation between team (' . $teamName . ') and data provider network (' . $dataProviderNetworkName . '). ID: ' . $createDataProviderNetworkTeam->id . PHP_EOL;
+
+        return true;
     }
 
     private function readMigrationFile(string $migrationFile): void
