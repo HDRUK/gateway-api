d279774f722b9b597d8d883dd5deb45d5afd4d8a
Sam Cox <sam.cox@nottingham.ac.uk>
2024-06-28 13:09:36 +0100

    Add publication_type_mk1 column to hold the migrated publication types. The existing publication_type column will now be used for controlled list.

---
+++ database/migrations/2024_06_28_124705_add_publication_type_mk1_column.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('publications', function (Blueprint $table) {
+            $table->string('publication_type_mk1', 256);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('publications', function (Blueprint $table) {
+            $table->dropColumn('publications_mk1');
+        });
+    }
+};
