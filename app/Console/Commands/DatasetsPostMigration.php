d1f580f6ce507a81f9899f60ca2061d48ad75db4
Calum Macdonald <calmacx@gmail.com>
2024-07-01 16:44:07 +0100

    some changes to get things working

--- app/Console/Commands/DatasetsPostMigration.php
+++ app/Console/Commands/DatasetsPostMigration.php
@@ -16,8 +16,6 @@ class DatasetsPostMigration extends Command
      */
     protected $signature = 'app:datasets-post-migration {reindex?}';
 
-    private $csvData = [];
-
     /**
      * The console command description.
      *
@@ -28,7 +26,6 @@ class DatasetsPostMigration extends Command
     public function __construct()
     {
         parent::__construct();
-        $this->csvData = $this->readMigrationFile(storage_path() . '/migration_files/mapped_mk1_access_service_category.csv');
     }
 
     /**
@@ -39,10 +36,31 @@ class DatasetsPostMigration extends Command
         $reindex = $this->argument('reindex');
         $reindexEnabled = $reindex !== null;
 
-        $progressbar = $this->output->createProgressBar(count($this->csvData));
+    
+        //$this->migrateAccessServiceCategory();
+        $this->curateDatasets();
+
+        /*
+        if ($reindexEnabled) {
+            MMC::reindexElastic($dataset->id);
+        }   
+        */
+
+    }
+
+    /**
+     * Execute the console command.
+     */
+    private function migrateAccessServiceCategory()
+    {
+
+        $csvData = $this->readMigrationFile(storage_path() . '/migration_files/mapped_mk1_access_service_category.csv');
+
+        
+        $progressbar = $this->output->createProgressBar(count($csvData));
         $progressbar->start();
 
-        foreach ($this->csvData as $csv) {
+        foreach ($csvData as $csv) {
             $mongoPid = $csv['pid'];
             $mapped = $csv['Mapped'];
             $dataset = Dataset::where([
@@ -64,18 +82,64 @@ class DatasetsPostMigration extends Command
                     DatasetVersion::where('id', $dataset->id)->update([
                         'metadata' => json_encode(json_encode($metadata)),
                     ]);
-                }
-
-                if ($reindexEnabled) {
-                    MMC::reindexElastic($dataset->id);
-                }                
+                }             
             }
             $progressbar->advance();
-            sleep(1); // to not kill ElasticSearch
         }
 
         $progressbar->finish();
     }
+ 
+    private function curateDatasets()
+    {
+
+        $csvData = $this->readMigrationFile(storage_path() . '/migration_files/datasets_curation_july2024.csv');
+               
+        $progressbar = $this->output->createProgressBar(count($csvData));
+        $progressbar->start();
+
+        foreach ($csvData as $csv) {
+            $datasetid = $csv['id'];
+        
+            $dataset = Dataset::where([
+                'datasetid' => $datasetid,
+            ])->first();
+
+            if (!$dataset) {
+                $this->warn("Not found datasetid=". $datasetid);
+                continue;
+            }
+            $datasetVersion = DatasetVersion::where([
+                'id' => $dataset->id,
+            ])->first();
+
+            if (!$datasetVersion) {
+                $this->error("cannot find a datasetverison associated with this dataset");
+                continue;
+            }
+            $metadata = $datasetVersion->metadata['metadata'];
+
+            $doiName = $csv['doiName'];
+            $datasetType = $csv['Mapped Data type'];
+            $datasetSubType = $csv['Mapped Data sub type'];
+
+            $associatedMedia = $csv['Annotation note link'];
+            $toolLink = $csv['Github tool link'];
+            $syntheticDataWebLink = $csv['Synthetic dataset link'];
+
+            if($doiName){
+                $this->info($metadata['summary']['doiName']);
+                $this->warn($doiName);
+                $this->info("...Updating doiName");
+            }
+
+            $progressbar->advance();
+
+        }
+        $progressbar->finish();
+
+    }
+
 
     private function readMigrationFile(string $migrationFile): array
     {
