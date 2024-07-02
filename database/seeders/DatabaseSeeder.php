0b125a099bbfccdabe01e3772acea11cdb0c8e46
Calum Macdonald <calmacx@gmail.com>
2024-07-02 15:00:10 +0100

    fix test

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,9 +14,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
 
-        $this->call(BaseDatabaseSeeder::class);
-
         $this->call([
+            BaseDatabaseSeeder::class,
             TagSeeder::class,
             FeatureSeeder::class,
             DarIntegrationSeeder::class,
