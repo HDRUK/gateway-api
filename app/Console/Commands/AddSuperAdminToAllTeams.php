57db2c1eda9aab9595a38333fb0e61e48b49c98e
Calum Macdonald <calmacx@gmail.com>
2024-07-03 09:30:58 +0100

    add superadmin add

---
+++ app/Console/Commands/AddSuperAdminToAllTeams.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Config;
+use App\Models\User;
+use App\Models\Team;
+use App\Models\TeamHasUser;
+use Illuminate\Console\Command;
+
+class AddSuperAdminToAllTeams extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:add-super-admin-to-all-teams';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Make sure the superadmin is a member of all teams';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $teamIds = Team::pluck("id");
+        foreach($teamIds as $teamId){
+            $superAdminIds = User::where("is_admin",true)->pluck('id');
+            foreach($superAdminIds as $adminId){
+                TeamHasUser::updateOrCreate(
+                    ['team_id' => $teamId, 'user_id' => $adminId],
+                    [] 
+                );
+            }
+        }
+    }
+}
