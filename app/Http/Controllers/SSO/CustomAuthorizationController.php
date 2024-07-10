fbd61351791729bbe751ccd9982fea56d95e2542
Dan Nita <dan.nita@hdruk.ac.uk>
2024-07-10 13:48:08 +0100

    update

---
+++ app/Http/Controllers/SSO/CustomAuthorizationController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\SSO;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class CustomAuthorizationController extends Controller
+{
+    public function customAuthorize(Request $request)
+    {
+        // logic 
+        
+        return;
+    }
+}
