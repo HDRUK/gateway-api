ec5816b9539b02c22ea8f3d36facc148ac9f19d5
tom.giles <tom.giles@nottingham.ac.uk>
2024-07-11 12:37:00 +0100

    MinorHotfix to remove redundant table from model

--- database/migrations/2024_06_10_100941_drop_datasets_has_tool_table.php
+++ database/migrations/2024_06_10_100941_drop_datasets_has_tool_table.php
@@ -14,7 +14,7 @@ class DropDatasetsHasToolTable extends Migration
     public function up()
     {
         Schema::disableForeignKeyConstraints();
-        Schema::dropIfExists('datasets_has_tool');
+        Schema::drop('datasets_has_tool');
         Schema::enableForeignKeyConstraints();
     }
 
