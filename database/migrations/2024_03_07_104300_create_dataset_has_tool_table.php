fb674fb745d506e902a006c3305b6972e7f15c00
tom.giles <tom.giles@nottingham.ac.uk>
2024-07-11 12:43:01 +0100

    hotfix for dataset_has_tools

--- database/migrations/2024_03_07_104300_create_dataset_has_tool_table.php
+++ database/migrations/2024_03_07_104300_create_dataset_has_tool_table.php
@@ -27,7 +27,7 @@ return new class extends Migration
     public function down(): void
     {
         // Drop the foreign key constraint first
-        Schema::table('datasets_has_tool', function (Blueprint $table) {
+        Schema::table('dataset_has_tools', function (Blueprint $table) {
             $table->dropForeign(['dataset_id']);
             $table->dropForeign(['tool_id']);
         });
