0b2404c22401c8cfb10f89d15aed77c00591622b
Dan Nita <dan.nita@hdruk.ac.uk>
2024-07-15 16:00:29 +0100

    update

--- app/Models/Tool.php
+++ app/Models/Tool.php
@@ -60,7 +60,7 @@ class Tool extends Model
 
     public function user(): BelongsTo
     {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(User::class)->select(['id', 'firstname', 'lastname']);
     }
 
     public function tag(): BelongsToMany
