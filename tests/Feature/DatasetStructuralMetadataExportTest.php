a58909ad68865498b0862770bd77ce4194446b41
Dan Nita <dan.nita@hdruk.ac.uk>
2024-07-16 14:37:16 +0100

    update

--- tests/Feature/DatasetStructuralMetadataExportTest.php
+++ tests/Feature/DatasetStructuralMetadataExportTest.php
@@ -2,7 +2,6 @@
 
 namespace Tests\Feature;
 
-use Config;
 use Tests\TestCase;
 use Illuminate\Support\Arr;
 use Tests\Traits\Authorization;
@@ -13,25 +12,21 @@ use Illuminate\Foundation\Testing\WithFaker;
 use App\Exports\DatasetStructuralMetadataExport;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
+
 class DatasetStructuralMetadataExportTest extends TestCase
 {
-    use RefreshDatabase;
-    use MockExternalApis {
-        setUp as commonSetUp;
-    }
+    use RefreshDatabase, MockExternalApis;
 
     protected $testMetadata;
 
     public function setUp(): void
     {
-        $this->testMetadata = json_decode($this->getMetadata(), true);
+        $this->testMetadata = $this->getMetadata();
     }
 
     public function test_generates_excel_dataset_structural_metadata_download_type_table(): void
     {
         Storage::fake('local');
-        var_dump($this->testMetadata);
-        exit();
 
         $export = Arr::has($this->testMetadata, 'versions.0.metadata.metadata.structuralMetadata') ? 
             $this->testMetadata['versions'][0]['metadata']['metadata']['structuralMetadata'] : 
