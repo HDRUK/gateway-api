04ffcc2931c5468a8be57b38f2750d4883d58f11
Dan Nita <dan.nita@hdruk.ac.uk>
2024-07-19 10:39:27 +0100

    update

--- tests/Feature/CloudPubSubServiceTest.php
+++ tests/Feature/CloudPubSubServiceTest.php
@@ -6,6 +6,7 @@ use Config;
 use Mockery;
 use Tests\TestCase;
 use Google\Cloud\PubSub\Topic;
+use Google\Cloud\PubSub\Message;
 use App\Services\CloudPubSubService;
 use Google\Cloud\PubSub\PubSubClient;
 use Google\Cloud\PubSub\MessageBuilder;
@@ -14,45 +15,54 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class CloudPubSubServiceTest extends TestCase
 {
-    public function test_publish_message()
+    protected $pubSubClientMock;
+    protected $topicMock;
+    protected $messageMock;
+    protected $cloudPubSubService;
+
+    protected function setUp(): void
     {
-        Config::set('services.googlepubsub.project_id', 'fake-project-id');
-        Config::set('services.googlepubsub.pubsub_topic', 'fake-topic-name');
-        Config::set('services.googlepubsub.enabled', true);
+        parent::setUp();
 
-        $data = ['test' => 'Test message'];
-        $messageId = 'message-id';
+        $this->pubSubClientMock = $this->createMock(PubSubClient::class);
+        $this->topicMock = $this->createMock(Topic::class);
+        $this->messageMock = $this->createMock(Message::class);
 
-        // Mock the Topic class
-        $topic = Mockery::mock(Topic::class);
-        $topic->shouldReceive('publish')
-            ->once()
-            ->with(Mockery::on(function ($message) {
-                $messageBuilder = new MessageBuilder();
-                $expectedMessage = $messageBuilder->setData(json_encode(['test' => 'Test message']))->build();
-                return $message == $expectedMessage;
+        Config::shouldReceive('get')
+            ->with('services.googlepubsub.project_id')
+            ->andReturn('test-project-id');
+        
+        Config::shouldReceive('get')
+            ->with('services.googlepubsub.enabled')
+            ->andReturn(true);
+        
+        Config::shouldReceive('get')
+            ->with('services.googlepubsub.pubsub_topic')
+            ->andReturn('test-topic');
+
+        $this->cloudPubSubService = new CloudPubSubService();
+        $this->cloudPubSubService->setPubSubClient($this->pubSubClientMock);
+    }
+
+    public function testPublishMessage()
+    {
+        $data = ['message' => 'test'];
+
+        $this->pubSubClientMock->expects($this->once())
+            ->method('topic')
+            ->with('test-topic')
+            ->willReturn($this->topicMock);
+
+        $this->topicMock->expects($this->once())
+            ->method('publish')
+            ->with($this->callback(function ($message) {
+                $data = json_decode($message->data(), true);
+                return $data['message'] === 'test';
             }))
-            ->andReturn($messageId);
+            ->willReturn(true);
 
-        // Mock the PubSubClient and make it return the mocked Topic
-        $pubSubClient = Mockery::mock(PubSubClient::class);
-        $pubSubClient->shouldReceive('topic')
-            ->once()
-            ->with('fake-topic-name')
-            ->andReturn($topic);
+        $result = $this->cloudPubSubService->publishMessage($data);
 
-        // Use reflection to inject the mock into CloudPubSubService
-        $service = new CloudPubSubService();
-        $reflection = new \ReflectionClass($service);
-
-        $pubSubClientProperty = $reflection->getProperty('pubSubClient');
-        $pubSubClientProperty->setAccessible(true);
-        $pubSubClientProperty->setValue($service, $pubSubClient);
-
-        $result = $service->publishMessage($data);
-
-        $this->assertEquals($messageId, $result);
-
-        Config::set('services.googlepubsub.enabled', false);
+        $this->assertTrue($result);
     }
 }
