dfc8861618e9253245af3af265e11a3927d99597
Calum Macdonald <calmacx@gmail.com>
2024-07-30 14:06:15 +0100

    make sure jwt found

--- app/Http/Middleware/CheckUserIdMatches.php
+++ app/Http/Middleware/CheckUserIdMatches.php
@@ -17,12 +17,15 @@ class CheckUserIdMatches
     public function handle($request, Closure $next)
     {
         $input = $request->all();
+        if (!isset($input['jwt_user'])) {
+            return response()->json(['message' => 'Unauthorized: User not found'], 401);
+        }
         $user = $input['jwt_user'];
         $jwtUserIsAdmin = $user['is_admin'];
 
         $routeId = $request->route('id');
 
-        if ($user && ($user['id'] == $routeId || $jwtUserIsAdmin)) {
+        if ($user['id'] == $routeId || $jwtUserIsAdmin) {
             return $next($request);
         }
 
