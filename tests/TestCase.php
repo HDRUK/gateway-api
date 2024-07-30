3d9a647c1f5cb302578a400710103179ed3fcf46
Branwen Snelling <branwen.snelling@hdruk.ac.uk>
2024-07-30 10:03:42 +0100

    debugging memory issue in tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,4 +7,10 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    public function tearDown(): void
+    {
+        parent::tearDown();
+        echo memory_get_usage() . PHP_EOL;
+    }
 }
