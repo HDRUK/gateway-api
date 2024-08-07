9b360b2cfaed924a217ad382f804f320c948129d
tom.giles <tom.giles@nottingham.ac.uk>
2024-08-07 15:32:16 +0100

    dfs

--- app/Console/Commands/ReindexEntities.php
+++ app/Console/Commands/ReindexEntities.php
@@ -116,7 +116,7 @@ class ReindexEntities extends Command
             $datasetIds = array_slice($datasetIds, 0, $maxIndex + 1);
         }
 
-        $termExtraction = $this->option('term-extraction'); // Initialize $termExtraction based on the command optio
+        $termExtraction = $this->option('term-extraction'); // Initialize $termExtraction based on the command option
 
         $progressbar = $this->output->createProgressBar(count($datasetIds));
         foreach ($datasetIds as $id) {
