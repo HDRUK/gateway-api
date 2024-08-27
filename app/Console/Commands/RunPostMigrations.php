d18b4568f5a365a26566ef8a7cc35105fb25734f
Jamie Byrne <jamie.byrne@hdruk.ac.uk>
2024-08-27 14:11:21 +0100

    PostMigration images

--- app/Console/Commands/RunPostMigrations.php
+++ app/Console/Commands/RunPostMigrations.php
@@ -73,6 +73,10 @@ class RunPostMigrations extends Command
                 'arguments' => [],
             ], // dataset linkage
             [
+                'command' => 'app:upload-images-post-migration-process',
+                'arguments' => [],
+            ], // uploaded images
+            [
                 'command' => 'app:reindex-entities',
                 'arguments' => [
                     'entity' => 'datasets',
---
+++ app/Console/Commands/UploadImagesPostMigrationProcess.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Exception;
+
+use App\Models\Upload;
+
+use Illuminate\Console\Command;
+
+
+class ImagesPostMigrationProcess extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:images-post-migration-process';
+
+    /**
+     * The file of migration mappings translated to CSV array
+     *
+     * @var array
+     */
+    private $csvData = [];
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'CLI command to post-process migrated uploaded images from mk1 mongo db.';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $this->readMigrationFile(storage_path() . '/migration_files/collection_images.csv');
+
+        $superUserID = 1
+        $status = 'PROCESSED'
+
+        // Traverse the CSV data and update migrations accordingly
+        foreach ($this->csvData as $csv) {
+            try {
+                $pID = $csv['PID'];
+                $action = $csv['action'];
+                $fileLoc = $csv['file_loc'];
+                $newFileName = $csv['New File Name'];
+                
+
+                if ($action == 'null') {
+                    $fileLoc = NULL
+                }
+                $upload = Upload::updateOrCreate([
+                'id' => (int) $pID,
+                'filename' => $newFileName,
+                'file_location' => $fileLoc,
+                'user_id' => $superUserID,
+                'status' => $status
+                ]);
+
+                $upload->save();
+
+                echo 'completed post-process of migration for uploaded image ' . $upload->id . PHP_EOL;
+
+            } catch (Exception $e) {
+                echo 'unable to process ' . $pID . ' because ' . $e->getMessage() . "\n";
+            }
+        }
+    }
+
+    private function readMigrationFile(string $migrationFile): void
+    {
+        $file = fopen($migrationFile, 'r');
+        $headers = fgetcsv($file);
+
+        while (($row = fgetcsv($file)) !== false) {
+            $item = [];
+            foreach ($row as $key => $value) {
+                $item[$headers[$key]] = $value ?: '';
+            }
+
+            $this->csvData[] = $item;
+        }
+
+        fclose($file);
+    }
+
+    /**
+     * Get the CSV data for testing purposes.
+     *
+     * @return array
+     */
+    public function getCsvData(): array
+    {
+        return $this->csvData;
+    }
+
+}
