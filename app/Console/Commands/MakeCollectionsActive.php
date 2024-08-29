91d703eb9c71f8122b1c3f559431c6b498d60f9c
Calum Macdonald <calmacx@gmail.com>
2024-08-29 15:07:18 +0100

    my changes

---
+++ app/Console/Commands/MakeCollectionsActive.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Collection;
+use Illuminate\Console\Command;
+
+class MakeCollectionsActive extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:make-collections-active';
+
+    private $csvData = [];
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->readMigrationFile(storage_path() . '/migration_files/collections_make_active.csv');
+    }
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+
+        $progressbar = $this->output->createProgressBar(count($this->csvData));
+        foreach ($this->csvData as $csv) {
+            $collectionName = trim($csv['Name']);
+            if ($collectionName == "") {
+                $progressbar->advance();
+                continue;
+            }
+
+            $match = Collection::where("name", $collectionName);
+            $count = $match->count();
+            if (!$count) {
+                throw new \Exception("Not found --> '$collectionName'");
+            } elseif ($count > 1) {
+                $found = $match->select("name")->get();
+                echo "WARNING--> Duplicate collections found json_encode($found) \n";
+            }
+
+            $publicFlag = (int) $csv['Publicflag'];
+            if ($publicFlag !== 1) {
+                $match->update(['status' => Collection::STATUS_ACTIVE]);
+            } else {
+                $match->update(['status' => Collection::STATUS_ACTIVE]);
+            }
+            $progressbar->advance();
+        }
+        $progressbar->finish();
+    }
+
+    private function readMigrationFile(string $migrationFile): void
+    {
+        $file = fopen($migrationFile, 'r');
+        $headers = fgetcsv($file);
+
+        while (($row = fgetcsv($file)) !== false) {
+            $item = [];
+            foreach ($row as $key => $value) {
+                $item[trim($headers[$key], "\xEF\xBB\xBF")] = $value ?: '';
+            }
+
+            $this->csvData[] = $item;
+        }
+
+        fclose($file);
+    }
+}
