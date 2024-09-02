c1dabbacc83c35caf8965dae4d4e8912d84808a7
Dan Nita <dan.nita@hdruk.ac.uk>
2024-09-02 16:24:55 +0100

    update

--- database/factories/CollectionFactory.php
+++ database/factories/CollectionFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use Config;
 use App\Models\Team;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
@@ -20,7 +21,7 @@ class CollectionFactory extends Factory
         return [
             'name' => fake()->unique()->word,
             'description' => htmlentities(implode(" ", fake()->paragraphs(5, false)), ENT_QUOTES | ENT_IGNORE, "UTF-8"),
-            'image_link' => env('MEDIA_URL', 'http://media.url') . '/collections/' . fake()->lexify('????_????_????.') . fake()->randomElement(['jpg', 'jpeg', 'png', 'gif']),
+            'image_link' => Config::get('services.media.base_url') . '/collections/' . fake()->lexify('????_????_????.') . fake()->randomElement(['jpg', 'jpeg', 'png', 'gif']),
             'enabled' => fake()->randomElement([0, 1]),
             'public' => fake()->randomElement([0, 1]),
             'counter' => fake()->randomNumber(5, true),
