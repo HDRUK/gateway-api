79c715182ad74c5c02a05c8aaf607dfb5267a70c
Dan Nita <dan.nita@hdruk.ac.uk>
2024-09-05 18:25:57 +0100

    update

--- app/Http/Controllers/SSO/CustomUserController.php
+++ app/Http/Controllers/SSO/CustomUserController.php
@@ -24,17 +24,29 @@ class CustomUserController extends Controller
 
             \Log::info('CustomUserController userInfo :: ' . json_encode($user));
             \Log::info('CustomUserController userInfo request :: ' . json_encode($request->user()));
-    
+
+            // return response()->json([
+            //     'id' => $user->id,
+            //     'name' => $user->name,
+            //     'preferred_username' => $user->name,
+            //     'given_name' => $user->firstname,
+            //     'family_name' => $user->lastname,
+            //     'firstname' => $user->firstname,
+            //     'lastname' => $user->family_name,
+            //     'email' => $user->email,
+            //     'rquestroles' => $user->rquestroles, // no idea if is ok
+            // ]);
+
             return response()->json([
                 'id' => $user->id,
                 'name' => $user->name,
                 'preferred_username' => $user->name,
                 'firstname' => $user->firstname,
-                'lastname' => $user->family_name,
+                'lastname' => $user->lastname,
                 'given_name' => $user->firstname,
-                'family_name' => $user->family_name,
+                'family_name' => $user->lastname,
                 'email' => $user->email,
-                'rquestroles' => ['GENERAL_ACCESS', 'SYSTEM_ADMIN'], // no idea if is ok
+                'rquestroles' => ['GENERAL_ACCESS'],
             ]);
         } catch (Exception $e) {
             throw new Exception($e);
