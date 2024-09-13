8a6f78e73c58adbb09a85477a79f8b64e61e1080
Dan Nita <dan.nita@hdruk.ac.uk>
2024-09-13 11:43:28 +0100

    update

--- app/Http/Controllers/SSO/JwksController.php
+++ app/Http/Controllers/SSO/JwksController.php
@@ -10,12 +10,6 @@ class JwksController extends Controller
 {
     public function getJwks(Request $request)
     {
-        // remove after
-        // $jwt = (new Parser())->parse((string) $token);
-        // $kid = $jwt->headers()->get('kid');
-        // $path = file_get_contents(__DIR__ . '/../../../storage/oauth-private.key');
-        // $details = openssl_pkey_get_details(openssl_pkey_get_private($path));
-
         $filePath = storage_path('oauth-public.key');
         if (!file_exists($filePath)) {
             throw new Exception('File not found');
@@ -28,17 +22,10 @@ class JwksController extends Controller
             'kty' => 'RSA',
             'alg' => 'RS256',
             'use' => 'sig',
-            // 'n2' => rtrim(str_replace(['+', '/'], ['-', '_'], base64_encode($details['rsa']['n'])), '='),
-            // 'e2' => rtrim(str_replace(['+', '/'], ['-', '_'], base64_encode($details['rsa']['e'])), '='),
-            // 'n' => rtrim(strtr(base64_encode($details['rsa']['n']), '+/', '-_'), '='),
-            // 'e' => rtrim(strtr(base64_encode($details['rsa']['e']), '+/', '-_'), '='),
             'n'   => strtr(rtrim(base64_encode($details['rsa']['n']), '='), '+/', '-_'),
             'e'   => strtr(rtrim(base64_encode($details['rsa']['e']), '='), '+/', '-_'),
             'kid' => env('JWT_KID', 'jwtkidnotfound'),
         ];
-
-        // we can generate for server
-        // $keys['kid'] = bin2hex(random_bytes(16));
 
         $jwks = [
             'keys' => [
