c89652622b416302d488f0a8ca65d225ca9d728a
Sam Cox <sam.cox@nottingham.ac.uk>
2024-09-13 09:26:41 +0100

    Correct the updateOrCreate.

--- app/Http/Controllers/Api/V1/ToolController.php
+++ app/Http/Controllers/Api/V1/ToolController.php
@@ -1046,10 +1046,29 @@ class ToolController extends Controller
                 if ($value === 0) {
                     continue;
                 }
-                ToolHasTag::updateOrCreate([
+                // This whole thing could be an updateOrCreate, but Eloquent can't cope with the fact
+                // this model has no single primary key column so we have to go around the houses.
+                $toolHasTag = ToolHasTag::withTrashed()->where(
+                    [
                     'tool_id' => (int)$toolId,
                     'tag_id' => (int)$value,
-                ]);
+                    ]
+                )->first();
+                // undelete if it has been soft-deleted
+                if ($toolHasTag && $toolHasTag->deleted_at != null) {
+                    // We have to use a raw query to undelete because Eloquent can't cope with the fact this model has no single primary key column.
+                    \DB::table('tool_has_tags')
+                    ->where(['tool_id' => (int)$toolId,
+                        'tag_id' => (int)$value])
+                        ->update(['deleted_at' => null]);
+                };
+                // create it if required
+                if (!$toolHasTag) {
+                    ToolHasTag::create([
+                        'tool_id' => (int)$toolId,
+                        'tag_id' => (int)$value,
+                    ]);
+                }
             }
 
             return true;
