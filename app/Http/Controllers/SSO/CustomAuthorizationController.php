02b4348d40c747e0533b965a2b2532c35e7d8bc1
Dan Nita <dan.nita@hdruk.ac.uk>
2024-09-16 14:17:34 +0100

    update

--- app/Http/Controllers/SSO/CustomAuthorizationController.php
+++ app/Http/Controllers/SSO/CustomAuthorizationController.php
@@ -5,8 +5,7 @@ namespace App\Http\Controllers\SSO;
 use CloudLogger;
 use App\Models\OauthUser;
 use Illuminate\Support\Str;
-// use Illuminate\Http\Request;
-use Illuminate\Http\Request as LaravelRequest;
+use Illuminate\Http\Request;
 use App\Models\CohortRequest;
 use Laravel\Passport\Passport;
 use App\Models\User as UserModel;
@@ -20,8 +19,6 @@ use League\OAuth2\Server\AuthorizationServer;
 use Illuminate\Contracts\Routing\ResponseFactory;
 use Laravel\Passport\Contracts\AuthorizationViewResponse;
 use Laravel\Passport\Http\Controllers\RetrievesAuthRequestFromSession;
-
-use Illuminate\Support\Facades\Auth;
 
 class CustomAuthorizationController extends Controller
 {
@@ -54,36 +51,16 @@ class CustomAuthorizationController extends Controller
      */
     public function customAuthorize(
         ServerRequestInterface $psrRequest,
+        Request $request,
         ClientRepository $clients,
     ) {
-        $request = new LaravelRequest(
-            $psrRequest->getQueryParams(),
-            $psrRequest->getParsedBody(),
-            [], // Attributes
-            $psrRequest->getCookieParams(),
-            $psrRequest->getUploadedFiles(),
-            $psrRequest->getServerParams(),
-            $psrRequest->getBody()->__toString()
-        );
-
-        // Set method and headers
-        $request->setMethod($psrRequest->getMethod());
-        foreach ($psrRequest->getHeaders() as $name => $values) {
-            $request->headers->set($name, implode(', ', $values));
-        }
-
-        // Ensure Content-Type is set
-        if (!$request->headers->has('Content-Type')) {
-            $request->headers->set('Content-Type', 'application/x-www-form-urlencoded');
-        }
-        
         // $userId = session('cr_uid');
 
         // mock user id for with we need:
         // - cohort_regests.request_status = 'APPROVED'
-        // $cohortRequests = CohortRequest::where(['request_status' => 'APPROVED'])->first();
-        $cohortRequests = CohortRequest::where(['user_id' => 3946])->first();
+        $cohortRequests = CohortRequest::where(['request_status' => 'APPROVED'])->first();
         $userId = $cohortRequests->user_id;
+        // end mock user id
 
         // this is only temporary - it needs to be there when the flow starts in the backend
         OauthUser::where('user_id', $userId)->delete();
@@ -92,20 +69,9 @@ class CustomAuthorizationController extends Controller
             'nonce' => $request->query('nonce'),
         ]);
 
-        // Debugging: Log request parameters
-        \Log::info('Request parameters : ' . json_encode($request->all()));
-
-        return $this->withErrorHandling(function () use ($psrRequest, $request, $userId) {
-            // Additional debugging
-            \Log::info('Auth request : ' . json_encode([
-                'grant_type' => $request->input('grant_type'),
-                'client_id' => $request->input('client_id'),
-            ]));
-
+        return $this->withErrorHandling(function () use ($psrRequest, $userId) {
             $authRequest = $this->server->validateAuthorizationRequest($psrRequest);
-            \Log::info('authRequest :: ' . json_encode($authRequest));
-
-            return $this->approveRequest($authRequest, $userId, $request);
+            return $this->approveRequest($authRequest, $userId);
         });
     }
 
@@ -131,14 +97,8 @@ class CustomAuthorizationController extends Controller
      * @param  Integer  $userId
      * @return \Psr\Http\Message\ResponseInterface
      */
-    private function approveRequest($authRequest, int $userId, LaravelRequest $request)
+    private function approveRequest($authRequest, int $userId)
     {
-        \Log::info('Approving authorization request', [
-            'user_id' => $userId,
-            'client_id' => $authRequest->getClient()->getIdentifier(),
-            'grant_type' => $request->input('grant_type'),
-        ]);
-
         $authRequest->setUser(new User($userId));
 
         $authRequest->setAuthorizationApproved(true);
