098c1b25b9a75b76286d75f1dd9b307403de9b4c
Dan Nita <dan.nita@hdruk.ac.uk>
2024-09-18 10:39:01 +0100

    update

--- app/Http/Controllers/SSO/JwksController.php
+++ app/Http/Controllers/SSO/JwksController.php
@@ -50,47 +50,4 @@ class JwksController extends Controller
             $jwks
         );
     }
-
-    // public function getJwks(Request $request)
-    // {
-    //     // Load the public key from storage
-    //     $publicKeyPath = storage_path('oauth-public.key');
-    //     if (!file_exists($publicKeyPath)) {
-    //         throw new Exception('File not found');
-    //     }
-
-    //     // Read the public key content
-    //     $publicKey = file_get_contents($publicKeyPath);
-    //     if ($publicKey === false) {
-    //         throw new Exception('Unable to read public key.');
-    //     }
-
-    //     // $pem = str_replace(["-----BEGIN PUBLIC KEY-----", "-----END PUBLIC KEY-----", "\n"], '', $publicKey);
-
-    //     try {
-    //         $der = base64_decode($publicKey);
-    //         $details = openssl_pkey_get_details(openssl_pkey_get_public($der));
-    //         $n = $details['rsa']['n'];
-    //         $n = rtrim(strtr(base64_encode($n), '+/', '-_'), '=');
-    //         $e = $details['rsa']['e'];
-    //         $e = rtrim(strtr(base64_encode($e), '+/', '-_'), '=');
-
-    //         $jwks = [
-    //             'keys' => [
-    //                 'kty' => 'RSA',
-    //                 'kid' => env('JWT_KID', 'jwtkidnotfound'),
-    //                 'use' => 'sig',
-    //                 'alg' => 'RS256',
-    //                 'n' => $n,
-    //                 'e' => $e,
-    //             ]
-    //         ];
-
-    //         return response()->json(
-    //             $jwks
-    //         );
-    //     } catch (\Exception $e) {
-    //         throw new Exception('Failed to create JWK :: ' . $e->getMessage());
-    //     }
-    // }
 }
