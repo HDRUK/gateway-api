53a1c15f6958b3a33c8f58da02f5fb3d81cf7751
Dan Nita <dan.nita@hdruk.ac.uk>
2024-09-19 11:58:50 +0100

    update

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -9,13 +9,7 @@ class Cors
 {
     public function handle(Request $request, Closure $next)
     {
-        $urls = "https://web.dev.hdruk.cloud,https://rquest.dev.hdruk.cloud/rquest/,https://rquest.test.healthdatagateway.org/bcrquest/";
-        // $allowedOrigins = explode(',', env('CORS_ACCESS_CONTROL_ALLOW_ORIGINS'));
-        $allowedOrigins = explode(',', $urls);
-
-        $origin = $request->headers->get('Origin');
-
-        \Log::info('Cors - $origin :: ' . json_encode($origin));
+        \Log::info('Cors - origin :: ' . json_encode($request->headers->get('Origin')));
         \Log::info('Cors - request headers:', $request->headers->all());
 
         $headers = [
@@ -25,17 +19,6 @@ class Cors
             'Access-Control-Allow-Methods' => 'GET, POST, PUT, PATCH, DELETE, OPTIONS',
             'Access-Control-Allow-Headers' => 'Content-Type, Origin, Authorization',
         ];
-
-        // if ($origin === 'null') {
-        //     // Option 1: Allow 'null' Origin (not generally recommended)
-        //     $headers['Access-Control-Allow-Origin'] = 'null';
-        // } elseif (in_array($origin, $allowedOrigins)) {
-        //     // Allow specific origins
-        //     $headers['Access-Control-Allow-Origin'] = $origin;
-        // } else {
-        //     // Optionally, deny the request
-        //     return response('Forbidden', 403)->withHeaders($headers);
-        // }
 
         if ($request->getMethod() === 'OPTIONS') {
             return response('OK')->withHeaders($headers);
