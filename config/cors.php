22c518d87aa0f5c89af57f3e9aa3b4dd83c4a346
Dan Nita <dan.nita@hdruk.ac.uk>
2024-09-19 14:20:33 +0100

    update

--- config/cors.php
+++ config/cors.php
@@ -17,11 +17,9 @@ return [
     'paths' => explode(',', env('CORS_PATH', 'api/*,sanctum/csrf-cookie')),
     'allowed_methods' => explode(',', env('CORS_ALLOWED_METHODS', '*')),
     'allowed_origins' => explode(',', env('CORS_ALLOWED_ORIGINS', '*')),
-    // 'allowed_origins_patterns' => explode(',', env('CORS_ALLOWED_ORIGINS_PATTERNS', '*')),
-    'allowed_origins_patterns' => explode(',', env('CORS_ALLOWED_ORIGINS_PATTERNS', '')),
+    'allowed_origins_patterns' => explode(',', env('CORS_ALLOWED_ORIGINS_PATTERNS', '*')),
     'allowed_headers' => explode(',', env('CORS_ALLOWED_HEADERS', '*')),
     'exposed_headers' => explode(',', env('CORS_EXPOSED_HEADERS', '')),
     'max_age' => env('CORS_MAX_AGE', 0),
-    // 'supports_credentials' => env('CORS_SUPPORTS_CREDENTIALS', true),
-    'supports_credentials' => env('CORS_SUPPORTS_CREDENTIALS', false),
+    'supports_credentials' => env('CORS_SUPPORTS_CREDENTIALS', true),
 ];
