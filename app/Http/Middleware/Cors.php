9ffb1deb134dee7f7044195fce54b2689832ef3c
Dan Nita <dan.nita@hdruk.ac.uk>
2024-09-19 11:55:48 +0100

    update

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -20,22 +20,22 @@ class Cors
 
         $headers = [
             // 'Access-Control-Allow-Origin' => env('CORS_ACCESS_CONTROL_ALLOW_ORIGIN'),
-            // 'Access-Control-Allow-Origin' => '*',
-            'Access-Control-Allow-Credentials' => 'true',
+            'Access-Control-Allow-Origin' => '*',
+            // 'Access-Control-Allow-Credentials' => 'true',
             'Access-Control-Allow-Methods' => 'GET, POST, PUT, PATCH, DELETE, OPTIONS',
             'Access-Control-Allow-Headers' => 'Content-Type, Origin, Authorization',
         ];
 
-        if ($origin === 'null') {
-            // Option 1: Allow 'null' Origin (not generally recommended)
-            $headers['Access-Control-Allow-Origin'] = 'null';
-        } elseif (in_array($origin, $allowedOrigins)) {
-            // Allow specific origins
-            $headers['Access-Control-Allow-Origin'] = $origin;
-        } else {
-            // Optionally, deny the request
-            return response('Forbidden', 403)->withHeaders($headers);
-        }
+        // if ($origin === 'null') {
+        //     // Option 1: Allow 'null' Origin (not generally recommended)
+        //     $headers['Access-Control-Allow-Origin'] = 'null';
+        // } elseif (in_array($origin, $allowedOrigins)) {
+        //     // Allow specific origins
+        //     $headers['Access-Control-Allow-Origin'] = $origin;
+        // } else {
+        //     // Optionally, deny the request
+        //     return response('Forbidden', 403)->withHeaders($headers);
+        // }
 
         if ($request->getMethod() === 'OPTIONS') {
             return response('OK')->withHeaders($headers);
