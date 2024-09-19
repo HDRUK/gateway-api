127d8c5bcaa9822fcc98d53f1691c93108c062c2
Dan Nita <dan.nita@hdruk.ac.uk>
2024-09-19 13:49:54 +0100

    update

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -9,27 +9,23 @@ class Cors
 {
     public function handle(Request $request, Closure $next)
     {
-        // $headers = [
-        //     'Access-Control-Allow-Origin' => env('CORS_ACCESS_CONTROL_ALLOW_ORIGIN'),
-        //     'Access-Control-Allow-Credentials' => 'true',
-        //     'Access-Control-Allow-Methods' => 'GET, POST, PUT, PATCH, DELETE, OPTIONS',
-        //     'Access-Control-Allow-Headers' => 'Content-Type, Origin, Authorization',
-        // ];
+        $headers = [
+            // 'Access-Control-Allow-Origin' => env('CORS_ACCESS_CONTROL_ALLOW_ORIGIN'),
+            'Access-Control-Allow-Origin' => '*',
+            // 'Access-Control-Allow-Credentials' => 'true',
+            'Access-Control-Allow-Methods' => 'GET, POST, PUT, PATCH, DELETE, OPTIONS',
+            'Access-Control-Allow-Headers' => 'Content-Type, Origin, Authorization',
+        ];
 
-        // if ($request->getMethod() === 'OPTIONS') {
-        //     return response('OK')->withHeaders($headers);
-        // }
+        if ($request->getMethod() === 'OPTIONS') {
+            return response('OK')->withHeaders($headers);
+        }
 
-        // $response = $next($request);
-        // foreach ($headers as $key => $value) {
-        //     $response->headers->add([$key => $value]);
-        // }
+        $response = $next($request);
+        foreach ($headers as $key => $value) {
+            $response->headers->add([$key => $value]);
+        }
 
-        // return $response;
-
-        return $next($request)
-            ->header('Access-Control-Allow-Origin', '*')
-            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
-            ->header('Access-Control-Allow-Headers', 'Authorization,Accept,Origin,DNT,X-CustomHeader,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Content-Range,Range'); 
+        return $response;
     }
 }
