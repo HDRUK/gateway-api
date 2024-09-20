21eb9c8a37b3acf1038203e6bb0a1d1579be6161
Dan Nita <dan.nita@hdruk.ac.uk>
2024-09-20 10:03:03 +0100

    update

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -9,13 +9,33 @@ class Cors
 {
     public function handle(Request $request, Closure $next)
     {
+        $allowedOrigins = [
+            env('CORS_ACCESS_CONTROL_ALLOW_ORIGIN'),
+            env('RQUEST_INIT_URL'),
+        ];
+        $origin = $request->headers->get('Origin');
+
+        \Log::info('Cors - origin :: ' . $request->headers->get('Origin'));
+
         $headers = [
             // 'Access-Control-Allow-Origin' => env('CORS_ACCESS_CONTROL_ALLOW_ORIGIN'),
-            'Access-Control-Allow-Origin' => '*',
-            // 'Access-Control-Allow-Credentials' => 'true',
+            // 'Access-Control-Allow-Origin' => '*',
+            'Access-Control-Allow-Credentials' => 'true',
             'Access-Control-Allow-Methods' => 'GET, POST, PUT, PATCH, DELETE, OPTIONS',
             'Access-Control-Allow-Headers' => 'Content-Type, Origin, Authorization',
         ];
+
+        // If Origin is null, decide how to handle it
+        if ($origin === 'null') {
+            // Option 1: Allow 'null' Origin (not generally recommended)
+            $headers['Access-Control-Allow-Origin'] = 'null';
+        } elseif (in_array($origin, $allowedOrigins)) {
+            // Allow specific origins
+            $headers['Access-Control-Allow-Origin'] = $origin;
+        } else {
+            // Optionally, deny the request
+            return response('Forbidden', 403)->withHeaders($headers);
+        }
 
         if ($request->getMethod() === 'OPTIONS') {
             return response('OK')->withHeaders($headers);
