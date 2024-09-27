7693a074c0f8ab8c0976f14483297f9a341e3208
Dan Nita <dan.nita@hdruk.ac.uk>
2024-09-27 11:18:04 +0100

    update

--- config/cors.php
+++ config/cors.php
@@ -14,12 +14,21 @@ return [
     |
     */
 
-    'paths' => explode(',', env('CORS_PATH', 'api/*,sanctum/csrf-cookie','*')),
-    'allowed_methods' => explode(',', env('CORS_ALLOWED_METHODS', '*')),
-    'allowed_origins' => explode(',', env('CORS_ALLOWED_ORIGINS', '*')),
-    'allowed_origins_patterns' => explode(',', env('CORS_ALLOWED_ORIGINS_PATTERNS', '')),
-    'allowed_headers' => explode(',', env('CORS_ALLOWED_HEADERS', '*')),
-    'exposed_headers' => explode(',', env('CORS_EXPOSED_HEADERS', '')),
-    'max_age' => env('CORS_MAX_AGE', 0),
-    'supports_credentials' => env('CORS_SUPPORTS_CREDENTIALS', true),
+    // 'paths' => explode(',', env('CORS_PATH', 'api/*,sanctum/csrf-cookie,*')),
+    // 'allowed_methods' => explode(',', env('CORS_ALLOWED_METHODS', '*')),
+    // 'allowed_origins' => explode(',', env('CORS_ALLOWED_ORIGINS', '*')),
+    // 'allowed_origins_patterns' => explode(',', env('CORS_ALLOWED_ORIGINS_PATTERNS', '')),
+    // 'allowed_headers' => explode(',', env('CORS_ALLOWED_HEADERS', '*')),
+    // 'exposed_headers' => explode(',', env('CORS_EXPOSED_HEADERS', '')),
+    // 'max_age' => env('CORS_MAX_AGE', 0),
+    // 'supports_credentials' => env('CORS_SUPPORTS_CREDENTIALS', true),
+
+    'paths' => explode(',', env('CORS_PATH', 'api/*,sanctum/csrf-cookie,*')),
+    'allowed_methods' => ['*'],
+    'allowed_origins' => ['*'],
+    'allowed_origins_patterns' => [],
+    'allowed_headers' => ['*'],
+    'exposed_headers' => [],
+    'max_age' => 0,
+    'supports_credentials' => false,
 ];
