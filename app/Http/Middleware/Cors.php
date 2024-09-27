a3ac32433823d9cd386ed2a61595b0b198b92b55
Dan Nita <dan.nita@hdruk.ac.uk>
2024-09-27 09:52:36 +0100

    update

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -10,8 +10,9 @@ class Cors
     public function handle(Request $request, Closure $next)
     {
         $headers = [
-            'Access-Control-Allow-Origin' => env('CORS_ACCESS_CONTROL_ALLOW_ORIGIN'),
-            'Access-Control-Allow-Credentials' => 'true',
+            // 'Access-Control-Allow-Origin' => env('CORS_ACCESS_CONTROL_ALLOW_ORIGIN'),
+            'Access-Control-Allow-Origin' => '*',
+            // 'Access-Control-Allow-Credentials' => 'true',
             'Access-Control-Allow-Methods' => 'GET, POST, PUT, PATCH, DELETE, OPTIONS',
             'Access-Control-Allow-Headers' => 'Content-Type, Origin, Authorization',
         ];
