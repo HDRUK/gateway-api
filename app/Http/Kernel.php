cd8750884c5fc1da452920f5eedf5ddb6c4ff04c
Dan Nita <dan.nita@hdruk.ac.uk>
2024-09-30 11:13:11 +0100

    update

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -14,8 +14,8 @@ class Kernel extends HttpKernel
      * @var array<int, class-string|string>
      */
     protected $middleware = [
-        // \Illuminate\Http\Middleware\HandleCors::class,
-        \App\Http\Middleware\Cors::class,
+        \Illuminate\Http\Middleware\HandleCors::class,
+        // \App\Http\Middleware\Cors::class,
         \App\Http\Middleware\TrustProxies::class,
         \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
