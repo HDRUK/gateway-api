ce8fc364ad210373d204bcfa477a44ffa29f3c58
Dan Nita <dan.nita@hdruk.ac.uk>
2024-10-02 13:14:56 +0100

    update

--- tests/Feature/JwtControllerTest.php
+++ tests/Feature/JwtControllerTest.php
@@ -3,100 +3,54 @@
 namespace Tests\Feature;
 
 use Tests\TestCase;
+use App\Models\User;
 use Carbon\CarbonImmutable;
+use Tests\Traits\MockExternalApis;
 use App\Http\Controllers\JwtController;
+use Database\Seeders\MinimalUserSeeder;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class JwtControllerTest extends TestCase
 {
+    use RefreshDatabase;
+    use MockExternalApis {
+        setUp as commonSetUp;
+    }
+
+    protected $header = [];
+
+    public function setUp(): void
+    {
+        $this->commonSetUp();
+
+        $this->seed([
+            MinimalUserSeeder::class,
+        ]);
+    }
+
     /**
      * generate jwt token
      */
     public function test_generate_jwt(): void
     {
         $jwtClass = new JwtController();
-
-        $currentTime = CarbonImmutable::now();
-        $expireTime = $currentTime->addSeconds(env('JWT_EXPIRATION'));
-
-        $userName = fake()->firstName() . ' ' . fake()->lastName;
-        $userId = mt_rand(100, 200);
-
-
-        $userClaims = [
-            'id' => (string) mt_rand(100, 200),
-            'name' => $userId,
-            'email' => fake()->unique()->safeEmail(),
-        ];
-
-        $arrayClaims = [
-            'iss' => (string) env('APP_URL'),
-            'sub' => (string) $userName,
-            'aud' => (string) env('APP_NAME'),
-            'iat' => (string) strtotime($currentTime),
-            'nbf' => (string) strtotime($currentTime),
-            'exp' => (string) strtotime($expireTime),
-            'jti' => (string) env('JWT_SECRET'),
-            'user' => $userClaims,
-        ];
-
-        $jwtClass->setPayload($arrayClaims);
-        $jwt = $jwtClass->create();
+        $userId = User::all()->random()->id;
+        $jwt = $jwtClass->generateToken($userId);
 
         $this->assertIsString($jwt);
     }
 
     public function test_generate_and_check_jwt_payload()
     {
-        // generate a jwt
         $jwtClass = new JwtController();
-
-        $currentTime = CarbonImmutable::now();
-        $expireTime = $currentTime->addSeconds(env('JWT_EXPIRATION'));
-
-        $userName = fake()->firstName() . ' ' . fake()->lastName;
-        $userId = mt_rand(100, 200);
-        $userEmail = fake()->unique()->safeEmail();
-
-        $userClaims = [
-            'id' => (string) $userId,
-            'name' => $userName,
-            'email' => $userEmail,
-        ];
-
-        $arrayClaims = [
-            'iss' => (string) env('APP_URL'),
-            'sub' => (string) $userName,
-            'aud' => (string) env('APP_NAME'),
-            'iat' => (string) strtotime($currentTime),
-            'nbf' => (string) strtotime($currentTime),
-            'exp' => (string) strtotime($expireTime),
-            'jti' => (string) env('JWT_SECRET'),
-            'user' => $userClaims,
-        ];
-
-        $jwtClass->setPayload($arrayClaims);
-        $jwt = $jwtClass->create();
-
-        $this->assertIsString($jwt);
-
-        // decode the jwt
+        $userId = User::all()->random()->id;
+        $jwt = $jwtClass->generateToken($userId);
         $jwtClass->setJwt($jwt);
         $jwtDecoded = $jwtClass->decode();
 
-        $this->assertArrayHasKey('iss', $jwtDecoded);
-        $this->assertArrayHasKey('sub', $jwtDecoded);
-        $this->assertArrayHasKey('aud', $jwtDecoded);
-        $this->assertArrayHasKey('iat', $jwtDecoded);
-        $this->assertArrayHasKey('nbf', $jwtDecoded);
-        $this->assertArrayHasKey('exp', $jwtDecoded);
-        $this->assertArrayHasKey('jti', $jwtDecoded);
-        $this->assertArrayHasKey('user', $jwtDecoded);
-        $this->assertArrayHasKey('id', $jwtDecoded['user']);
-        $this->assertArrayHasKey('name', $jwtDecoded['user']);
-        $this->assertArrayHasKey('email', $jwtDecoded['user']);
+        $isValidJwt = $jwtClass->isValid();
 
+        $this->assertEquals(true, $isValidJwt);
         $this->assertEquals($jwtDecoded['user']['id'], $userId);
-        $this->assertEquals($jwtDecoded['user']['name'], $userName);
-        $this->assertEquals($jwtDecoded['user']['email'], $userEmail);
     }
 }
