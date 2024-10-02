1aa55e617e419cd96bb3ba0ae7628a9ad111a676
Dan Nita <dan.nita@hdruk.ac.uk>
2024-10-02 16:36:21 +0100

    update

--- app/Http/Controllers/Api/V1/CohortRequestController.php
+++ app/Http/Controllers/Api/V1/CohortRequestController.php
@@ -1126,11 +1126,16 @@ class CohortRequestController extends Controller
             $rquestInitUrl = Config::get('services.rquest.init_url');
             \Log::info('CohortRequestController checkAccess :: ' . json_encode($rquestInitUrl));
 
-            return new RedirectResponse($rquestInitUrl);
+            return new RedirectResponse($rquestInitUrl, 302, $request->headers->all());
             // return redirect()->away($rquestInitUrl);
             // return response()->json([
             //     'redirect_uri' => $rquestInitUrl,
             // ], Config::get('statuscodes.STATUS_OK.code'));
+            // return redirect('https://rquest.test.healthdatagateway.org/bcrquest/')
+            //     ->withHeaders([
+            //         'Access-Control-Allow-Origin' => 'https://rquest.test.healthdatagateway.org',
+            //         'Access-Control-Allow-Credentials' => 'true',
+            //     ]);
         } catch (Exception $e) {
             Auditor::log([
                 'user_id' => (int)$jwtUser['id'],
