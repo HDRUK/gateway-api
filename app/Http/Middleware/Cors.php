902b58ea5092f08f232c6cbd51f7989495f40f5b
Dan Nita <dan.nita@hdruk.ac.uk>
2024-10-03 19:07:02 +0100

    update

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -9,7 +9,7 @@ class Cors
 {
     public function handle(Request $request, Closure $next)
     {
-        $allowedOrigins = explode(',', 'https://web.dev.hdruk.cloud,https://rquest.dev.hdruk.cloud,https://rquest.test.healthdatagateway.org');
+        $allowedOrigins = explode(',', env('CORS_ACCESS_CONTROL_ALLOW_ORIGIN'));
 
         $origin = $request->headers->get('Origin');
 
@@ -39,24 +39,4 @@ class Cors
         }
         return $response;
     }
-
-    // public function handle(Request $request, Closure $next)
-    // {
-    //     $headers = [
-    //         'Access-Control-Allow-Origin' => env('CORS_ACCESS_CONTROL_ALLOW_ORIGIN'),
-    //         'Access-Control-Allow-Credentials' => 'true',
-    //         'Access-Control-Allow-Methods' => 'GET, POST, PUT, PATCH, DELETE, OPTIONS',
-    //         'Access-Control-Allow-Headers' => 'Content-Type, Origin, Authorization',
-    //     ];
-
-    //     if ($request->getMethod() === 'OPTIONS') {
-    //         return response('OK')->withHeaders($headers);
-    //     }
-
-    //     $response = $next($request);
-    //     foreach ($headers as $key => $value) {
-    //         $response->headers->add([$key => $value]);
-    //     }
-    //     return $response;
-    // }
 }
