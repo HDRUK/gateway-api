85a14af05aa30cbe3f0c28ebe146b720f2d08b18
Dan Nita <dan.nita@hdruk.ac.uk>
2024-10-03 18:07:44 +0100

    update

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -9,21 +9,9 @@ class Cors
 {
     public function handle(Request $request, Closure $next)
     {
-        // $headers = [
-        //     'Access-Control-Allow-Origin' => env('CORS_ACCESS_CONTROL_ALLOW_ORIGIN'),
-        //     'Access-Control-Allow-Credentials' => 'true',
-        //     'Access-Control-Allow-Methods' => 'GET, POST, PUT, PATCH, DELETE, OPTIONS',
-        //     'Access-Control-Allow-Headers' => 'Content-Type, Origin, Authorization',
-        // ];
-
-        // $allowedOrigins = explode(',', env('CORS_ACCESS_CONTROL_ALLOW_ORIGINS'));
         $allowedOrigins = explode(',', 'https://web.dev.hdruk.cloud,https://rquest.dev.hdruk.cloud,https://rquest.test.healthdatagateway.org');
 
         $origin = $request->headers->get('Origin');
-        // $origin = $request->headers->get('X-Forwarded-Host');
-        \Log::info('$origin :: ' . json_encode($origin));
-        \Log::info('Cors :: ' . json_encode($request));
-        \Log::info('Cors 2 :: ' . json_encode($request->headers->get('X-Forwarded-Host')));
 
         if (in_array($origin, $allowedOrigins)) {
             $headers = [
@@ -32,7 +20,6 @@ class Cors
                 'Access-Control-Allow-Methods' => 'GET, POST, PUT, PATCH, DELETE, OPTIONS',
                 'Access-Control-Allow-Headers' => 'Origin, X-Requested-With, Content-Type, Accept, Authorization',
             ];
-            \Log::info('Cors A :: ' . json_encode($headers));
         } else {
             $headers = [
                 'Access-Control-Allow-Origin' => 'https://rquest.test.healthdatagateway.org',
@@ -40,7 +27,6 @@ class Cors
                 'Access-Control-Allow-Methods' => 'GET, POST, PUT, PATCH, DELETE, OPTIONS',
                 'Access-Control-Allow-Headers' => 'Origin, X-Requested-With, Content-Type, Accept, Authorization',
             ];
-            \Log::info('Cors B :: ' . json_encode($headers));
         }
 
         if ($request->getMethod() === 'OPTIONS') {
