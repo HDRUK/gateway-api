2a5995ae08ee72677a1786837a8478ff23cfc869
lewissaunders11 <lewis.saunders@hdruk.ac.uk>
2024-10-04 08:26:34 +0100

    add dataset title to returned object

--- app/Http/Traits/DatasetFetch.php
+++ app/Http/Traits/DatasetFetch.php
@@ -30,11 +30,10 @@ trait DatasetFetch
         foreach ($datasets as $dataset) {
             // Retrieve dataset version IDs associated with the current dataset
             $datasetVersionIds = $dataset->versions()->whereIn('id', $versionIds)->pluck('id')->toArray();
-            // $metadata =$dataset->latestMetadata(); // This can be modified to return metadata
-
+            $datasetTitle = $dataset->latestMetadata()->whereIn('id', $versionIds)->pluck('metadata')->toArray()[0]['metadata']['summary']['abstract']; // This can be modified to return metadata
             // Add associated dataset versions to the dataset object
             $dataset->setAttribute('dataset_version_ids', $datasetVersionIds);
-            // $dataset->setAttribute('latest_metadata', $metadata); // This can be modified to return metadata
+            $dataset->setAttribute('name', $datasetTitle); // This can be modified to return metadata
 
             // Add extra fields as required for DatasetVersionHasTool case.
             if ($linkageTable instanceof DatasetVersionHasTool) {
