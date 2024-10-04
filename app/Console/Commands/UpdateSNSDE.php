3ffbbeba241f42efe10bc3b3e66bf99c8753e74f
Branwen Snelling <branwen.snelling@hdruk.ac.uk>
2024-10-04 12:05:19 +0100

    update to valid markdown

--- app/Console/Commands/UpdateSNSDE.php
+++ app/Console/Commands/UpdateSNSDE.php
@@ -29,18 +29,18 @@ class UpdateSNSDE extends Command
     {
         $data = [
             'name' => 'The NHS Research Secure Data Environment (SDE) Network',
-            'summary' => 'The NHS Research Secure Data Environment (SDE) Network helps approved researchers by simplifying and accelerating secure access to the health and social care data they need.
-            It is designed to be the default route for accessing NHS data for research and will drive federation and interoperability in two main ways:
-            - aggregating data at a scale much larger than previously possible across the NHS, reducing data silos and landscape complexity.
-            - embedding interoperability and enabling federated analytics across multiple platforms – including genomics, imaging, and Electronic Health Records.
-            ## National coverage
-            The SDE Network includes the NHS England SDE and 11 regional SDE teams, covering all of England through a federated approach.
-            ## Rapid access
-            By using the Health Data Research Gateway as its ‘front door’, the SDE Network aims to de-fragment health data assets and make searching for data easier for researchers. The SDE Network provides rapid access to data that has both breadth and depth across care settings and data types (multi-modality), including reliably uniting primary and secondary care datasets.
-            ## Supporting research
-            The NHS Research SDE Network can support research use cases from AI development and epidemiology to health systems research and real-world studies. It is a foundational layer to a strong, data and evidence-based, research and innovation ecosystem.
-            ## Find out more
-            Information about the services provided by members of the NHS Research SDE Network is available [here](https://digital.nhs.uk/data-and-information/research-powered-by-data/sde-network).',
+            'summary' => "The NHS Research Secure Data Environment (SDE) Network helps approved researchers by simplifying and accelerating secure access to the health and social care data they need. " .
+            "It is designed to be the default route for accessing NHS data for research and will drive federation and interoperability in two main ways:" .
+            "\n- aggregating data at a scale much larger than previously possible across the NHS, reducing data silos and landscape complexity. " .
+            "\n- embedding interoperability and enabling federated analytics across multiple platforms – including genomics, imaging, and Electronic Health Records. " .
+            "\n\n### National coverage" .
+            "\nThe SDE Network includes the NHS England SDE and 11 regional SDE teams, covering all of England through a federated approach. " .
+            "\n\n### Rapid access" .
+            "\nBy using the Health Data Research Gateway as its ‘front door’, the SDE Network aims to de-fragment health data assets and make searching for data easier for researchers. The SDE Network provides rapid access to data that has both breadth and depth across care settings and data types (multi-modality), including reliably uniting primary and secondary care datasets. " .
+            "\n\n### Supporting research" .
+            "\nThe NHS Research SDE Network can support research use cases from AI development and epidemiology to health systems research and real-world studies. It is a foundational layer to a strong, data and evidence-based, research and innovation ecosystem. " .
+            "\n\n### Find out more" .
+            "\nInformation about the services provided by members of the NHS Research SDE Network is available [here](https://digital.nhs.uk/data-and-information/research-powered-by-data/sde-network).",
         ];
 
         try {
