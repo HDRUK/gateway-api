2315ca6ff5a421198d53b863bbd4f139ebf2cdb2
Peter Hammans <peter.hammans@hdruk.ac.uk>
2024-10-07 20:25:25 +0100

    chore: remove irrelevant changes

--- app/Http/Controllers/Api/V1/SearchController.php
+++ app/Http/Controllers/Api/V1/SearchController.php
@@ -1291,7 +1291,6 @@ class SearchController extends Controller
      *                      @OA\Property(property="_source", type="array",
      *                          @OA\Items(
      *                              @OA\Property(property="name", type="string"),
-     *                              @OA\Property(property="img_url", type="string"),
      *                              @OA\Property(property="datasetTitles", type="array", @OA\Items()),
      *                              @OA\Property(property="geographicLocation", type="array", @OA\Items())
      *                          )
@@ -1351,7 +1350,6 @@ class SearchController extends Controller
                     if ((int)$dp['_id'] === $model['id']) {
                         $dataProviderCollArray[$i]['_source']['updated_at'] = $model['updated_at'];
                         $dataProviderCollArray[$i]['name'] = $model['name'];
-                        $dataProviderCollArray[$i]['img_url'] = $model['img_url'];
                         $dataProviderCollArray[$i]['datasetTitles'] = $this->dataProviderDatasetTitles($model);
                         $dataProviderCollArray[$i]['geographicLocations'] = $this->dataProviderLocations($model);
                         $foundFlag = true;
