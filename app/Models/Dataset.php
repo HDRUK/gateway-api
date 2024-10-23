f4175b353b98c16479ed6919c9b7bad5b3d9d19b
Calum Macdonald <calmacx@gmail.com>
2024-10-23 14:48:51 +0100

    accidental removal of withTrashed

--- app/Models/Dataset.php
+++ app/Models/Dataset.php
@@ -123,7 +123,7 @@ class Dataset extends Model
 
     public function latestMetadata(): HasOne
     {
-        return $this->hasOne(DatasetVersion::class, 'dataset_id')
+        return $this->hasOne(DatasetVersion::class, 'dataset_id')->withTrashed()
             ->orderBy('version', 'desc');
     }
 
