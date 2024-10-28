75634b3485c5de83c94d615f610d2972ec391b28
Calum Macdonald <calmacx@gmail.com>
2024-10-28 14:33:49 +0000

    didnt add it

--- app/Models/DatasetVersionHasNamedEntities.php
+++ app/Models/DatasetVersionHasNamedEntities.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Model;
 
 class DatasetVersionHasNamedEntities extends Model
