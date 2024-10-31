d86038e2806d2b44de8d49a0d086d6ffaad27bd7
Branwen Snelling <branwen.snelling@hdruk.ac.uk>
2024-10-31 14:44:41 +0000

    debugging CI test

--- tests/Feature/UploadTest.php
+++ tests/Feature/UploadTest.php
@@ -238,6 +238,8 @@ class UploadTest extends TestCase
             ]
         );
 
+        var_dump($response);
+
         $response->assertJsonStructure([
             'data' => [
                 'id',
