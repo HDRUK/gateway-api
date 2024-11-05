beea3574b5f9380b8f765b674609ff61ae856ab2
Calum Macdonald <calmacx@gmail.com>
2024-11-05 19:18:15 +0000

    change for AdminDatasetController

--- app/Http/Controllers/Api/V1/AdminDatasetController.php
+++ app/Http/Controllers/Api/V1/AdminDatasetController.php
@@ -10,7 +10,7 @@ use App\Http\Controllers\Controller;
 
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
-use App\Jobs\TermExtraction;
+use App\Jobs\LinkageExtraction;
 
 use App\Models\Dataset;
 
@@ -133,6 +133,94 @@ class AdminDatasetController extends Controller
 
             throw new Exception($e->getMessage());
         }
+    }
+    /**
+     * @OA\Post(
+     *     path="/api/v1/datasets/admin_ctrl/trigger/term_extraction",
+     *     summary="Trigger Term Extraction for Datasets",
+     *     description="Triggers the term extraction job for datasets within a specified range and controls whether data is partially indexed in Elasticsearch.",
+     *     tags={"Datasets"},
+     *     security={{"jwt": {}}},
+     *     @OA\RequestBody(
+     *         required=true,
+     *         @OA\JsonContent(
+     *             type="object",
+     *             @OA\Property(
+     *                 property="minId",
+     *                 type="integer",
+     *                 default=1,
+     *                 description="Minimum dataset ID to include in the term extraction"
+     *             ),
+     *             @OA\Property(
+     *                 property="maxId",
+     *                 type="integer",
+     *                 description="Maximum dataset ID to include in the term extraction. Defaults to the maximum dataset ID available."
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Linkage extraction triggered successfully",
+     *         @OA\JsonContent(
+     *             type="object",
+     *             @OA\Property(property="message", type="string", example="triggered linkage"),
+     *             @OA\Property(property="dataset_ids", type="array", @OA\Items(type="integer"))
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=500,
+     *         description="Internal server error",
+     *         @OA\JsonContent(
+     *             type="object",
+     *             @OA\Property(property="error", type="string", example="An error message detailing the issue")
+     *         )
+     *     ),
+     *     @OA\Parameter(
+     *         name="Authorization",
+     *         in="header",
+     *         required=true,
+     *         @OA\Schema(type="string"),
+     *         description="JWT token for authorization in the format 'Bearer {token}'"
+     *     )
+     * )
+     */
+    public function triggerLinkageExtraction(Request $request): JsonResponse
+    {
+        try {
 
+            $minId = $request->input('minId', 1);
+            $maxId = $request->input('maxId', Dataset::max('id'));
+            $elasticIndexing = $request->input('indexElastic', true);
+
+            $datasetIds = Dataset::whereBetween("id", [$minId, $maxId])
+                            ->select('id')
+                            ->pluck('id');
+
+            foreach ($datasetIds as $id) {
+                $dataset = Dataset::where('id', $id)->first();
+                $latestMetadata = $dataset->lastMetadata();
+                $datasetVersionId = $dataset->latestVersionId($id);
+
+                $linkage = $latestMetadata['metadata']['linkage']['datasetLinkage'];
+                LinkageExtraction::dispatch(
+                    $dataset->id,
+                    $datasetVersionId,
+                    base64_encode(gzcompress(gzencode(json_encode($linkage))))
+                );
+            }
+            return response()->json([
+                'message' => "triggered linkage extraction",
+                "dataset_ids" => $datasetIds,
+            ], 200);
+
+        } catch (Exception $e) {
+            Auditor::log([
+                'action_type' => 'EXCEPTION',
+                'action_name' => class_basename($this) . '@'.__FUNCTION__,
+                'description' => $e->getMessage(),
+            ]);
+
+            throw new Exception($e->getMessage());
+        }
     }
 }
---
+++ app/Jobs/LinkageExtraction.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace App\Jobs;
+
+use Auditor;
+use Exception;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+use App\Models\DatasetVersionHasDatasetVersion;
+use App\Models\DatasetVersion;
+use App\Models\Dataset;
+
+class LinkageExtraction implements ShouldQueue
+{
+    use Dispatchable;
+    use InteractsWithQueue;
+    use Queueable;
+    use SerializesModels;
+
+    protected string $sourceDatasetId = '';
+    protected string $sourceDatasetVersionId = '';
+    protected array $linkages;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct(string $datasetId, string $datasetVersionId, string $linkages)
+    {
+        $this->sourceDatasetId = $datasetId;
+        $this->sourceDatasetVersionId = $datasetVersionId;
+        $this->linkages = json_decode(gzdecode(gzuncompress(base64_decode($linkages))), true);
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        foreach ($this->linkages as $key => $data) {
+            if(!$data) {
+                continue;
+            }
+            foreach ($data as $d) {
+                $targetDatasetVersionId = $this->findTarget($d);
+                if(!$targetDatasetVersionId) {
+                    continue;
+                }
+                DatasetVersionHasDatasetVersion::updateOrCreate([
+                    'dataset_version_source_id' => $this->sourceDatasetId,
+                    'dataset_version_target_id' => $targetDatasetVersionId,
+                    'linkage_type' => $key,
+                    'direct_linkage' => 1,
+                    'description' => 'Extracted linkage from LinkageExtraction job'
+                ]);
+
+            }
+        }
+    }
+
+    public function findTarget(array $data): int|null
+    {
+        try {
+            $id = null;
+            if (isset($data['url'])) {
+                $urlParts = explode('/', parse_url($data['url'], PHP_URL_PATH));
+                $id = end($urlParts);
+            }
+            $pid = $data['pid'] ?? null;
+            $title = $data['title'] ?? null;
+
+            if ($id) {
+                // Find by id if pid is null and id is available
+                $dataset = Dataset::find($id);
+                if($dataset) {
+                    return $dataset->latestVersionID($dataset->id);
+                }
+            }
+
+            if ($pid) {
+                // Find by pid if it exists
+                $dataset = Dataset::where('pid', $pid)->first();
+                if($dataset) {
+                    return $dataset->latestVersionID($dataset->id);
+                }
+            }
+
+            if ($title) {
+                $datasetVersion = DatasetVersion::filterTitle($title)->first();
+                if($datasetVersion) {
+                    return $datasetVersion->id;
+                }
+            }
+            return null;
+
+        } catch (Exception $e) {
+            Auditor::log([
+                'action_type' => 'EXCEPTION',
+                'action_name' => class_basename($this) . '@' . __FUNCTION__,
+                'description' => $e->getMessage(),
+            ]);
+
+            throw new Exception($e->getMessage());
+        }
+
+    }
+
+    public function tags(): array
+    {
+        return [
+            'dataset:' . $this->sourceDatasetId,
+            'version:' . $this->sourceDatasetVersionId,
+        ];
+    }
+}
