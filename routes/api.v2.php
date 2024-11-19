ddbcebdcccf5a792c27a2680bdcdede59df18e18
Sam Cox <sam.cox@nottingham.ac.uk>
2024-11-19 09:29:54 +0000

    Clean up from comments.

--- routes/api.v2.php
+++ routes/api.v2.php
@@ -2,19 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/test', function () {
-    return Response::json([
-        'message' => 'lorem ipsum dolor sit amet, consectetur adip',
-    ]);
-});
-
 foreach (config('routes_v2') as $route) {
     Route::{$route['method']}($route['path'], $route['namespaceController'] . '\\' . $route['methodController'])
         ->where($route['constraint'])
         ->middleware($route['middleware']);
 }
 
-// stop all all other routes
+// stop all other routes
 Route::any('{path}', function () {
     $response = [
         'message' => 'Resource not found',
