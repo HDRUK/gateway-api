d38bcf039f193d7d94cbd8886f446f7e9218cdbe
Jamie Byrne <jamie.byrne@hdruk.ac.uk>
2024-12-30 17:11:22 +0000

    fix: (GAT-5945) - encode name if exists (#990)
    * fix: (GAT-5945) - encode name if exists
    
    * added in for collection creation
    
    * santize
    
    * Add in a test for Big L
    
    * switch tests
    
    * fix for test

--- app/Http/Controllers/Api/V2/CollectionController.php
+++ app/Http/Controllers/Api/V2/CollectionController.php
@@ -522,6 +522,10 @@ class CollectionController extends Controller
                     'status',
                 ];
                 $array = $this->checkEditArray($input, $arrayKeys);
+                
+                if (array_key_exists('name', $input)) {
+                    $array['name'] = sanitize_input($input['name']);
+                }
 
                 // Handle the 'deleted_at' field based on 'status'
                 if (isset($input['status']) && ($input['status'] === Collection::STATUS_ARCHIVED)) {
---
+++ tests/Feature/InputSanitizerTest.php
@@ -0,0 +1,23 @@
+<?php
+namespace Tests\Feature;
+
+use Tests\TestCase;
+
+class InputSanitizerTest extends TestCase
+{
+    /**
+     * Test that the sanitize_input helper function decodes HTML entities and sanitizes input.
+     *
+     * @return void
+     */
+    public function test_sanitize_input()
+    {
+        $input = "<script>alert('XSS Attack')</script>Jamie B Tester&#039;s";
+
+        $sanitized_input = sanitize_input($input);
+
+        $this->assertStringNotContainsString('<script>', $sanitized_input);
+        $this->assertStringContainsString("Jamie", $sanitized_input);
+        $this->assertEquals("Jamie B Tester's", $sanitized_input);
+    }
+}
