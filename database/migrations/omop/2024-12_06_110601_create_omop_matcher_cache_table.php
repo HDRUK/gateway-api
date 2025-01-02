ebb30cb0371611695ecec6f00c92728c4b0b8f6b
thomgiles <thomas.giles@nottingham.ac.uk>
2025-01-02 11:27:10 +0000

    Update 2024-12_06_110601_create_omop_matcher_cache_table.php

--- database/migrations/omop/2024-12_06_110601_create_omop_matcher_cache_table.php
+++ database/migrations/omop/2024-12_06_110601_create_omop_matcher_cache_table.php
@@ -10,6 +10,8 @@ return new class () extends Migration {
      */
     public function up()
     {
+        Schema::dropIfExists('omop_matcher_cache');
+        
         Schema::create('omop_matcher_cache', function (Blueprint $table) {
             $table->id();
             $table->string('search_term', 255);
