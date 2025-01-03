be6175963f5622a912f606c5f3d7350d7c60995e
tom.giles <tom.giles@nottingham.ac.uk>
2025-01-03 14:11:51 +0000

    Undo Temp

--- config/ted.php
+++ config/ted.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'url' => env('TED_SERVICE_URL', 'https://hdr-gateway-ted-dev-qmnkcg5qjq-ew.a.run.app'),
+    'url' => env('TED_SERVICE_URL', 'http://localhost:8001'),
     'enabled' => env('TED_ENABLED', false),
     'use_partial' => env('TED_USE_PARTIAL', true),
 ];
