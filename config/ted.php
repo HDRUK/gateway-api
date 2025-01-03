ad5af23e27907df0ba139fee0eda67a0b7fa1e07
tom.giles <tom.giles@nottingham.ac.uk>
2025-01-03 11:32:54 +0000

    temp

--- config/ted.php
+++ config/ted.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'url' => env('TED_SERVICE_URL', 'https://hdr-gateway-ted-dev-qmnkcg5qjq-ew.a.run.app:8001'),
+    'url' => env('TED_SERVICE_URL', 'https://hdr-gateway-ted-dev-qmnkcg5qjq-ew.a.run.app'),
     'enabled' => env('TED_ENABLED', false),
     'use_partial' => env('TED_USE_PARTIAL', true),
 ];
