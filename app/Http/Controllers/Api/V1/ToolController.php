62a701bfcd2bc55dcd4fdadd17d87f8a26616053
Jamie Byrne <jamie.byrne@hdruk.ac.uk>
2025-01-10 17:32:49 +0000

    fix: (GAT-5946) - Format name for edits on collections, teams and tools

--- app/Http/Controllers/Api/V1/ToolController.php
+++ app/Http/Controllers/Api/V1/ToolController.php
@@ -645,7 +645,9 @@ class ToolController extends Controller
             ];
 
             $array = $this->checkEditArray($input, $arrayKeys);
-
+            if (array_key_exists('name', $input)) {
+                $array['name'] = format_clean_input($input['name']);
+            }
             Tool::where('id', $id)->first()->update($array);
 
             ToolHasTag::where('tool_id', $id)->delete();
@@ -862,7 +864,9 @@ class ToolController extends Controller
             ];
 
             $array = $this->checkEditArray($input, $arrayKeys);
-
+            if (array_key_exists('name', $input)) {
+                $array['name'] = format_clean_input($input['name']);
+            }
             $initTool = Tool::withTrashed()->where('id', $id)->first();
 
             if ($initTool['status'] === Tool::STATUS_ARCHIVED && !array_key_exists('status', $input)) {
