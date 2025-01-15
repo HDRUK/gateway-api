4cffa73638c291fa2d832dced43fb2cbe91307c1
Dan Nita <dan.nita@hdruk.ac.uk>
2025-01-15 09:55:34 +0000

    update

--- app/Models/DatasetVersionHasDatasetVersion.php
+++ app/Models/DatasetVersionHasDatasetVersion.php
@@ -3,11 +3,8 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use App\Observers\DurHasDatasetVersionObserver;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Attributes\ObservedBy;
 
-#[ObservedBy([DurHasDatasetVersionObserver::class])]
 class DatasetVersionHasDatasetVersion extends Model
 {
     use HasFactory;
