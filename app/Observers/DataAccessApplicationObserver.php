cb1fae105b7f4284002b7b830fed876a1ca5f3e2
Branwen Snelling <branwen.snelling@hdruk.ac.uk>
2025-01-30 14:12:55 +0000

    rm logging in observer

--- app/Observers/DataAccessApplicationObserver.php
+++ app/Observers/DataAccessApplicationObserver.php
@@ -7,20 +7,11 @@ use App\Models\DataAccessApplicationStatus;
 
 class DataAccessApplicationObserver
 {
-    // /**
-    //  * Handle the Collection "updating" event.
-    //  */
-    // public function updating(Collection $collection)
-    // {
-    //     $collection->prevStatus = $collection->getOriginal('status'); // 'status' before updating
-    // }
-
     /**
      * Handle the DataAccessApplication "updated" event.
      */
     public function updated(DataAccessApplication $application): void
     {
-        \Log::info('in updated observation');
         if ($application->wasChanged('approval_status')) {
             DataAccessApplicationStatus::create([
                 'application_id' => $application->id,
