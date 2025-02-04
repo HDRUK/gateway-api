15c15068aa91f87392061d4bcdc17ecb8b90d137
tom.giles <tom.giles@nottingham.ac.uk>
2025-02-04 15:59:26 +0000

    Last one

--- app/Http/Controllers/Api/V1/PublicationController.php
+++ app/Http/Controllers/Api/V1/PublicationController.php
@@ -939,6 +939,10 @@ class PublicationController extends Controller
                     'description' => 'Extracted from Publication',
                 ],
                 [
+                    'publication_id' => $publicationId,
+                    'dataset_version_id' => $datasetVersionId,
+                    'link_type' => $dataset['link_type'] ?? 'USING',
+                    'description' => 'Extracted from Publication',
                     'deleted_at' => null,
                     'created_at' => $dataset['updated_at'] ?? null,
                     'updated_at' => $dataset['updated_at'] ?? null,
