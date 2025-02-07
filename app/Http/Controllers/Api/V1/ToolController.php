ac99b2020ce680ee304840580c6e0fa0d3889b76
Sam Cox <sam.cox@nottingham.ac.uk>
2025-02-07 14:42:11 +0000

    Decode name, description and results_insights correctly.

--- app/Http/Controllers/Api/V1/ToolController.php
+++ app/Http/Controllers/Api/V1/ToolController.php
@@ -1066,8 +1066,9 @@ class ToolController extends Controller
         ->where(['id' => $toolId])
         ->first();
 
-        $tool->description = htmlspecialchars_decode($tool->description);
-        $tool->results_insights = htmlspecialchars_decode($tool->results_insights);
+        $tool->name = html_entity_decode($tool->name);
+        $tool->description = html_entity_decode($tool->description);
+        $tool->results_insights = html_entity_decode($tool->results_insights);
         $tool->setAttribute('datasets', $tool->allDatasets  ?? []);
         return $tool;
     }
