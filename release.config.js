9c896ac9f517231504dba45cf93776571ec5057a
Jamie Byrne <jamie.byrne@hdruk.ac.uk>
2025-02-21 15:08:00 +0000

    fix(GAT-6216): Add in force and rebase to avoid conflicts (#1104)

--- release.config.js
+++ release.config.js
@@ -1,61 +1,106 @@
 module.exports = {
-  branches: ["main"], // Keep main as the release branch
 
-  plugins: [
-    "@semantic-release/commit-analyzer",
+    branches: ["main"],
 
-    [
-      "@semantic-release/release-notes-generator",
-      {
-        preset: "conventionalcommits",
-        presetConfig: {
-          types: [
-            { type: "feat", section: "✨ Features" },
-            { type: "fix", section: "🐛 Bug Fixes" },
-            { type: "perf", section: "⚡ Performance Improvements" },
-            { type: "docs", section: "📖 Documentation", hidden: false },
-            { type: "chore", section: "🔧 Maintenance", hidden: true },
+    plugins: [
+
+      "@semantic-release/commit-analyzer",
+
+      [
+
+        "@semantic-release/release-notes-generator",
+
+        {
+
+          preset: "conventionalcommits",
+
+          presetConfig: {
+
+            types: [
+
+              { type: "feat", section: "✨ Features" },
+
+              { type: "fix", section: "🐛 Bug Fixes" },
+
+              { type: "perf", section: "⚡ Performance Improvements" },
+
+              { type: "docs", section: "📖 Documentation", hidden: false },
+
+              { type: "chore", section: "🔧 Maintenance", hidden: true },
+
+            ],
+
+            issuePrefixes: ["GAT", "TPD", "OS"], // Jira prefixes
+
+            issueUrlFormat: process.env.JIRA_URL + "{{prefix}}{{id}}"
+
+          }
+
+        }
+
+      ],
+
+      [
+
+        "@semantic-release/changelog",
+
+        {
+
+          changelogFile: "CHANGELOG.md",
+
+        }
+
+      ],
+
+      [
+
+        "@semantic-release/github",
+
+        {
+
+          successComment: false,
+
+          failComment: false,
+
+        }
+
+      ],
+
+      [
+
+        "@semantic-release/exec",
+
+        {
+
+          prepareCmd: "node updateVersions.js ${nextRelease.version} && git add chart/gateway-api/Chart.yaml package.json"
+
+        }
+
+      ],
+
+      [
+
+        "@semantic-release/git",
+
+        {
+
+          assets: [
+
+            "package.json",
+
+            "package-lock.json",
+
+            "CHANGELOG.md",
+
+            "chart/gateway-api/Chart.yaml"
+
           ],
-          issuePrefixes: ["GAT", "TPD", "OS"], // Jira prefixes
-          issueUrlFormat: process.env.JIRA_URL + "{{prefix}}{{id}}",
-        },
-      },
-    ],
+          message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+          pushArgs: ["--force"]
+        }
 
-    [
-      "@semantic-release/changelog",
-      {
-        changelogFile: "CHANGELOG.md",
-      },
-    ],
+      ]
 
-    [
-      "@semantic-release/github",
-      {
-        successComment: false,
-        failComment: false,
-      },
-    ],
+    ]
 
-    [
-      "@semantic-release/exec",
-      {
-        prepareCmd: "git pull --rebase origin main && node updateVersions.js ${nextRelease.version} && git add chart/gateway-api/Chart.yaml package.json"
-      },
-    ],
-
-    [
-      "@semantic-release/git",
-      {
-        assets: [
-          "package.json",
-          "package-lock.json",
-          "CHANGELOG.md",
-          "chart/gateway-api/Chart.yaml",
-        ],
-        message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
-        pushArgs: ["--force"]
-      },
-    ],
-  ],
-};
+  };
