35dce0f28134a79ef142d7cba8d6aae684f90950
dnhdruk <92719613+dnhdruk@users.noreply.github.com>
2025-03-11 16:08:30 +0000

    fix(GAT-6531): Data Use Register not matching Gateway Datasets & BioSamples during upload process (#1132)
    * Data Use Register not matching Gateway Datasets & BioSamples during upload process
    
    * update
    
    * update
    
    * update

---
+++ app/Console/Commands/UpdateDurHasDatasetsGat6531.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Dur;
+use App\Models\DatasetVersion;
+use Illuminate\Console\Command;
+use App\Models\DurHasDatasetVersion;
+
+class UpdateDurHasDatasetsGat6531 extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:update-dur-has-datasets-gat6531';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Data Use Register not matching Gateway Datasets & BioSamples during upload process';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $durs = Dur::select('id', 'non_gateway_datasets')->get();
+
+        foreach ($durs as $dur) {
+            $nonGatewayDatasets = $dur['non_gateway_datasets'];
+            $this->info("non_gateway_datasets before :: " . json_encode($nonGatewayDatasets));
+            $durId = $dur->id;
+            $unmatched = [];
+            foreach ($nonGatewayDatasets as $nonGatewayDataset) {
+                $datasetName = trim($nonGatewayDataset);
+                $search = DatasetVersion::whereRaw(
+                    "LOWER(JSON_EXTRACT(JSON_UNQUOTE(metadata), '$.metadata.summary.shortTitle')) LIKE LOWER(?)",
+                    ["%$datasetName%"]
+                )->latest('version')->first();
+
+                if (is_null($search)) {
+                    $this->warn("non_gateway_datasets not-found :: " . $datasetName);
+                    $unmatched[] = $datasetName;
+                    continue;
+                }
+
+                $this->info("non_gateway_datasets found :: " . $datasetName);
+                $datasetVersionId = $search->id;
+                DurHasDatasetVersion::updateOrCreate([
+                    'dur_id' => $durId,
+                    'dataset_version_id' => $datasetVersionId,
+                ], [
+                    'dur_id' => $durId,
+                    'dataset_version_id' => $datasetVersionId,
+                ]);
+            }
+
+            $this->info("non_gateway_datasets after :: " . json_encode($unmatched));
+            Dur::where('id', $durId)->update([
+                'non_gateway_datasets' => $unmatched
+            ]);
+            $this->info('Dur Id :: ' . $durId . ' done');
+        }
+    }
+}
