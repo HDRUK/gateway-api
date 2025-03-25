4e6ea3c139fd825058ecc47e0e8ccc746700b2fb
Dan Nita <dan.nita@hdruk.ac.uk>
2025-03-25 11:41:30 +0000

    get tools by user id or team id and status

--- app/Http/Traits/ToolsV2Helper.php
+++ app/Http/Traits/ToolsV2Helper.php
@@ -46,9 +46,13 @@ trait ToolsV2Helper
         ->where([
             'user_id' => $userId,
             'id' => $toolId,
-            'status' => $toolId,
+            'status' => strtoupper($status),
         ])
         ->first();
+
+        if (is_null($tool)) {
+            return null;
+        }
 
         $tool->name = html_entity_decode($tool->name);
         $tool->description = html_entity_decode($tool->description);
