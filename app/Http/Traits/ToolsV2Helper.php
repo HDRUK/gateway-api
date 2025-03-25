c6f83690d59ab68269cd84c79d3983912f657a1f
dnhdruk <92719613+dnhdruk@users.noreply.github.com>
2025-03-25 17:49:08 +0000

    fix get tools by status and teams (#1160)

--- app/Http/Traits/ToolsV2Helper.php
+++ app/Http/Traits/ToolsV2Helper.php
@@ -85,9 +85,13 @@ trait ToolsV2Helper
         ->where([
             'team_id' => $teamId,
             'id' => $toolId,
-            'status' => $toolId,
+            'status' => strtoupper($status),
         ])
         ->first();
+
+        if (is_null($tool)) {
+            return null;
+        }
 
         $tool->name = html_entity_decode($tool->name);
         $tool->description = html_entity_decode($tool->description);
