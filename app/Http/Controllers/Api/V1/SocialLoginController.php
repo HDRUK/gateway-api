a74f52d0d1ce5021123bdcd43022caf5542d799e
Jamie Byrne <jamie.byrne@hdruk.ac.uk>
2025-04-22 10:47:58 +0100

    tester

--- app/Http/Controllers/Api/V1/SocialLoginController.php
+++ app/Http/Controllers/Api/V1/SocialLoginController.php
@@ -173,8 +173,8 @@ class SocialLoginController extends Controller
                     $providerURL = str_replace(env('APP_URL').'/api/v1/auth', env('DTA_API_URL').'/api/v1/auth/dta', $providerURL);
                 }
                 return Socialite::driver($provider)
-                //->with(['redirect_uri' => $providerURL])
-                ->redirectUrl($providerURL)
+                ->with(['redirect_uri' => $providerURL])
+                //->redirectUrl($providerURL)
                 ->redirect();
 
             }
@@ -265,8 +265,8 @@ class SocialLoginController extends Controller
                 if (env('APP_ENV') !== 'local') {
                     $providerURL = str_replace(env('APP_URL').'/api/v1/auth', env('DTA_API_URL').'/api/v1/auth/dta', $providerURL);
                 }                $socialUser = Socialite::driver($provider)
-                ->redirectUrl($providerURL)
-                //->with(['redirect_uri' => $providerURL])
+                //->redirectUrl($providerURL)
+                ->with(['redirect_uri' => $providerURL])
                 ->stateless()
                 ->user();
 
