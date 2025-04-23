508c2aa89b1cc167be8c383ee0429a080a9d2c16
Jamie Byrne <jamie.byrne@hdruk.ac.uk>
2025-04-23 16:16:22 +0100

    fix(): test cors for dta

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -9,13 +9,22 @@ class Cors
 {
     public function handle(Request $request, Closure $next)
     {
+        $origin = $request->headers->get('origin');
+
+        $allowedOrigins = [
+            'https://web.dev.hdruk.cloud',
+            'https://web.dev.dementia-trials-accelerator.org',
+            'https://api.dev.dementia-trials-accelerator.org',
+        ];
 
         $headers = [
-            'Access-Control-Allow-Origin' => env('CORS_ACCESS_CONTROL_ALLOW_ORIGIN'),
             'Access-Control-Allow-Credentials' => 'true',
             'Access-Control-Allow-Methods' => 'GET, POST, PUT, PATCH, DELETE, OPTIONS',
             'Access-Control-Allow-Headers' => 'Content-Type, Origin, Authorization',
         ];
+        if (in_array($origin, $allowedOrigins)) {
+            $headers['Access-Control-Allow-Origin'] = $origin;
+        }
 
         if ($request->getMethod() === 'OPTIONS') {
             return response('OK')->withHeaders($headers);
