2d7a09eb9cafbcd53a634837e1ad5724600be60d
Jamie Byrne <jamie.byrne@hdruk.ac.uk>
2025-06-19 07:50:22 +0100

    feat(GAT-7312): federation error and history migrations

---
+++ database/migrations/2025_06_19_064110_create_federation_histories_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class () extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('federation_run_histories', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('federations_id');
+            $table->dateTime('run_at');
+            $table->enum('result', ['success', 'error']);
+            $table->text('output')->nullable();
+            $table->text('errors')->nullable();
+            $table->string('session_id')->nullable();
+            $table->timestamps();
+            $table->foreign('federations_id')->references('id')->on('federations')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('federation_run_histories');
+    }
+};
