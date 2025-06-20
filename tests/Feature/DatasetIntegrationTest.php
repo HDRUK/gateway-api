87389ce3b5afab8aa526d89644391b563cbb08ec
Branwen Snelling <branwen.snelling@hdruk.ac.uk>
2025-06-20 15:39:46 +0100

    merge dataset integration controller and add perms to dataset v2 endpoints

--- tests/Feature/DatasetIntegrationTest.php
+++ tests/Feature/DatasetIntegrationTest.php
@@ -85,6 +85,7 @@ class DatasetIntegrationTest extends TestCase
             'x-application-id' => $this->integration->app_id,
             'x-client-id' => $this->integration->client_id,
         ];
+<<<<<<< HEAD
 
         $this->authorisationUser();
         $this->superUserJwt = $this->getAuthorisationJwt();
@@ -92,6 +93,8 @@ class DatasetIntegrationTest extends TestCase
             'Accept' => 'application/json',
             'Authorization' => 'Bearer ' . $this->superUserJwt,
         ];
+=======
+>>>>>>> 7c75b08c (merge dataset integration controller and add perms to dataset v2 endpoints)
 
         // Lengthy process, but a more consistent representation
         // of an incoming dataset
