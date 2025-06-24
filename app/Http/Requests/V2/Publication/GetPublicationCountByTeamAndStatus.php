c6d041952708c7b53bf1069834013cee176962ad
Sam Cox <sam.cox@hdruk.ac.uk>
2025-06-24 13:06:15 +0100

    Add Requests for previous commit.

---
+++ app/Http/Requests/V2/Publication/GetPublicationCountByTeamAndStatus.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests\V2\Publication;
+
+use App\Http\Requests\BaseFormRequest;
+
+class GetPublicationCountByTeamAndStatus extends BaseFormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'teamId' => 'required|int|exists:teams,id',
+            'status' => 'required|string|in:active,draft,archived',
+        ];
+    }
+
+    /**
+     * Add Route parameters to the FormRequest.
+     *
+     * @return void
+     */
+    protected function prepareForValidation()
+    {
+        $this->merge([
+            'teamId' => $this->route('teamId'),
+            'status' => $this->route('status') ?? 'active',
+        ]);
+    }
+}
