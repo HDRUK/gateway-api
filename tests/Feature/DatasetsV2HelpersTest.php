584c5e0337e56df2bf5cf8cdf75007a1d436b735
Sam Cox <sam.cox@hdruk.ac.uk>
2025-06-27 14:19:12 +0100

    Rename tests that were being skipped.

--- tests/Feature/DatasetsV2HelpersTest.php
+++ tests/Feature/DatasetsV2HelpersTest.php
@@ -20,7 +20,7 @@ class ExtractMetadataTest extends TestCase
         );
     }
 
-    public function it_handles_metadata_key_with_json_string()
+    public function test_it_handles_metadata_key_with_json_string()
     {
         $result = $this->extractMetadata([
             'metadata' => '{"foo":"bar"}'
@@ -29,7 +29,7 @@ class ExtractMetadataTest extends TestCase
     }
 
 
-    public function it_handles_nested_metadata_metadata_key()
+    public function test_it_handles_nested_metadata_metadata_key()
     {
         $result = $this->extractMetadata([
             'metadata' => [
@@ -39,14 +39,14 @@ class ExtractMetadataTest extends TestCase
         $this->assertEquals(['metadata' => ['foo' => 'bar']], $result);
     }
 
-    public function it_handles_invalid_json_gracefully()
+    public function test_it_handles_invalid_json_gracefully()
     {
         $invalidJson = '{"foo":"bar"';
         $result = $this->extractMetadata($invalidJson);
         $this->assertEquals($invalidJson, $result);
     }
 
-    public function it_returns_metadata_as_is_if_already_array()
+    public function test_it_returns_metadata_as_is_if_already_array()
     {
         $input = ['metadata' => ['foo' => 'bar']];
         $result = $this->extractMetadata($input);
