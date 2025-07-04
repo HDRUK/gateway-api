a9d7eb846ccb9d4e51cbecc1cf9f11384c3fdff4
Sam Cox <sam.cox@hdruk.ac.uk>
2025-07-04 12:12:46 +0100

    Update pre and post checks, and add the comparison script.

--- app/Console/Commands/CompareGAT7330.php
+++ app/Console/Commands/CompareGAT7330.php
@@ -57,7 +57,6 @@ class CompareGAT7330 extends Command
             $match = array_filter($json_post, function ($val) use ($entity) {
                 return (($val->type === $entity->type) && ($val->id === $entity->id));
             });
-
             if ($match) {
                 $match = $match[array_key_first($match)];
                 foreach (['DatasetVersion', 'Dur', 'Collection', 'Publication', 'Tool'] as $entityType) {
