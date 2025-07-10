aaa7b40b1a9a098a04279b7a884d161632870b85
Sam Cox <sam.cox@hdruk.ac.uk>
2025-07-10 12:57:54 +0100

    Add active entity helpers for Dataset and use where needed.

--- app/Models/Dataset.php
+++ app/Models/Dataset.php
@@ -323,6 +323,18 @@ class Dataset extends Model
         );
     }
 
+    // Accessor for all ACTIVE tools
+    public function getAllActiveToolsAttribute()
+    {
+        return $this->getRelationsViaDatasetVersion(
+            DatasetVersionHasTool::class,
+            Tool::class,
+            'tool_id',
+            false,
+            true
+        );
+    }
+
     // Accessor for all collections
     public function getAllCollectionsAttribute()
     {
@@ -333,6 +345,18 @@ class Dataset extends Model
         );
     }
 
+    // Accessor for all ACTIVE collections
+    public function getAllActiveCollectionsAttribute()
+    {
+        return $this->getRelationsViaDatasetVersion(
+            CollectionHasDatasetVersion::class,
+            Collection::class,
+            'collection_id',
+            false,
+            true
+        );
+    }
+
     // Accessor for all durs
     public function getAllDursAttribute()
     {
@@ -340,6 +364,18 @@ class Dataset extends Model
             DurHasDatasetVersion::class,
             Dur::class,
             'dur_id'
+        );
+    }
+
+    // Accessor for all ACTIVE durs
+    public function getAllActiveDursAttribute()
+    {
+        return $this->getRelationsViaDatasetVersion(
+            DurHasDatasetVersion::class,
+            Dur::class,
+            'dur_id',
+            false,
+            true
         );
     }
 
