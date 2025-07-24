9a4d9757dfa44d647ecc3e555fb0a9f66b0f9d44
soringumeni1 <sorin.gumeni@hdruk.ac.uk>
2025-07-24 18:00:58 +0100

    Change swoole version

--- Dockerfile
+++ Dockerfile
@@ -32,7 +32,7 @@ RUN apt-get update && apt-get install -y \
 # Install Redis and Imagick
 RUN wget -O redis-5.3.7.tgz 'http://pecl.php.net/get/redis-5.3.7.tgz' \
     && pecl install redis-5.3.7.tgz \
-    && pecl install swoole \
+    && pecl install swoole-5.1.7 \
     && rm -rf redis-5.3.7.tgz \
     && rm -rf /tmp/pear \
     && docker-php-ext-enable redis \
--- docker/start.sh
+++ docker/start.sh
@@ -5,7 +5,7 @@ if [ -e /var/www/.env ]; then
 fi
 
 # Initialize the base command
-base_command="php artisan serve --host=127.0.0.1 --port=8000"
+base_command="php artisan octane:start --host=0.0.0.0 --port=8000"
 
 # Check the application environment and append the appropriate options
 if [ "$APP_ENV" = 'local' ] || [ "$APP_ENV" = 'dev' ]; then
@@ -21,7 +21,6 @@ if [ -n "$OCTANE_WORKERS" ]; then
 fi
 
 # Start the Octane server in the background
-echo "StarStartingtign the php webserver"
 $base_command &
 
 if [ "$APP_ENV" = 'local' ]; then
