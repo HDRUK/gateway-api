c91d9fbd2f39a8f3bd20b0226c518a050e277d85
Dan Nita <dan.nita@hdruk.ac.uk>
2025-08-05 13:26:31 +0100

    update

--- config/session.php
+++ config/session.php
@@ -196,6 +196,6 @@ return [
     |
     */
 
-    'same_site' => env('SESSION_SAME_SITE', 'lax')
-    // 'same_site' => null
+    // 'same_site' => env('SESSION_SAME_SITE', 'lax')
+    'same_site' => null
 ];
