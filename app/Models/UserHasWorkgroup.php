d35a8812f669d1c5c1f11e85405e198f0a0bf86d
Loki Sinclair <loki.sinclair@hdruk.ac.uk>
2025-08-05 09:10:31 +0100

    feat(GAT-7670-4): Implements workgroups encoded within tokens for users

---
+++ app/Models/UserHasWorkgroup.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\Pivot;
+
+use App\Models\User;
+use App\Models\Workgroup;
+
+class UserHasWorkgroup extends Pivot
+{
+    //
+    protected $table = 'user_has_workgroups';
+    public $timestamps = false;
+
+    protected $fillable = [
+        'user_id',
+        'workgroup_id',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function workgroup(): BelongsTo
+    {
+        return $this->belongsTo(Workgroup::class);
+    }
+}
