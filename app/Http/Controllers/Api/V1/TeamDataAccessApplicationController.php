53e2117c35155a174f53a97612ce40296263ebeb
Tristan Millington <tristan.millington@gmail.com>
2025-09-04 16:30:19 +0100

    Files are now added via content rather than path

--- app/Http/Controllers/Api/V1/TeamDataAccessApplicationController.php
+++ app/Http/Controllers/Api/V1/TeamDataAccessApplicationController.php
@@ -443,13 +443,15 @@ class TeamDataAccessApplicationController extends Controller
 
                 foreach ($files as $f) {
                     $filePath = Storage::disk(env('SCANNING_FILESYSTEM_DISK', 'local_scan') . '.scanned')->path($f->file_location);
+                    $contents = Storage::disk(env('SCANNING_FILESYSTEM_DISK', 'local_scan') . '.scanned')->get($f->file_location);
+
                     $fileNameInZip = basename($f->file_location);
                     $files[] = $filePath; 
-                    //$zip->addFile($filePath, $fileNameInZip);
+                    $zip->addFromString($fileNameInZip, $contents);
                 }
             }
 
-            return response()->json($files);
+            //return response()->json($files);
 
             $csvFilename = "/tmp/csv" . time() . "_$id.csv";
 
