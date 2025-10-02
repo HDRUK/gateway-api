0f5453e5c169c000b250150a9073e46f30b44e60
Tristan Millington <tmillington001@dundee.ac.uk>
2025-10-02 11:21:30 +0100

    fix!(GAT-7759): Added sanitation on the search term and filters to search requests (#1413)

--- app/Http/Requests/Search/PublicationSearch.php
+++ app/Http/Requests/Search/PublicationSearch.php
@@ -6,6 +6,25 @@ use App\Http\Requests\BaseFormRequest;
 
 class PublicationSearch extends BaseFormRequest
 {
+    private function validateQuery($query) {
+        if (filter_var($query, FILTER_VALIDATE_URL)) {
+            $parse = parse_url($query);
+            if ($parse['host'] === "doi.org") {
+                return $query;
+            }
+        }
+
+        return preg_replace('/[^a-zA-Z0-9_-]/', '', $query);
+    }
+
+    protected function prepareForValidation(): void
+    {
+        $this->merge([
+            'query' => $this->validateQuery($this->input('query')),
+            'source' => $this->source ?? 'GAT'
+        ]);
+    }
+
     /**
      * Get the validation rules that apply to the request.
      *
@@ -14,21 +33,21 @@ class PublicationSearch extends BaseFormRequest
     public function rules(): array
     {
         return [
+            'query' => [
+                'nullable',
+                'max:255',
+            ],
             'source' => [
                 'nullable',
                 'string',
                 'in:GAT,FED',
             ],
+            'page' => 'integer',
+            'per_page' => 'integer',
+            'sort' => [
+                'regex:/^(projectTitle|created_at|year_of_publication|updated_at|name|score|date|title):(asc|desc)$/i',
+                'nullable'
+            ],
         ];
-    }
-
-    /**
-     * Add Query parameters to the FormRequest.
-     *
-     * @return void
-     */
-    protected function prepareForValidation()
-    {
-        $this->merge(['source' => $this->query('source')]);
     }
 }
---
+++ app/Http/Requests/Search/Search.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests\Search;
+
+use App\Http\Requests\BaseFormRequest;
+
+class Search extends BaseFormRequest
+{
+    protected function prepareForValidation(): void
+    {
+        $this->merge([
+            'query' => preg_replace('/[^a-zA-Z0-9_-]/', '', $this->input('query')),
+        ]);
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'query' => [
+                'nullable',
+                'max:255'
+            ],
+            'sort' => [
+                'regex:/^(projectTitle|updated_at|name|score|date|title):(asc|desc)$/i',
+                'nullable'
+            ],
+            'page' => 'integer',
+            'view_type' => ['nullable', 'in:full,mini'],
+            'per_page' => 'integer',
+            'download' => 'boolean',
+        ];
+    }
+}
