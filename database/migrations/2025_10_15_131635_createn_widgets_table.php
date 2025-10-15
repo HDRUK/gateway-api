10a0fc96b2f4756c52a3a3ae39f005f0823fa668
Jamie Byrne <jamie.byrne@hdruk.ac.uk>
2025-10-15 14:17:40 +0100

    test

--- database/migrations/2025_10_15_131635_createn_widgets_table.php
+++ database/migrations/2025_10_15_131635_createn_widgets_table.php
@@ -4,8 +4,11 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateWidgetsTable extends Migration
+return new class extends Migration
 {
+    /**
+     * Run the migrations.
+     */
     public function up(): void
     {
         Schema::create('widgets', function (Blueprint $table) {
@@ -30,8 +33,11 @@ class CreateWidgetsTable extends Migration
         });
     }
 
+    /**
+     * Reverse the migrations.
+     */
     public function down(): void
     {
         Schema::dropIfExists('widgets');
     }
-}
+};
