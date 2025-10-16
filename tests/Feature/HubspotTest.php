7e60e45d44ee775bd56a00dba44a8d65e0887bf1
Sam Cox <sam.cox@hdruk.ac.uk>
2025-10-16 14:18:55 +0100

    Reinstate hubspot test

--- tests/Feature/HubspotTest.php
+++ tests/Feature/HubspotTest.php
@@ -9,150 +9,150 @@ use Illuminate\Support\Facades\Http;
 
 class HubspotTest extends TestCase
 {
-    // protected function setUp(): void
-    // {
-    //     parent::setUp();
+    protected function setUp(): void
+    {
+        parent::setUp();
 
-    //     Config::shouldReceive('get')
-    //         ->with('services.hubspot.base_url')
-    //         ->andReturn('http://hub.local');
-    //     Config::makePartial();
+        Config::shouldReceive('get')
+            ->with('services.hubspot.base_url')
+            ->andReturn('http://hub.local');
+        Config::makePartial();
 
-    //     Config::shouldReceive('get')
-    //         ->with('services.hubspot.key')
-    //         ->andReturn('test_api_key');
-    //     Config::makePartial();
-    // }
+        Config::shouldReceive('get')
+            ->with('services.hubspot.key')
+            ->andReturn('test_api_key');
+        Config::makePartial();
+    }
 
-    // public function testCreateContact()
-    // {
-    //     $hubspotService = new Hubspot();
+    public function testCreateContact()
+    {
+        $hubspotService = new Hubspot();
 
-    //     $properties = [
-    //         'firstname' => 'John',
-    //         'lastname' => 'Doe',
-    //         'email' => 'john.doe@example.com',
-    //     ];
+        $properties = [
+            'firstname' => 'John',
+            'lastname' => 'Doe',
+            'email' => 'john.doe@example.com',
+        ];
 
-    //     $expectedBody = [
-    //         'properties' => [
-    //             ['property' => 'firstname', 'value' => 'John'],
-    //             ['property' => 'lastname', 'value' => 'Doe'],
-    //             ['property' => 'email', 'value' => 'john.doe@example.com'],
-    //         ],
-    //     ];
+        $expectedBody = [
+            'properties' => [
+                ['property' => 'firstname', 'value' => 'John'],
+                ['property' => 'lastname', 'value' => 'Doe'],
+                ['property' => 'email', 'value' => 'john.doe@example.com'],
+            ],
+        ];
 
-    //     Http::fake([
-    //         'http://hub.local/contacts/v1/contact' => Http::response(['vid' => 12345], 200),
-    //     ]);
+        Http::fake([
+            'http://hub.local/contacts/v1/contact' => Http::response(['vid' => 12345], 200),
+        ]);
 
-    //     $response = $hubspotService->createContact($properties);
+        $response = $hubspotService->createContact($properties);
 
-    //     $this->assertIsArray($response);
-    //     $this->assertEquals(12345, $response['vid']);
+        $this->assertIsArray($response);
+        $this->assertEquals(12345, $response['vid']);
 
-    //     Http::assertSent(function ($request) use ($expectedBody) {
-    //         return $request->url() == 'http://hub.local/contacts/v1/contact'
-    //             && $request->method() == 'POST'
-    //             && $request->hasHeader('Authorization', 'Bearer test_api_key')
-    //             && $request->body() == json_encode($expectedBody);
-    //     });
-    // }
+        Http::assertSent(function ($request) use ($expectedBody) {
+            return $request->url() == 'http://hub.local/contacts/v1/contact'
+                && $request->method() == 'POST'
+                && $request->hasHeader('Authorization', 'Bearer test_api_key')
+                && $request->body() == json_encode($expectedBody);
+        });
+    }
 
-    // public function testUpdateContactById()
-    // {
-    //     $hubspotService = new Hubspot();
+    public function testUpdateContactById()
+    {
+        $hubspotService = new Hubspot();
 
-    //     $id = 12345;
-    //     $properties = [
-    //         'firstname' => 'John',
-    //         'lastname' => 'Doe',
-    //     ];
+        $id = 12345;
+        $properties = [
+            'firstname' => 'John',
+            'lastname' => 'Doe',
+        ];
 
-    //     $expectedBody = [
-    //         'properties' => [
-    //             ['property' => 'firstname', 'value' => 'John'],
-    //             ['property' => 'lastname', 'value' => 'Doe'],
-    //         ],
-    //     ];
+        $expectedBody = [
+            'properties' => [
+                ['property' => 'firstname', 'value' => 'John'],
+                ['property' => 'lastname', 'value' => 'Doe'],
+            ],
+        ];
 
-    //     Http::fake([
-    //         "http://hub.local/contacts/v1/contact/vid/{$id}/profile" => Http::response([], 204),
-    //     ]);
+        Http::fake([
+            "http://hub.local/contacts/v1/contact/vid/{$id}/profile" => Http::response([], 204),
+        ]);
 
-    //     $response = $hubspotService->updateContactById($id, $properties);
+        $response = $hubspotService->updateContactById($id, $properties);
 
-    //     $this->assertEmpty($response);
+        $this->assertEmpty($response);
 
-    //     Http::assertSent(function ($request) use ($id, $expectedBody) {
-    //         return $request->url() == "http://hub.local/contacts/v1/contact/vid/{$id}/profile"
-    //             && $request->method() == 'POST'
-    //             && $request->hasHeader('Authorization', 'Bearer test_api_key')
-    //             && $request->body() == json_encode($expectedBody);
-    //     });
-    // }
+        Http::assertSent(function ($request) use ($id, $expectedBody) {
+            return $request->url() == "http://hub.local/contacts/v1/contact/vid/{$id}/profile"
+                && $request->method() == 'POST'
+                && $request->hasHeader('Authorization', 'Bearer test_api_key')
+                && $request->body() == json_encode($expectedBody);
+        });
+    }
 
-    // public function testGetContactByEmail()
-    // {
-    //     $hubspotService = new Hubspot();
+    public function testGetContactByEmail()
+    {
+        $hubspotService = new Hubspot();
 
-    //     $email = 'john.doe@example.com';
+        $email = 'john.doe@example.com';
 
-    //     Http::fake([
-    //         "http://hub.local/contacts/v1/contact/email/{$email}/profile" => Http::response(['vid' => 12345], 200),
-    //     ]);
+        Http::fake([
+            "http://hub.local/contacts/v1/contact/email/{$email}/profile" => Http::response(['vid' => 12345], 200),
+        ]);
 
-    //     $response = $hubspotService->getContactByEmail($email);
+        $response = $hubspotService->getContactByEmail($email);
 
-    //     $this->assertEquals(12345, $response);
+        $this->assertEquals(12345, $response);
 
-    //     Http::assertSent(function ($request) use ($email) {
-    //         return $request->url() == "http://hub.local/contacts/v1/contact/email/{$email}/profile"
-    //             && $request->method() == 'GET'
-    //             && $request->hasHeader('Authorization', 'Bearer test_api_key');
-    //     });
-    // }
+        Http::assertSent(function ($request) use ($email) {
+            return $request->url() == "http://hub.local/contacts/v1/contact/email/{$email}/profile"
+                && $request->method() == 'GET'
+                && $request->hasHeader('Authorization', 'Bearer test_api_key');
+        });
+    }
 
-    // public function testGetContactById()
-    // {
-    //     $hubspotService = new Hubspot();
+    public function testGetContactById()
+    {
+        $hubspotService = new Hubspot();
 
-    //     $id = 12345;
+        $id = 12345;
 
-    //     Http::fake([
-    //         "http://hub.local/contacts/v1/contact/vid/{$id}/profile" => Http::response(['vid' => 12345, 'properties' => []], 200),
-    //     ]);
+        Http::fake([
+            "http://hub.local/contacts/v1/contact/vid/{$id}/profile" => Http::response(['vid' => 12345, 'properties' => []], 200),
+        ]);
 
-    //     $response = $hubspotService->getContactById($id);
+        $response = $hubspotService->getContactById($id);
 
-    //     $this->assertIsArray($response);
-    //     $this->assertEquals(12345, $response['vid']);
+        $this->assertIsArray($response);
+        $this->assertEquals(12345, $response['vid']);
 
-    //     Http::assertSent(function ($request) use ($id) {
-    //         return $request->url() == "http://hub.local/contacts/v1/contact/vid/{$id}/profile"
-    //             && $request->method() == 'GET'
-    //             && $request->hasHeader('Authorization', 'Bearer test_api_key');
-    //     });
-    // }
+        Http::assertSent(function ($request) use ($id) {
+            return $request->url() == "http://hub.local/contacts/v1/contact/vid/{$id}/profile"
+                && $request->method() == 'GET'
+                && $request->hasHeader('Authorization', 'Bearer test_api_key');
+        });
+    }
 
-    // public function testDeleteContactById()
-    // {
-    //     $hubspotService = new Hubspot();
+    public function testDeleteContactById()
+    {
+        $hubspotService = new Hubspot();
 
-    //     $id = 12345;
+        $id = 12345;
 
-    //     Http::fake([
-    //         "http://hub.local/contacts/v1/contact/vid/{$id}" => Http::response([], 200),
-    //     ]);
+        Http::fake([
+            "http://hub.local/contacts/v1/contact/vid/{$id}" => Http::response([], 200),
+        ]);
 
-    //     $response = $hubspotService->deleteContactById($id);
+        $response = $hubspotService->deleteContactById($id);
 
-    //     $this->assertIsArray($response);
+        $this->assertIsArray($response);
 
-    //     Http::assertSent(function ($request) use ($id) {
-    //         return $request->url() == "http://hub.local/contacts/v1/contact/vid/{$id}"
-    //             && $request->method() == 'DELETE'
-    //             && $request->hasHeader('Authorization', 'Bearer test_api_key');
-    //     });
-    // }
+        Http::assertSent(function ($request) use ($id) {
+            return $request->url() == "http://hub.local/contacts/v1/contact/vid/{$id}"
+                && $request->method() == 'DELETE'
+                && $request->hasHeader('Authorization', 'Bearer test_api_key');
+        });
+    }
 }
