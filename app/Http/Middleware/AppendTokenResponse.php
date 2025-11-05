594103ae593e7951a94526fbdb08c253502ae274
Jamie Byrne <jamie.byrne@hdruk.ac.uk>
2025-11-05 12:22:39 +0000

    logs

--- app/Http/Middleware/AppendTokenResponse.php
+++ app/Http/Middleware/AppendTokenResponse.php
@@ -5,6 +5,7 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Log; 
 use App\Http\Traits\CustomIdTokenTrait;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -21,15 +22,51 @@ class AppendTokenResponse
      */
     public function handle(Request $request, Closure $next): Response|JsonResponse
     {
+        Log::info('AppendTokenResponse middleware triggered', [
+            'url' => $request->url(),
+            'method' => $request->method(),
+        ]);
+
         $response = $next($request);
+
         $currentUrl = $request->url();
+        Log::info('AppendTokenResponse current URL check', ['url' => $currentUrl]);
 
         if (strpos($currentUrl, 'oauth/token') !== false) {
+            Log::info('Matched oauth/token endpoint. Processing token response.');
+
             $content = json_decode($response->getContent(), true);
-            $content['id_token'] = $this->generateIdToken($content['access_token']);
+
+            if (json_last_error() !== JSON_ERROR_NONE) {
+                Log::info('Failed to decode JSON from response', [
+                    'error' => json_last_error_msg(),
+                    'response_content' => $response->getContent(),
+                ]);
+                return $response;
+            }
+
+            if (!isset($content['access_token'])) {
+                Log::info('Response missing access_token', ['content' => $content]);
+                return $response;
+            }
+
+            try {
+                $content['id_token'] = $this->generateIdToken($content['access_token']);
+                Log::info('Generated id_token successfully.');
+            } catch (\Throwable $e) {
+                Log::info('Error generating id_token', [
+                    'message' => $e->getMessage(),
+                    'trace' => $e->getTraceAsString(),
+                ]);
+                return $response; 
+            }
+
+            Log::info('Final modified response', ['content' => $content]);
+
             return response()->json($content, $response->getStatusCode(), $response->headers->all());
         }
 
+        Log::info('URL did not match oauth/token. Returning unmodified response.');
         return $response;
     }
 }
