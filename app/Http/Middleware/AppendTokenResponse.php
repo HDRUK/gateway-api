6ca434ae416cd89d79b009f3576a02787ae1c3bd
Jamie Byrne <jamie.byrne@hdruk.ac.uk>
2025-11-06 08:07:04 +0000

    dev logs

--- app/Http/Middleware/AppendTokenResponse.php
+++ app/Http/Middleware/AppendTokenResponse.php
@@ -5,9 +5,10 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Log; 
 use App\Http\Traits\CustomIdTokenTrait;
 use Symfony\Component\HttpFoundation\Response;
-
+// use DB;
 class AppendTokenResponse
 {
     use CustomIdTokenTrait;
@@ -21,7 +22,47 @@ class AppendTokenResponse
      */
     public function handle(Request $request, Closure $next): Response|JsonResponse
     {
+        Log::info('AppendTokenResponse::handle called', [
+        'route' => $request->route()?->getName(),
+        'controller' => $request->route()?->getActionName(),
+        'request_body' => $request->all(),
+        'query' => $request->query(),
+        'headers' => $request->headers->all(),
+    ]);
+       Log::info('AppendTokenResponse before $next() call');
+        Log::info('AppendTokenResponse incoming request payload', [
+            'headers' => [
+                'Content-Type' => $request->header('Content-Type'),
+                'Accept' => $request->header('Accept'),
+            ],
+            'input' => $request->input(),
+            'all' => $request->all(),
+            'query' => $request->query(),
+        ]);
+
+//  $response = $next($request);
+//        try {
+//            DB::enableQueryLog();
+//            $response = $next($request);
+//         } catch (\Throwable $e) {
+//             Log::info('AppendTokenResponse caught exception during $next()', [
+//                 'message' => $e->getMessage(),
+//                 'trace' => $e->getTraceAsString(),
+//                 'queries' => DB::getQueryLog(),
+//             ]);
+//             throw $e;
+//         }
+       try {
         $response = $next($request);
+        } catch (\Throwable $e) {
+            Log::info('AppendTokenResponse caught exception during $next()', [
+                'message' => $e->getMessage(),
+                'trace' => $e->getTraceAsString()
+            ]);
+            throw $e;
+        }
+        Log::info('AppendTokenResponse after $next() call');
+
         $currentUrl = $request->url();
 
         if (strpos($currentUrl, 'oauth/token') !== false) {
