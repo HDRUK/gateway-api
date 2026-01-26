8199f2ced5e3d92f13da5403c7205f4bd1371bd4
Calum Macdonald <calmacx@gmail.com>
2026-01-26 15:57:47 +0000

    update seeder for feature flag

--- database/seeders/FeatureSeeder.php
+++ database/seeders/FeatureSeeder.php
@@ -2,9 +2,8 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Seeder;
 use App\Models\Feature;
-use Illuminate\Support\Carbon;
+use Illuminate\Database\Seeder;
 
 class FeatureSeeder extends Seeder
 {
@@ -13,31 +12,22 @@ class FeatureSeeder extends Seeder
      */
     public function run(): void
     {
-        Feature::truncate();
+        $now = now();
 
-        $now = Carbon::now();
-
-        // Seed features with global scope
         $globalFeatures = [
             ['name' => 'SDEConciergeServiceEnquiry', 'value' => 'true'],
             ['name' => 'Aliases', 'value' => 'true'],
             ['name' => 'NhsSdeApplicationsEnabled', 'value' => 'false'],
             ['name' => 'Widgets', 'value' => 'false'],
-            
+            ['name' => 'RQuest', 'value' => 'true'],
+            ['name' => 'CohortDiscoveryService', 'value' => 'false'],
         ];
 
         foreach ($globalFeatures as $feature) {
-            Feature::create([
-                'name'       => $feature['name'],
-                'scope'      => '__laravel_null',
-                'value'      => $feature['value'],
-                'created_at' => $now,
-                'updated_at' => $now,
-            ]);
+            Feature::updateOrCreate(
+                ['name' => $feature['name'], 'scope' => '__laravel_null'],
+                ['value' => $feature['value'], 'updated_at' => $now]
+            );
         }
-
-        $this->command->newLine();
-        $this->command->info('All feature flags seeded successfully!');
-        $this->command->newLine();
     }
 }
