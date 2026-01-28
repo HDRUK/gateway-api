dec835da803a45f02c6b1a5c91d779506340b07e
Phil Reeks <phil.reeks@hdruk.ac.uk>
2026-01-28 10:25:39 +0000

    Fix issue with data access seeder

--- database/seeders/DataAccessTemplateSeeder.php
+++ database/seeders/DataAccessTemplateSeeder.php
@@ -14,8 +14,8 @@ class DataAccessTemplateSeeder extends Seeder
      */
     public function run(): void
     {
-        DataAccessTemplate::truncate();
-        DataAccessTemplateHasQuestion::truncate();
+        DataAccessTemplate::query()->delete();
+        DataAccessTemplateHasQuestion::query()->delete();
         DataAccessTemplate::factory(3)->create();
 
         DataAccessTemplate::all()->each(function ($model) {
