b927f5c4abbf1ef25254b4a4240a6d26ebb573eb
Dan Nita <dan.nita@hdruk.ac.uk>
2026-02-17 11:07:23 +0000

    feat(GAT-7350): Add first made active date column

---
+++ database/migrations/2026_02_17_102008_add_active_date_to_datasets_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class () extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('datasets', function (Blueprint $table) {
+            $table->dateTime('active_date')->nullable()->after('deleted_at');
+        });
+
+        \DB::statement("
+            UPDATE datasets
+            JOIN dataset_versions ON dataset_versions.dataset_id = datasets.id
+            AND dataset_versions.version = (
+                SELECT MIN(dv.version)
+                FROM dataset_versions dv
+                WHERE dv.dataset_id = datasets.id
+            )
+            SET datasets.active_date = dataset_versions.active_date
+            WHERE datasets.status = 'ACTIVE'
+        ");
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('datasets', function (Blueprint $table) {
+            $table->dropColumn('active_date');
+        });
+    }
+};
