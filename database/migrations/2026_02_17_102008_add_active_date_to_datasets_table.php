f1ffcfa992b6dcd26fad773d30b418a6e7cd3345
Dan Nita <dan.nita@hdruk.ac.uk>
2026-02-17 11:20:24 +0000

    update

--- database/migrations/2026_02_17_102008_add_active_date_to_datasets_table.php
+++ database/migrations/2026_02_17_102008_add_active_date_to_datasets_table.php
@@ -1,6 +1,6 @@
 <?php
 
-use DB;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
