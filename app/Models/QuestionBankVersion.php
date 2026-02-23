dadf7305eb9393cc01d143d8a13471a9af65fbd8
Dan Nita <dan.nita@hdruk.ac.uk>
2026-02-23 15:41:03 +0000

    update

--- app/Models/QuestionBankVersion.php
+++ app/Models/QuestionBankVersion.php
@@ -22,7 +22,6 @@ class QuestionBankVersion extends Model
      *
      * @var string
      */
-
     protected $table = 'question_bank_versions';
 
     public $timestamps = true;
@@ -47,15 +46,17 @@ class QuestionBankVersion extends Model
     /**
      * The question this version is associated with.
      */
-    public function question(): belongsTo
+    public function question(): BelongsTo
     {
         return $this->belongsTo(QuestionBank::class, 'question_id');
     }
 
     /**
-     * @return belongsToMany<QuestionBankVersion, $this>
+     * The child versions associated with this version.
+     *
+     * @return BelongsToMany<QuestionBankVersion>
      */
-    public function childVersions(): belongsToMany
+    public function childVersions(): BelongsToMany
     {
         return $this->belongsToMany(
             QuestionBankVersion::class,
@@ -65,7 +66,10 @@ class QuestionBankVersion extends Model
         )->withPivot('condition');
     }
 
-    public function parentVersion(): belongsTo
+    /**
+     * The parent version associated with this version.
+     */
+    public function parentVersion(): BelongsTo
     {
         return $this->belongsTo(
             QuestionBankVersion::class,
