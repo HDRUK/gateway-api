db490d824c5435fd266b6c96ca1594fa026db293
Loki Sinclair <117099666+loki-sinclair-hdruk@users.noreply.github.com>
2026-03-13 16:46:15 +0000

    feat(GAT-8465): rework of widget colours (#1584)

---
+++ database/migrations/2026_03_13_142624_widget-data-rework.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::dropIfExists('widget_settings');
+
+        Schema::table('widgets', function (Blueprint $table) {
+            $table->dropColumn('colours');
+
+            $table->string('branding_primary', 7)->nullable();
+            $table->string('branding_secondary', 7)->nullable();
+            $table->string('branding_neutral', 7)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        // Nope - One way...
+    }
+};
