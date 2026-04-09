61bc3c40f1bd22c9589453a9603f9105dced8a6a
Loki Sinclair <loki.sinclair@hdruk.ac.uk>
2026-04-09 16:02:23 +0100

    initial commit of search aggregator pre-work

---
+++ app/Services/SearchAggregator.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Services;
+
+use App\Contracts\SearchProvider;
+
+class SearchAggregator
+{
+    /** @var SearchProvider[] */
+    protected array $providers = [];
+
+    public function __construct(iterable $providers)
+    {
+        $this->providers = $providers;
+    }
+
+    public function search(string $query): array
+    {
+        $results = [];
+
+        foreach ($this->providers as $provider) {
+            try {
+                $results[$provider->getShortName()] = [
+                    'provider_logo' => $provider->getProviderLogo(),
+                    'about' => $provider->getProviderBlurb(),
+                    $provider->search($query),
+                ];
+            } catch (\Throwable $e) {
+                dd($e->getMessage());
+                \Log::error("Search provider {$provider->getShortName()} failed with error ", [
+                    'error' => $e->getMessage(),
+                ]);
+            }
+        }
+
+        return [
+            'message' => 'success',
+            'data' => [
+                'query' => $query,
+                'results' => $results,
+            ],
+        ];
+    }
+}
