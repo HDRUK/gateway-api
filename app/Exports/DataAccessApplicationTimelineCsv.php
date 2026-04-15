e29c347d381b9cdef39f826d6bb111bc1b47dbde
Dan Nita <dan.nita@hdruk.ac.uk>
2026-04-15 17:01:41 +0100

    update

--- app/Exports/DataAccessApplicationTimelineCsv.php
+++ app/Exports/DataAccessApplicationTimelineCsv.php
@@ -2,6 +2,7 @@
 
 namespace App\Exports;
 
+use Carbon\Carbon;
 use Maatwebsite\Excel\Concerns\FromArray;
 
 class DataAccessApplicationTimelineCsv implements FromArray
@@ -14,6 +15,24 @@ class DataAccessApplicationTimelineCsv implements FromArray
     public function array(): array
     {
         $rows = [];
+
+        $rows[] = ['Application Timeline'];
+        $rows[] = ['Application Title', 'Current Status', 'Intermediar State', 'Days Between States', 'Last Activity'];
+        foreach ($this->applicationIimeline as $item) {
+            $appTitle = $item['project_title'];
+            $latestActivity = $item['created_at'];
+            $latestStatus = end($item['states']);
+
+            foreach ($item['states'] as $value) {
+                $rows[] = [
+                    $appTitle,
+                    $latestStatus['approval_status'] ?? $latestStatus['submission_status'],
+                    $value['approval_status'] ?? $value['submission_status'],
+                    $value['days_between_states'],
+                    Carbon::parse($latestActivity)->format('Y-m-d'),
+                ];
+            }
+        }
 
         return $rows;
     }
