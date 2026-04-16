47b3494a43ca1933f1199f55d1f28c33e710844e
Dan Nita <dan.nita@hdruk.ac.uk>
2026-04-16 15:28:24 +0100

    fix(GAT-8744): update laravel passport package

--- app/Http/Controllers/SSO/CustomAccessTokenController.php
+++ app/Http/Controllers/SSO/CustomAccessTokenController.php
@@ -3,27 +3,16 @@
 namespace App\Http\Controllers\SSO;
 
 use Laravel\Passport\Http\Controllers\AccessTokenController as PassportAccessTokenController;
-use Illuminate\Http\Request;
-use Symfony\Bridge\PsrHttpMessage\Factory\PsrHttpFactory;
-use Nyholm\Psr7\Factory\Psr17Factory;
 use League\OAuth2\Server\Exception\OAuthServerException;
+use Psr\Http\Message\ServerRequestInterface;
+use Nyholm\Psr7\Response as Psr7Response;
 
 class CustomAccessTokenController extends PassportAccessTokenController
 {
-    public function issueOAuthToken(Request $request)
+    public function issueOAuthToken(ServerRequestInterface $psrRequest)
     {
-        $psr17Factory = new Psr17Factory();
-        $psrHttpFactory = new PsrHttpFactory(
-            $psr17Factory,
-            $psr17Factory,
-            $psr17Factory,
-            $psr17Factory
-        );
-
-        $psrRequest = $psrHttpFactory->createRequest($request);
-
         try {
-            return parent::issueToken($psrRequest);
+            return parent::issueToken($psrRequest, new Psr7Response());
         } catch (OAuthServerException $e) {
             return response()->json([
                 'code' => $e->getCode(),
