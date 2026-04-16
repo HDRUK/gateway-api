6a54a2bc0fec7bbce8772f2c2de284f212dd708c
Dan Nita <dan.nita@hdruk.ac.uk>
2026-04-16 16:50:44 +0100

    update

--- app/Http/Controllers/SSO/CustomAccessTokenController.php
+++ app/Http/Controllers/SSO/CustomAccessTokenController.php
@@ -3,16 +3,27 @@
 namespace App\Http\Controllers\SSO;
 
 use Laravel\Passport\Http\Controllers\AccessTokenController as PassportAccessTokenController;
+use Illuminate\Http\Request;
+use Symfony\Bridge\PsrHttpMessage\Factory\PsrHttpFactory;
+use Nyholm\Psr7\Factory\Psr17Factory;
 use League\OAuth2\Server\Exception\OAuthServerException;
-use Psr\Http\Message\ServerRequestInterface;
-use Nyholm\Psr7\Response as Psr7Response;
 
 class CustomAccessTokenController extends PassportAccessTokenController
 {
-    public function issueOAuthToken(ServerRequestInterface $psrRequest)
+    public function issueOAuthToken(Request $request)
     {
+        $psr17Factory = new Psr17Factory();
+        $psrHttpFactory = new PsrHttpFactory(
+            $psr17Factory,
+            $psr17Factory,
+            $psr17Factory,
+            $psr17Factory
+        );
+
+        $psrRequest = $psrHttpFactory->createRequest($request);
+
         try {
-            return parent::issueToken($psrRequest, new Psr7Response());
+            return parent::issueToken($psrRequest);
         } catch (OAuthServerException $e) {
             return response()->json([
                 'code' => $e->getCode(),
