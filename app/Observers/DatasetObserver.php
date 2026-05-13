de0be3da045fc27da2738fbffd7d8c1080d98b69
Loki Sinclair <loki.sinclair@hdruk.ac.uk>
2026-05-13 11:00:36 +0100

    fix(GAT-8785): Make elastic indexing calls async to avoid gmi falling out of its window

--- app/Observers/DatasetObserver.php
+++ app/Observers/DatasetObserver.php
@@ -2,14 +2,13 @@
 
 namespace App\Observers;
 
+use App\Jobs\DeindexDataset;
+use App\Jobs\IndexDataset;
 use App\Models\Dataset;
-use App\Http\Traits\IndexElastic;
 use App\Models\DatasetVersion;
 
 class DatasetObserver
 {
-    use IndexElastic;
-
     /**
      * Handle the Dataset "created" event.
      */
@@ -24,10 +23,7 @@ class DatasetObserver
             'dataset_id' => $dataset->id
         ])->select('id')->first();
         if ($dataset->status === Dataset::STATUS_ACTIVE && !is_null($datasetVersion)) {
-            $this->reindexElastic($dataset->id);
-            if ($dataset->team_id) {
-                $this->reindexElasticDataProviderWithRelations((int) $dataset->team_id, 'dataset');
-            }
+            IndexDataset::dispatch($dataset->id);
         }
     }
 
@@ -54,21 +50,12 @@ class DatasetObserver
             'dataset_id' => $dataset->id
         ])->select('id')->first();
 
-        // TODO - Stop this from being synchronous calls. These are blocking
-        // and should be handed off to job queues.
-        //
         if ($prevStatus === Dataset::STATUS_ACTIVE && $dataset->status !== Dataset::STATUS_ACTIVE) {
-            $this->deleteDatasetFromElastic($dataset->id);
-            if ($dataset->team_id) {
-                $this->reindexElasticDataProviderWithRelations((int) $dataset->team_id, 'dataset');
-            }
+            DeindexDataset::dispatch($dataset->id, $dataset->team_id ? (int) $dataset->team_id : null);
         }
 
         if ($dataset->status === Dataset::STATUS_ACTIVE && !is_null($datasetVersion)) {
-            $this->reindexElastic($dataset->id);
-            if ($dataset->team_id) {
-                $this->reindexElasticDataProviderWithRelations((int) $dataset->team_id, 'dataset');
-            }
+            IndexDataset::dispatch($dataset->id);
         }
     }
 
@@ -88,10 +75,7 @@ class DatasetObserver
         $prevStatus = $dataset->prevStatus;
 
         if ($prevStatus === Dataset::STATUS_ACTIVE) {
-            $this->deleteDatasetFromElastic($dataset->id);
-            if ($dataset->team_id) {
-                $this->reindexElasticDataProviderWithRelations((int) $dataset->team_id, 'dataset');
-            }
+            DeindexDataset::dispatch($dataset->id, $dataset->team_id ? (int) $dataset->team_id : null);
         }
     }
 
