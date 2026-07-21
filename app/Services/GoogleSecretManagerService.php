7d9e7321d29dc2a0fc6f1e07e3426c24c587e30d
Loki Sinclair <117099666+loki-sinclair-hdruk@users.noreply.github.com>
2026-07-21 15:07:00 +0100

    fix(GAT-9247): Fixes GSM failure while calling old service no longer … (#1707)

--- app/Services/GoogleSecretManagerService.php
+++ app/Services/GoogleSecretManagerService.php
@@ -4,7 +4,14 @@ namespace App\Services;
 
 use Config;
 use Google\Cloud\SecretManager\V1\AccessSecretVersionRequest;
+use Google\Cloud\SecretManager\V1\AddSecretVersionRequest;
 use Google\Cloud\SecretManager\V1\Client\SecretManagerServiceClient;
+use Google\Cloud\SecretManager\V1\CreateSecretRequest;
+use Google\Cloud\SecretManager\V1\DeleteSecretRequest;
+use Google\Cloud\SecretManager\V1\Replication;
+use Google\Cloud\SecretManager\V1\Replication\Automatic;
+use Google\Cloud\SecretManager\V1\Secret;
+use Google\Cloud\SecretManager\V1\SecretPayload;
 
 class GoogleSecretManagerService
 {
@@ -26,8 +33,42 @@ class GoogleSecretManagerService
         return $response->getPayload()->getData();
     }
 
-    public function createSecret(): void
+    public function createSecret(string $secretId, string $payload): void
     {
-        // to do
+        $projectPath = Config::get('metadata.google_project_path');
+
+        $secret = (new Secret())->setReplication(
+            (new Replication())->setAutomatic(new Automatic())
+        );
+
+        $request = (new CreateSecretRequest())
+            ->setParent($projectPath)
+            ->setSecretId($secretId)
+            ->setSecret($secret);
+
+        $this->client->createSecret($request);
+
+        $this->addSecretVersion($secretId, $payload);
+    }
+
+    public function addSecretVersion(string $secretId, string $payload): void
+    {
+        $projectPath = Config::get('metadata.google_project_path');
+
+        $request = (new AddSecretVersionRequest())
+            ->setParent($projectPath . '/secrets/' . $secretId)
+            ->setPayload((new SecretPayload())->setData($payload));
+
+        $this->client->addSecretVersion($request);
+    }
+
+    public function deleteSecret(string $secretId): void
+    {
+        $projectPath = Config::get('metadata.google_project_path');
+
+        $request = (new DeleteSecretRequest())
+            ->setName($projectPath . '/secrets/' . $secretId);
+
+        $this->client->deleteSecret($request);
     }
 }
