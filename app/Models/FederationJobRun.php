962ff179e96009cd9be6e609639d20250f0850bc
Matthew Butterworth <matthew.butterworth@hdruk.ac.uk>
2026-07-24 15:21:33 +0100

    refactor(GAT-9231): Add seeders for federation job runs

--- app/Models/FederationJobRun.php
+++ app/Models/FederationJobRun.php
@@ -2,12 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Prunable;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class FederationJobRun extends Model
 {
+    use HasFactory;
     use SoftDeletes;
     use Prunable;
 
