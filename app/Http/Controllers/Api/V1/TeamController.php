3bd8d3037c34a0f94ac8c3d1e7abffe0622f4b7f
Loki Sinclair <117099666+loki-sinclair-hdruk@users.noreply.github.com>
2026-08-06 16:54:42 +0100

    feat(GAT-8899): improves call speeds to return teams (#1749)

--- app/Http/Controllers/Api/V1/TeamController.php
+++ app/Http/Controllers/Api/V1/TeamController.php
@@ -119,10 +119,13 @@ class TeamController extends Controller
             }
 
             $perPage = request('per_page', Config::get('constants.per_page'));
-            $teams = $query
+            $teamsPaginator = $query
                 ->with(['users', 'aliases'])
-                ->paginate($perPage, ['*'], 'page')
-                ->toArray();
+                ->paginate($perPage, ['*'], 'page');
+
+            User::preloadCohortDataForUsers($teamsPaginator->getCollection()->flatMap(fn ($team) => $team->users));
+
+            $teams = $teamsPaginator->toArray();
 
             $teams['data'] = $this->getTeams($teams['data']);
 
@@ -259,7 +262,11 @@ class TeamController extends Controller
         $jwtUser = array_key_exists('jwt_user', $input) ? $input['jwt_user'] : [];
 
         try {
-            $userTeam = Team::where('id', $teamId)->with(['users', 'notifications', 'aliases'])->get()->toArray();
+            $userTeamCollection = Team::where('id', $teamId)->with(['users', 'notifications', 'aliases'])->get();
+
+            User::preloadCohortDataForUsers($userTeamCollection->flatMap(fn ($team) => $team->users));
+
+            $userTeam = $userTeamCollection->toArray();
 
             Auditor::log([
                 'user_id' => (int)$jwtUser['id'],
